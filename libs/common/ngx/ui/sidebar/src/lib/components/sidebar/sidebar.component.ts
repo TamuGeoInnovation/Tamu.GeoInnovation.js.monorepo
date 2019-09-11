0bffaaa062764f33b2d19f00e7c848273b64d74e
Jonathan Janzen <jonjanzen@tamu.edu>
2019-09-11 13:17:26 -0500

    Fix sidebar lint

--- libs/common/ngx/ui/sidebar/src/lib/components/sidebar/sidebar.component.ts
+++ libs/common/ngx/ui/sidebar/src/lib/components/sidebar/sidebar.component.ts
@@ -64,7 +64,6 @@ export class SidebarComponent implements OnInit, AfterContentInit, OnDestroy {
       )
       .subscribe((res) => {
         // TODO: Do routing here.
-        debugger;
       });
   }
 
@@ -88,7 +87,7 @@ export class SidebarComponent implements OnInit, AfterContentInit, OnDestroy {
     this._updateCurrentView();
 
     // If selected view name is the same, hide the sidebar
-    if (viewName == this.currentView) {
+    if (viewName === this.currentView) {
       this.sidebarVisible = !this.sidebarVisible;
     } else {
       // If selected view name is different than current, show sidebar (in case it's hidden), store the selected view name as the current, and navigate to that route
