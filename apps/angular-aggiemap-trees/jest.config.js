2a488c3e47de7bae23dfd7a18b7158ab0136c81d
Edgar <edher@tamu.edu>
2019-09-11 19:27:36 -0500

    Added angular-aggiemap-trees app
    Will house the tree inventory application.
    
    Has working map and sidebar components

---
+++ apps/angular-aggiemap-trees/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: 'angular-aggiemap-trees',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/angular-aggiemap-trees',
+  snapshotSerializers: ['jest-preset-angular/AngularSnapshotSerializer.js', 'jest-preset-angular/HTMLCommentSerializer.js']
+};
---
+++ apps/angular-aggiemap-trees/src/environments/definitions.ts
@@ -0,0 +1,25 @@
+export const Connections = {
+  basemapUrl: 'https://gis.tamu.edu/arcgis/rest/services/FCOR/BaseMap_20190813/MapServer',
+  inforUrl: 'https://gis.tamu.edu/arcgis/rest/services/FCOR/MapInfo_20190529/MapServer',
+  accessibleUrl: 'https://fc-gis.tamu.edu/arcgis/rest/services/FCOR/ADA_120717/MapServer/0',
+  constructionUrl: 'https://gis.tamu.edu/arcgis/rest/services/FCOR/Construction_2018/MapServer',
+  departmentUrl: 'https://fc-gis.tamu.edu/arcgis/rest/services/FCOR/DepartmentSearch/MapServer/1',
+  tsMainUrl: 'https://gis.tamu.edu/arcgis/rest/services/TS/TS_Main/MapServer'
+};
+
+export const Definitions = {
+  BUILDINGS: {
+    id: 'buildings',
+    layerId: 'buildings-layer',
+    name: 'Buildings',
+    url: `${Connections.basemapUrl}/1`,
+    popupComponent: 'BuildingPopupComponent'
+  },
+  CONSTRUCTION: {
+    id: 'construction_zone',
+    layerId: 'construction_zone-layer',
+    name: 'Construction Zone',
+    url: `${Connections.constructionUrl}`,
+    popupComponent: 'ConstructionPopupComponent'
+  }
+};
