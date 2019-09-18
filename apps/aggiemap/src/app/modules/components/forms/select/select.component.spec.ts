65db99f7d5e7aa861e6beb131b7cec2c392b2eef
Jonathan Janzen <jonjanzen@tamu.edu>
2019-09-18 13:22:21 -0500

    Fix tslint

--- apps/aggiemap/src/app/modules/components/forms/select/select.component.spec.ts
+++ apps/aggiemap/src/app/modules/components/forms/select/select.component.spec.ts
@@ -1,7 +1,6 @@
-import { async, ComponentFixture, inject, TestBed } from '@angular/core/testing';
+import { inject } from '@angular/core/testing';
 
 import { SelectComponent } from './select.component';
-import { MapComponent } from '../../../../../../../angular-aggiemap-trees/src/app/components/map/map.component';
 
 describe('SelectComponent', () => {
   it('should create', () => {
