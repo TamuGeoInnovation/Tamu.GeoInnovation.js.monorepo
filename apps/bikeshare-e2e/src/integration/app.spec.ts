900a29ebeb4d23739026aaffab944f1bc2c6e911
jacobericson <jacobericson@users.noreply.github.com>
2019-09-19 11:51:28 -0500

    Remove aggiemap references (#24)

--- apps/bikeshare-e2e/src/integration/app.spec.ts
+++ apps/bikeshare-e2e/src/integration/app.spec.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('aggiemap', () => {
+describe('bikeshare', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
