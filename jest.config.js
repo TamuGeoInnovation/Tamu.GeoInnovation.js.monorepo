0fcb53ce6ffb51fd45867beae1c957119fc299dd
Jonathan Janzen <jonjanzen@tamu.edu>
2019-09-25 13:34:42 -0500

    Switch test loader to maybe fix Edgar's machine?

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,10 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
-const { compilerOptions } = require("./tsconfig.json");
-
 module.exports = {
-  globals: {
-    "ts-jest": {
-      tsConfig: "./test/tsconfig.spec.json",
-      stringifyContentPathRegex: '\\.html$',
-      astTransformers: [require.resolve('jest-preset-angular/InlineHtmlStripStylesTransformer')],
-      diagnostics: {
-        ignoreCodes: [151001]
-      }
-    }
+  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
+  transform: {
+    '^.+\\.(ts|js|html)$': 'ts-jest'
   },
-  roots: [
-    "<rootDir>/libs/",
-    "<rootDir>/apps/"
-  ],
-  modulePathIgnorePatterns: [
-    "<rootDir>/apps/.*-e2e"
-  ],
-  preset: "jest-preset-angular",
-  setupFilesAfterEnv: ["./test/setupJest.ts"],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' })
+  resolver: '@nrwl/jest/plugins/resolver',
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageReporters: ['html'],
+  passWithNoTests: true
 };
