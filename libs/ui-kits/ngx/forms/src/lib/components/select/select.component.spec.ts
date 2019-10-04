985daf9a09daab9cddc2fab7cfcfc8fee9f2ba40
Jonathan Janzen <jonjanzen@tamu.edu>
2019-10-04 16:43:22 -0500

    Add tests for select.component.ts

--- libs/ui-kits/ngx/forms/src/lib/components/select/select.component.spec.ts
+++ libs/ui-kits/ngx/forms/src/lib/components/select/select.component.spec.ts
@@ -9,3 +9,13 @@ describe('SelectComponent', () => {
     });
   });
 });
+
+describe('getDataItemValue', () => {
+  it('should handle template being undefined', () => {
+    expect(new SelectComponent().getDataItemValue({ test: 'value' })).toEqual({ test: 'value' });
+  });
+
+  it('should handle correct inputs', () => {
+    expect(new SelectComponent().getDataItemValue({ test: 'value' }, 'test')).toEqual('value');
+  });
+});
