db0e7da0adf4dc1ca82d2d71a08b18b5e0900060
Jon Janzen <jjjonjanzen@gmail.com>
2019-10-11 16:58:24 -0500

    Increase test coverage (#19)

--- libs/search/src/lib/services/search.service.spec.ts
+++ libs/search/src/lib/services/search.service.spec.ts
@@ -1,11 +1,35 @@
 import { inject, TestBed } from '@angular/core/testing';
 
-import { SearchService } from './search.service';
+import { SearchResult, SearchService } from './search.service';
 
 describe('SearchService', () => {
   it('should be created', () => {
     inject([SearchService], (searchService: SearchService) => {
       expect(searchService).toBeTruthy();
     });
+  });
+});
+
+describe('SearchResult', () => {
+  it('should list features', () => {
+    const search_result = new SearchResult({
+      results: [
+        {
+          features: ['test'],
+          breadcrumbs: {
+            source: 'tests',
+            value: 'probably nothings'
+          }
+        },
+        {
+          features: ['test2'],
+          breadcrumbs: {
+            source: 'tests',
+            value: 'probably nothings'
+          }
+        }
+      ]
+    });
+    expect(search_result.features()).toEqual(['test', 'test2']);
   });
 });
