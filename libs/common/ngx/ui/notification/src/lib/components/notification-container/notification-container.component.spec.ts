218780e5fed24989e4913bab5b8d3fa34a0bed72
Jonathan Janzen <jonjanzen@tamu.edu>
2019-10-28 16:07:25 -0500

    Use correct inject() calls in the rest of lib tests

--- libs/common/ngx/ui/notification/src/lib/components/notification-container/notification-container.component.spec.ts
+++ libs/common/ngx/ui/notification/src/lib/components/notification-container/notification-container.component.spec.ts
@@ -1,11 +1,32 @@
-import { inject } from '@angular/core/testing';
+import { async, inject, TestBed } from '@angular/core/testing';
 
 import { NotificationContainerComponent } from './notification-container.component';
+import { AppStorage } from '@tamu-gisc/common/ngx/local-store';
+import { env, EnvironmentModule } from '@tamu-gisc/common/ngx/environment';
+import { LOCAL_STORAGE, StorageServiceModule } from 'angular-webstorage-service';
 
 describe('ContainerComponent', () => {
-  it('should create', () => {
-    inject([NotificationContainerComponent], (notificationContainerComponent: NotificationContainerComponent) => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        NotificationContainerComponent,
+        {
+          provide: AppStorage,
+          useExisting: LOCAL_STORAGE
+        },
+        {
+          provide: env,
+          useValue: { NotificationEvents: [] }
+        }
+      ],
+      imports: [EnvironmentModule, StorageServiceModule]
+    }).compileComponents();
+  }));
+
+  it('should create', inject(
+    [NotificationContainerComponent],
+    (notificationContainerComponent: NotificationContainerComponent) => {
       expect(notificationContainerComponent).toBeTruthy();
-    });
-  });
+    }
+  ));
 });
