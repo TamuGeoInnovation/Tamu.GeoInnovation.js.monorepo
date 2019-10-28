700fcce599f2b3177579074be697557db861d68a
Jonathan Janzen <jonjanzen@tamu.edu>
2019-10-28 16:33:55 -0500

    Use correct inject() calls in all of the apps tests

--- apps/aggiemap/src/app/modules/services/transportation/drive/parking.service.spec.ts
+++ apps/aggiemap/src/app/modules/services/transportation/drive/parking.service.spec.ts
@@ -1,11 +1,32 @@
-import { inject, TestBed } from '@angular/core/testing';
+import { async, inject, TestBed } from '@angular/core/testing';
 
 import { ParkingService } from './parking.service';
+import { EsriMapModule } from '@tamu-gisc/maps/esri';
+import { SearchModule } from '@tamu-gisc/search';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { env, EnvironmentModule } from '@tamu-gisc/common/ngx/environment';
+import { AppStorage } from '@tamu-gisc/common/ngx/local-store';
+import { LOCAL_STORAGE, StorageServiceModule } from 'angular-webstorage-service';
 
 describe('ParkingService', () => {
-  it('should be created', () => {
-    inject([ParkingService], (component: ParkingService) => {
-      expect(component).toBeTruthy();
-    });
-  });
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [EsriMapModule, SearchModule, HttpClientTestingModule, EnvironmentModule, StorageServiceModule],
+      providers: [
+        ParkingService,
+        {
+          provide: env,
+          useValue: { SearchSources: [] }
+        },
+        {
+          provide: AppStorage,
+          useExisting: LOCAL_STORAGE
+        }
+      ]
+    }).compileComponents();
+  }));
+
+  it('should be created', inject([ParkingService], (component: ParkingService) => {
+    expect(component).toBeTruthy();
+  }));
 });
