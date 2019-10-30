7e4a355789cbaaaed38ceb13e32f9182931509c1
Jonathan Janzen <jonjanzen@tamu.edu>
2019-10-30 12:08:23 -0500

    92% coverage of esri-map component

--- libs/maps/esri/src/lib/components/esri-map/esri-map.component.spec.ts
+++ libs/maps/esri/src/lib/components/esri-map/esri-map.component.spec.ts
@@ -1,11 +1,15 @@
 import { async, inject, TestBed } from '@angular/core/testing';
 
 import { EsriMapComponent } from './esri-map.component';
-import { EsriMapModule } from '@tamu-gisc/maps/esri';
+import { EsriMapModule, EsriMapService, MapServiceInstance } from '@tamu-gisc/maps/esri';
 import { RouterTestingModule } from '@angular/router/testing';
 import { SearchModule } from '@tamu-gisc/search';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { env, EnvironmentModule } from '@tamu-gisc/common/ngx/environment';
+import { ElementRef } from '@angular/core';
+import { AsyncSubject } from 'rxjs';
+
+import esri = __esri;
 
 describe('EsriMapComponent', () => {
   beforeEach(async(() => {
@@ -23,5 +27,15 @@ describe('EsriMapComponent', () => {
 
   it('should create', inject([EsriMapComponent], (esriMapComponent: EsriMapComponent) => {
     expect(esriMapComponent).toBeTruthy();
+  }));
+
+  it('should error on missing config', inject([EsriMapComponent], (esriMapComponent: EsriMapComponent) => {
+    expect(() => esriMapComponent.ngOnInit()).toThrow(new Error('Incorrectly formed configuration provided.'));
+  }));
+
+  it('should correctly configure map service', inject([EsriMapComponent], (esriMapComponent: EsriMapComponent) => {
+    esriMapComponent.config = { basemap: null, view: { properties: {}, mode: '2d' } };
+    ((esriMapComponent as unknown) as { container: ElementRef }).container = { nativeElement: null };
+    expect(esriMapComponent.ngOnInit()).toBeUndefined();
   }));
 });
