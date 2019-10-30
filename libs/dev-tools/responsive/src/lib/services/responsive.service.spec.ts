358becc9d481c63d51c589259e4001d38947805b
Jonathan Janzen <jonjanzen@tamu.edu>
2019-10-30 11:44:29 -0500

    100% coverage of responsive service

--- libs/dev-tools/responsive/src/lib/services/responsive.service.spec.ts
+++ libs/dev-tools/responsive/src/lib/services/responsive.service.spec.ts
@@ -10,4 +10,28 @@ describe('ResponsiveService', () => {
   it('should be created', inject([ResponsiveService], (responsiveService: ResponsiveService) => {
     expect(responsiveService).toBeTruthy();
   }));
+
+  it('should get snapshot', inject([ResponsiveService], (responsiveService: ResponsiveService) => {
+    expect(responsiveService.snapshot).toEqual({ isMobile: false, screenWidth: 1024 });
+  }));
+
+  it('should get status', inject([ResponsiveService], (responsiveService: ResponsiveService) => {
+    responsiveService.getStatus().forEach((value) => {
+      expect(value).toEqual(false);
+    });
+  }));
+
+  it('should be mobile for <768', () => {
+    ((window as unknown) as { innerWidth: number }).innerWidth = 700;
+    inject([ResponsiveService], (responsiveService: ResponsiveService) => {
+      expect(responsiveService.snapshot).toEqual({ isMobile: true, screenWidth: 700 });
+    })();
+  });
+
+  it('should not be mobile for >768 && <992', () => {
+    ((window as unknown) as { innerWidth: number }).innerWidth = 900;
+    inject([ResponsiveService], (responsiveService: ResponsiveService) => {
+      expect(responsiveService.snapshot).toEqual({ isMobile: false, screenWidth: 900 });
+    })();
+  });
 });
