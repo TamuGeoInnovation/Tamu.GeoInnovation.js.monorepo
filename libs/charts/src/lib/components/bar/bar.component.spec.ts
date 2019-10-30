db04b14d4e37c255dd9cf59945437993cfcdfb0e
Jonathan Janzen <jonjanzen@tamu.edu>
2019-10-30 12:17:42 -0500

    100% coverage of bar chart component

--- libs/charts/src/lib/components/bar/bar.component.spec.ts
+++ libs/charts/src/lib/components/bar/bar.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, inject, TestBed } from '@angular/core/testing';
 
 import { BarChartComponent } from './bar.component';
+import { BarChartConfiguration, ChartContainerComponent } from '../chart-container/chart-container.component';
+import { Observable } from 'rxjs';
 
 describe('BarChartComponent', () => {
   beforeEach(async(() => {
@@ -9,5 +11,12 @@ describe('BarChartComponent', () => {
 
   it('should create', inject([BarChartComponent], (component: BarChartComponent) => {
     expect(component).toBeTruthy();
+  }));
+
+  it('should set baseConfig', inject([BarChartComponent], (component: BarChartComponent) => {
+    component.source = new Observable<unknown[]>();
+    ((component as unknown) as { chart: Partial<ChartContainerComponent> }).chart = { create: () => {} };
+    component.ngAfterViewInit();
+    expect(component.baseConfig).toEqual(new BarChartConfiguration());
   }));
 });
