2984b3e4baa7a69538c319e644832841b4cdc566
atharmon <atharmon@tamu.edu>
2019-10-31 19:57:32 -0300

    It finally works!

--- apps/gisday-competition/src/app/app.module.ts
+++ apps/gisday-competition/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './modules/routing/routing.module';
 
+import { EnvironmentModule, env } from '@tamu-gisc/common/ngx/environment';
+import * as environment from '../environments/environment';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, EnvironmentModule],
+  providers: [{ provide: env, useValue: environment }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
