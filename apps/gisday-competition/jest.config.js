eb3972ec30183b8a80e5a4d081bd6051656ae643
atharmon <atharmon@tamu.edu>
2019-10-31 18:06:59 -0300

    Intial commit of gisday-competition

---
+++ apps/gisday-competition/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: 'gisday-competition',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/gisday-competition',
+  snapshotSerializers: ['jest-preset-angular/AngularSnapshotSerializer.js', 'jest-preset-angular/HTMLCommentSerializer.js']
+};
