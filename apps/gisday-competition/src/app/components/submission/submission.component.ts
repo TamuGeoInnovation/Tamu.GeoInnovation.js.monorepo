c8cd0cf5ce8a04a6d1c8bb021336a644338a4f56
atharmon <atharmon@tamu.edu>
2019-11-03 23:04:15 -0300

    Added in generated code

---
+++ apps/gisday-competition/src/app/components/submission/submission.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tamu-gisc-submission',
+  templateUrl: './submission.component.html',
+  styleUrls: ['./submission.component.scss']
+})
+export class SubmissionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
