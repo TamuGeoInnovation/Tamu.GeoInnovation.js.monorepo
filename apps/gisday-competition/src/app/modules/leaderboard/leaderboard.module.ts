25766ca96e125fba5c6333d8353a638eb9327780
atharmon <atharmon@tamu.edu>
2019-11-04 01:00:50 -0300

    Rearranged the file structure

--- apps/gisday-competition/src/app/modules/leaderboard/leaderboard.module.ts
+++ apps/gisday-competition/src/app/modules/leaderboard/leaderboard.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
-import { LeaderboardComponent } from '../../components/leaderboard/leaderboard.component';
+import { LeaderboardComponent } from './components/leaderboard.component';
 
 const routes: Routes = [{
   path: '', component: LeaderboardComponent
