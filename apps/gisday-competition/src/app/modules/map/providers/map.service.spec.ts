0284d70f7b8db28c18835a6e9b183177c8b1184c
Edher <edher@tamu.edu>
2019-11-06 02:38:43 -0600

    Styling changes
    Changes map component basemap
    
    Fixed gisday competition map service test

--- apps/gisday-competition/src/app/modules/map/providers/map.service.spec.ts
+++ apps/gisday-competition/src/app/modules/map/providers/map.service.spec.ts
@@ -1,9 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MapService } from './map.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('MapService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({ imports: [HttpClientModule] }));
 
   it('should be created', () => {
     const service: MapService = TestBed.get(MapService);
