77582b54b61b8ed43c2fad87d7acaf378a8fe1ed
a152252 <a152252@tamu.edu>
2019-11-07 10:35:32 -0600

    Send date data from component to layerService (#5)

--- apps/bikeshare/src/app/modules/components/sidebar/components/date-picker/date.component.ts
+++ apps/bikeshare/src/app/modules/components/sidebar/components/date-picker/date.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+import { LayerListService } from '../../../../services/ui/layer-list.service';
 
 @Component({
   selector: 'date-picker',
@@ -9,10 +10,17 @@ import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core
 export class DateComponent implements OnInit {
   public dateTimeRange: Date[];
 
-  constructor() {}
+  constructor(private layerListService: LayerListService) {}
 
   public ngOnInit() {
+    // Default Dates = last 24h to now
     const yesterday = new Date(new Date().getTime() - 24 * 60 * 60 * 1000);
     this.dateTimeRange = [yesterday, new Date()];
+  }
+
+  // Called when datePicker is closed
+  // Sends the dates to the layer service
+  public newDate() {
+    this.layerListService.changeDate(this.dateTimeRange);
   }
 }
