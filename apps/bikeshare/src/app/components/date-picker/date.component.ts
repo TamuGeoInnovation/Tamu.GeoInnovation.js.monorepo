b141c2468f1d39c9344329ab5e86f3ab0cc7c901
a152252 <a152252@tamu.edu>
2019-12-02 18:26:42 -0600

    Add max date check (Nov 21 last day of trip data)

--- apps/bikeshare/src/app/components/date-picker/date.component.ts
+++ apps/bikeshare/src/app/components/date-picker/date.component.ts
@@ -10,13 +10,18 @@ import { LayerListService } from '@tamu-gisc/maps/feature/layer-list';
 export class DateRange implements OnInit {
   public dateTimeRange: Date[];
 
+  // Max date allowed by the date picker
+  // Currently set to Nov 21, 2019 as the last day with trip data
+  public max = new Date(2019, 10, 21);
+
   constructor(private layerListService: LayerListService) {}
   //constructor() {}
 
   public ngOnInit() {
     // Default Dates = last 24h to now
-    const yesterday = new Date(new Date().getTime() - 24 * 60 * 60 * 1000);
-    this.dateTimeRange = [yesterday, new Date()];
+    //const yesterday = new Date(new Date().getTime() - 24 * 60 * 60 * 1000);
+    //this.dateTimeRange = [yesterday, new Date()];
+    this.dateTimeRange = [new Date(2019, 10, 20), new Date(2019, 10, 21)];
   }
 
   // Called when datePicker is closed
