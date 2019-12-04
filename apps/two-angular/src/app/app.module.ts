1e79e1a102b5652d317ddf136aacbcc4cc8d1531
Edgar <edher@tamu.edu>
2019-12-03 18:40:44 -0600

    Added header and footer

--- apps/two-angular/src/app/app.module.ts
+++ apps/two-angular/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './modules/routing/routing.module';
+import { FrameModule } from './modules/frame/frame.module';
 
 @NgModule({
+  imports: [BrowserModule, AppRoutingModule, FrameModule],
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
