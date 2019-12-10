093e108c5475b8f8bf9f03b4871070f7c6463765
Edgar <edher@tamu.edu>
2019-12-10 11:43:34 -0600

    Removed some empty placeholder libraries

--- libs/common/ngx/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  name: 'common-ngx',
-  preset: '../../../jest.config.js',
-  coverageDirectory: '../../../coverage/libs/common/ngx',
-  snapshotSerializers: ['jest-preset-angular/AngularSnapshotSerializer.js', 'jest-preset-angular/HTMLCommentSerializer.js']
-};
