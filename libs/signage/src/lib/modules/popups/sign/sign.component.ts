a1f011e45508b03186a185dcf9a0fb87cef832e6
Edgar <edher@tamu.edu>
2019-12-12 13:29:03 -0600

    Added signage sign popup component details

--- libs/signage/src/lib/modules/popups/sign/sign.component.ts
+++ libs/signage/src/lib/modules/popups/sign/sign.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { shareReplay, pluck } from 'rxjs/operators';
 
 import { BasePopupComponent } from '@tamu-gisc/maps/feature/popup';
 
@@ -8,9 +11,33 @@ import { BasePopupComponent } from '@tamu-gisc/maps/feature/popup';
   styleUrls: ['./sign.component.scss']
 })
 export class SignComponent extends BasePopupComponent implements OnInit {
-  constructor() {
+  public details: Observable<ISignagePhoto>;
+
+  constructor(private http: HttpClient) {
     super();
   }
 
-  public ngOnInit() {}
+  public ngOnInit() {
+    this.details = this.http
+      .get('https://gisday.tamu.edu/Rest/Signage/Get/Photos/', {
+        params: {
+          submissionGuid: this.data.attributes.guid
+        }
+      })
+      .pipe(
+        pluck<ISignagePhotoResponse, ISignagePhoto>('items', '0'),
+        shareReplay(1)
+      );
+  }
+}
+
+interface ISignagePhoto {
+  added: string;
+  guid: string;
+  imageBase64String: string;
+  submissionGuid: string;
+}
+
+interface ISignagePhotoResponse {
+  items: Array<ISignagePhoto>;
 }
