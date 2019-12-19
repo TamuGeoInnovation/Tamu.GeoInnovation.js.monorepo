4448b4d3b77620e06cc38eecc326dcc9d5a59c90
Edgar <edher@tamu.edu>
2019-12-19 14:30:02 -0600

    Fixed draggable directive test

--- libs/ui-kits/ngx/interactions/draggable/src/lib/directives/drag/drag.directive.spec.ts
+++ libs/ui-kits/ngx/interactions/draggable/src/lib/directives/drag/drag.directive.spec.ts
@@ -1,8 +1,34 @@
+import { Component, ViewChild } from '@angular/core';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+
 import { DragDirective } from './drag.directive';
 
+@Component({
+  template: `
+    <div draggable></div>
+  `
+})
+class MockDraggableDirectiveComponent {
+  @ViewChild(DragDirective, { static: true })
+  public directive: DragDirective;
+}
+
 describe('DragDirective', () => {
+  let fixture: ComponentFixture<MockDraggableDirectiveComponent>;
+  let component: MockDraggableDirectiveComponent;
+
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      declarations: [DragDirective, MockDraggableDirectiveComponent]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MockDraggableDirectiveComponent);
+    component = fixture.componentInstance;
+  });
+
   it('should create an instance', () => {
-    const directive = new DragDirective({});
-    expect(directive).toBeTruthy();
+    expect(component.directive).toBeDefined();
   });
 });
