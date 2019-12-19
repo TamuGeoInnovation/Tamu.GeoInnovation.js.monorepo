f38295054eed1d6db41326b0c3c1c0ed1301f0c4
Edgar <edher@tamu.edu>
2019-12-19 15:14:54 -0600

    Fixed basic render host directive test

--- libs/ui-kits/ngx/layout/structural/src/lib/directives/render-host/render-host.directive.spec.ts
+++ libs/ui-kits/ngx/layout/structural/src/lib/directives/render-host/render-host.directive.spec.ts
@@ -1,8 +1,33 @@
+import { Component, ViewChild } from '@angular/core';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+
 import { RenderHostDirective } from './render-host.directive';
 
+@Component({
+  template: `
+    <div render-host></div>
+  `
+})
+class MockRenderHostDirectiveComponent {
+  @ViewChild(RenderHostDirective, { static: true })
+  public directive: RenderHostDirective;
+}
+
 describe('RenderHostDirective', () => {
+  let fixture: ComponentFixture<MockRenderHostDirectiveComponent>;
+  let component: MockRenderHostDirectiveComponent;
+
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      declarations: [RenderHostDirective, MockRenderHostDirectiveComponent]
+    }).compileComponents();
+  });
+
   it('should create an instance', () => {
-    const directive = new RenderHostDirective();
-    expect(directive).toBeTruthy();
+    fixture = TestBed.createComponent(MockRenderHostDirectiveComponent);
+    component = fixture.componentInstance;
+
+    // Tests directive selector and element reference.
+    expect(component.directive.viewContainerRef).toBeTruthy();
   });
 });
