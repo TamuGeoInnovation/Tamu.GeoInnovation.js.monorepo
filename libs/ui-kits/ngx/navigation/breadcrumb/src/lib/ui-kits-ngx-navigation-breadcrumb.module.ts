2a0630b614350cb2a0cde5225f85f2c23147125c
Edgar <edher@tamu.edu>
2020-01-30 20:03:04 -0600

    Working breadcrumb logic

--- libs/ui-kits/ngx/navigation/breadcrumb/src/lib/ui-kits-ngx-navigation-breadcrumb.module.ts
+++ libs/ui-kits/ngx/navigation/breadcrumb/src/lib/ui-kits-ngx-navigation-breadcrumb.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   declarations: [BreadcrumbComponent],
   exports: [BreadcrumbComponent]
 })
