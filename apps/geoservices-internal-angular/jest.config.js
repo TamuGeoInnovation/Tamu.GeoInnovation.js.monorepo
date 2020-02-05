d3b098662af432b14b19e0693d978149cd414738
Edgar <edher@tamu.edu>
2020-02-04 19:30:54 -0600

    Base app

---
+++ apps/geoservices-internal-angular/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: 'geoservices-internal-angular',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/geoservices-internal-angular',
+  snapshotSerializers: ['jest-preset-angular/AngularSnapshotSerializer.js', 'jest-preset-angular/HTMLCommentSerializer.js']
+};
