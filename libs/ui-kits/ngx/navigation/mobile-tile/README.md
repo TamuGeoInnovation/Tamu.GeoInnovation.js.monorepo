5c625e6a60e9907fe430a7e9d6d48d729e9f0a4e
Edgar Hernandez <imacleopard2010@gmail.com>
2020-02-24 14:56:19 -0600

    Initial styling and flow

--- libs/ui-kits/ngx/navigation/mobile-tile/README.md
+++ libs/ui-kits/ngx/navigation/mobile-tile/README.md
@@ -5,3 +5,5 @@ This library was generated with [Nx](https://nx.dev).
 ## Running unit tests
 
 Run `nx test ui-kits-ngx-navigation-mobile-tile` to execute the unit tests.
+
+Design inspiration from: https://dribbble.com/shots/4350409-Mobile-Nav-Tiles
--- libs/ui-kits/ngx/navigation/mobile-tile/src/lib/components/tile-navigation/tile-navigation.component.ts
+++ libs/ui-kits/ngx/navigation/mobile-tile/src/lib/components/tile-navigation/tile-navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -8,15 +8,34 @@ import { Observable } from 'rxjs';
 })
 export class TileNavigationComponent implements OnInit {
   @Input()
-  public toggle: Observable<unknown>;
+  public toggle: Observable<boolean>;
+
+  public visible = true;
+
+  public submenuVisible = false;
+
+  @HostBinding('style.display')
+  public get _visible() {
+    return this.visible ? '' : 'none';
+  }
 
   constructor() {}
 
   public ngOnInit() {
     if (this.toggle !== undefined) {
-      this.toggle.subscribe(() => {
-        debugger;
+      this.toggle.subscribe((res) => {
+        this.switchState();
       });
+    } else {
+      console.warn(`No toggle observable for tile navigation provided.`);
     }
+  }
+
+  public switchState() {
+    this.visible = !this.visible;
+  }
+
+  public switchSubMenuState() {
+    this.submenuVisible = !this.submenuVisible;
   }
 }
