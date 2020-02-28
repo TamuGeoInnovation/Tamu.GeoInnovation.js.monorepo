8793cff6e9d88b52d5281932fb9705704286eb76
Edgar Hernandez <imacleopard2010@gmail.com>
2020-02-27 19:57:33 -0600

    Workign on landing page layout for geoservice

--- libs/geoservices/public/src/lib/pages/landing/landing.module.ts
+++ libs/geoservices/public/src/lib/pages/landing/landing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
+import { UILayoutModule } from '@tamu-gisc/ui-kits/ngx/layout';
+
 import { LandingComponent } from './landing.component';
 
 const routes: Routes = [
@@ -12,7 +14,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), UILayoutModule],
   declarations: [LandingComponent]
 })
 export class LandingModule {}
