9e173a24e769db5234545de33eae8158f7e6f6eb
Edgar Hernandez <imacleopard2010@gmail.com>
2020-02-28 18:56:56 -0600

    Added layout inputs to tabs component
    Determine the tab and tab content layout and direction

--- libs/ui-kits/ngx/layout/src/lib/components/tabs/tabs.component.ts
+++ libs/ui-kits/ngx/layout/src/lib/components/tabs/tabs.component.ts
@@ -5,7 +5,9 @@ import {
   ViewChild,
   ViewContainerRef,
   TemplateRef,
-  AfterContentInit
+  AfterContentInit,
+  Input,
+  HostBinding
 } from '@angular/core';
 import { TabComponent } from './tab/tab.component';
 
@@ -15,11 +17,68 @@ import { TabComponent } from './tab/tab.component';
   styleUrls: ['./tabs.component.scss']
 })
 export class TabsComponent implements AfterContentInit {
+  /**
+   * Describes the tab orientation.
+   *
+   * Row: places the tabs along the x-axis.
+   * Column: places the tabs along the y-axis.
+   *
+   * Defaults to `row`.
+   */
+  @Input()
+  public tabLayout: 'row' | 'column' = 'row';
+
+  /**
+   * Describes the tab sizing scaling relationship.
+   *
+   * - Default: Will not apply a sizing scaling factor.
+   * - Contain: Will space out tabs evenly
+   *
+   * Defaults to `default`.
+   */
+  @Input()
+  public tabSizing: 'default' | 'contain' = 'default';
+
+  /**
+   * Describes the placement of tabs vs. content for either direction setting.
+   *
+   * - Forwards: For row directions, sets the tabs above the content. For column directions,
+   * sets the tabs left of the content.
+   * - Reverse:  For row directions, sets the tabs below the content. For column directions,
+   * sets the tabs right of the content.
+   *
+   * Defaults to `forwards`.
+   */
+  @Input()
+  public tabDirection: 'forwards' | 'reverse' = 'forwards';
+
+  /**
+   * Describes the layout direction the tab content.
+   *
+   * - Forwards: Displays as is marked up.
+   * - Reverse: Reverses the display direction relative to how it was marked up.
+   *
+   * Defaults to `forwards`.
+   */
+
+  @Input()
+  public contentDirection: 'forwards' | 'reverse' = 'forwards';
+
   @ViewChild('content', { static: true, read: ViewContainerRef })
-  public contentView: ViewContainerRef;
+  private _contentView: ViewContainerRef;
 
   @ContentChildren(TabComponent)
   private _tabList: QueryList<TabComponent>;
+
+  @HostBinding('class')
+  private get _classBindings() {
+    return [this.tabLayout, this.tabDirection].join(' ');
+  }
+
+  @HostBinding('class.tab-sizing-contain')
+  private get _tabSizingBinding() {
+    return this.tabSizing === 'contain';
+  }
 
   public tabIndex = 0;
 
@@ -44,8 +103,8 @@ export class TabsComponent implements AfterContentInit {
       this.tabIndex = tab.index;
     }
 
-    this.contentView.clear();
-    this.contentView.createEmbeddedView(this.tabs[this.tabIndex].template);
+    this._contentView.clear();
+    this._contentView.createEmbeddedView(this.tabs[this.tabIndex].template);
   }
 }
 
