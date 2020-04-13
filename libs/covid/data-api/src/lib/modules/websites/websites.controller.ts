2951989ea48cd1078a1568ffb99f55d550882df8
Edher <imacleopard2010@gmail.com>
2020-04-13 06:05:35 -0500

    Fixing numbers and website format

--- libs/covid/data-api/src/lib/modules/websites/websites.controller.ts
+++ libs/covid/data-api/src/lib/modules/websites/websites.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 import { CategoryValue } from '@tamu-gisc/covid/common/entities';
 
 import { BaseController } from '../base/base.controller';
@@ -13,5 +13,14 @@ export class WebsitesController extends BaseController<CategoryValue> {
   @Get('county/:fips')
   public async getWebsitesForCounty(@Param() params) {
     return this.service.getWebsitesForCounty(params.fips);
+  }
+
+  @Post('')
+  public async storePhoneNumber(@Body() body) {
+    return {
+      status: 402,
+      success: false,
+      message: 'Not implemented'
+    };
   }
 }
--- libs/covid/data-api/src/lib/modules/websites/websites.service.ts
+++ libs/covid/data-api/src/lib/modules/websites/websites.service.ts
@@ -2,16 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
-import { County, CountyClaim, CategoryValue } from '@tamu-gisc/covid/common/entities';
+import { County, CountyClaim, CategoryValue, CountyClaimInfo } from '@tamu-gisc/covid/common/entities';
 
 import { BaseService } from '../base/base.service';
+import { CATEGORY } from '@tamu-gisc/covid/common/enums';
 
 @Injectable()
 export class WebsitesService extends BaseService<CategoryValue> {
   constructor(
     @InjectRepository(CategoryValue) public repo: Repository<CategoryValue>,
     @InjectRepository(County) public countyRepo: Repository<County>,
-    @InjectRepository(CountyClaim) public claimRepo: Repository<CountyClaim>
+    @InjectRepository(CountyClaim) public claimRepo: Repository<CountyClaim>,
+    @InjectRepository(CountyClaimInfo) public claimInfoRepo: Repository<CountyClaimInfo>
   ) {
     super(repo);
   }
@@ -25,34 +27,21 @@ export class WebsitesService extends BaseService<CategoryValue> {
       };
     }
 
-    // const claim = await this.claimRepo.findOne({
-    //   join: {
-    //     alias: 'claim',
-    //     leftJoinAndSelect: {
-    //       infos: 'claim.infos'
-    //     }
-    //   },
-    //   where: qb => {
-
-    //   },
-    //   // relations: ['info', 'info.websites', 'info.websites.type'],
-    //   order: {
-    //     created: 'DESC'
-    //   }
-    // });
-
-    const claim = await this.claimRepo
-      .createQueryBuilder('claim')
-      .leftJoinAndSelect('infos', 'claim.infos')
-      .where('claim.countyFips = :countyFips', {
-        countyFips: countyFips
+    const claim = await this.claimInfoRepo
+      .createQueryBuilder('info')
+      .innerJoinAndSelect('info.claim', 'claim')
+      .innerJoinAndSelect('info.responses', 'responses')
+      .innerJoinAndSelect('responses.entityValue', 'entityToValue')
+      .innerJoinAndSelect('entityToValue.value', 'value')
+      .innerJoinAndSelect('value.category', 'category')
+      .where('claim.countyFips = :countyFips AND category.id = :type', {
+        countyFips: countyFips,
+        type: CATEGORY.WEBSITES
       })
-      .getMany();
+      .orderBy('claim.created', 'DESC')
+      .getOne();
 
-    debugger;
-
-    // TODO: Get websites for county
-    // return claim && claim.info && claim.info.websites ? claim.info.websites : [];
-    return [];
+    // TODO: Return formatted phone number list, to match the front-end
+    return claim;
   }
 }
