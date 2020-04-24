3dcb1679d34d00db72eb0032f3cb5a142faf2961
Edher <imacleopard2010@gmail.com>
2020-04-24 17:25:02 -0500

    Split up the core geoservices footer
    - Covid website no longer imports the services and geocoding-specific footer items

--- libs/geoservices/core/ngx/src/lib/geoservices-core-ngx.module.ts
+++ libs/geoservices/core/ngx/src/lib/geoservices-core-ngx.module.ts
@@ -10,10 +10,28 @@ import { TestingSiteComponent } from './components/forms/testing-site/testing-si
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { UIFormsModule } from '@tamu-gisc/ui-kits/ngx/forms';
+import { FooterLegalComponent } from './components/footer/legal/legal.component';
+import { FooterShortcutsComponent } from './components/footer/shortcuts/shortcuts.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, UIFormsModule],
-  declarations: [HeaderComponent, FooterComponent, HeaderMobileComponent, HeaderCovidComponent, TestingSiteComponent],
-  exports: [HeaderComponent, FooterComponent, HeaderMobileComponent, HeaderCovidComponent, TestingSiteComponent]
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    HeaderMobileComponent,
+    HeaderCovidComponent,
+    TestingSiteComponent,
+    FooterLegalComponent,
+    FooterShortcutsComponent
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    FooterLegalComponent,
+    FooterShortcutsComponent,
+    HeaderMobileComponent,
+    HeaderCovidComponent,
+    TestingSiteComponent
+  ]
 })
 export class GeoservicesCoreNgxModule {}
