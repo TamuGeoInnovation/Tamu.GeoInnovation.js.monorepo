2a6102cf7b3f144193ebb51df89da94a0432efcb
atharmon <atharmon@tamu.edu>
2020-05-14 18:49:29 -0300

    added two-directory library

---
+++ libs/two/directory/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'two-directory',
+  preset: '../../../jest.config.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../../coverage/libs/two/directory'
+};
