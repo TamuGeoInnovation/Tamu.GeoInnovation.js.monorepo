4278ddd2a8b32a8a0c4b98450e987fe3ab05dd4a
atharmon <atharmon@tamu.edu>
2020-05-15 15:34:16 -0300

    two-directory-nest / two-directory lib working

--- apps/two-directory-nest/src/app/app.module.ts
+++ apps/two-directory-nest/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { DirectoryService } from '@tamu-gisc/two/directory';
+
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DirectoryService],
 })
 export class AppModule {}
