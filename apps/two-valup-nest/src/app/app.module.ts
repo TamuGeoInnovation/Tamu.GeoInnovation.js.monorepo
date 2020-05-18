3c9fef69647587655733f61f0c5cfaeb45fd4fc9
atharmon <atharmon@tamu.edu>
2020-05-18 12:25:00 -0300

    Changed the processes ports

--- apps/two-valup-nest/src/app/app.module.ts
+++ apps/two-valup-nest/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { IrgasonValidationService, FileAccessPipe } from '@tamu-gisc/two/valup';
+
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, IrgasonValidationService]
 })
 export class AppModule {}
