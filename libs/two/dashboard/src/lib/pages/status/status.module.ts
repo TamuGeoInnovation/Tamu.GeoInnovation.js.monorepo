624dca4f30c04f4887ceb7848e19c5a77fecbf2b
atharmon <atharmon@tamu.edu>
2020-05-18 19:24:15 -0300

    Working on the new site

---
+++ libs/two/dashboard/src/lib/pages/status/status.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StatusComponent } from './status.component';
+
+
+
+@NgModule({
+  declarations: [StatusComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class StatusModule { }
