a5cac06c87a31ecbeea8b550932281837e85a443
atharmon <atharmon@tamu.edu>
2020-05-19 10:59:08 -0300

    Styles and whatnot applied

--- libs/two/dashboard/src/lib/pages/status/status.module.ts
+++ libs/two/dashboard/src/lib/pages/status/status.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+
+import { UILayoutModule } from '@tamu-gisc/ui-kits/ngx/layout';
+import { GeoservicesCoreNgxModule } from '@tamu-gisc/geoservices/core/ngx';
 import { StatusComponent } from './status.component';
 
 const routes: Routes = [
@@ -14,7 +17,9 @@ const routes: Routes = [
   declarations: [StatusComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    UILayoutModule,
+    GeoservicesCoreNgxModule, 
   ]
 })
 export class StatusModule { }
