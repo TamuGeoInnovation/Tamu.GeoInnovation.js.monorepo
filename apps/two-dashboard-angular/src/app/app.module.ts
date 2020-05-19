c354ae8f3db49ddcd0bd81f0048b31264085c9a5
atharmon <atharmon@tamu.edu>
2020-05-19 10:52:30 -0300

    Routing is finally set

--- apps/two-dashboard-angular/src/app/app.module.ts
+++ apps/two-dashboard-angular/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HighlightPlusModule } from 'ngx-highlightjs/plus';
 import { EnvironmentModule, env } from '@tamu-gisc/common/ngx/environment';
 import { LocalStoreModule } from '@tamu-gisc/common/ngx/local-store';
+import { HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import * as WebFont from 'webfontloader';
 
 import { AppComponent } from './app.component';
 import { AuthService } from '@tamu-gisc/geoservices/data-access';
+import * as environment from '../environments/environment';
 
 const routes: Routes = [
   {
@@ -17,6 +20,20 @@ const routes: Routes = [
   }
 ];
 
+WebFont.load({
+  google: {
+    families: ['Material Icons', 'Ubuntu:300,400,500,600', 'Muli:300,400']
+  }
+});
+
+export function getHighlightLanguages() {
+  return {
+    xml: () => import('highlight.js/lib/languages/xml'),
+    json: () => import('highlight.js/lib/languages/json'),
+    javascript: () => import('highlight.js/lib/languages/javascript')
+  };
+}
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -24,10 +41,23 @@ const routes: Routes = [
     RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled', relativeLinkResolution: 'corrected' }),
     BrowserAnimationsModule,
     HighlightPlusModule,
-    // EnvironmentModule,
+    EnvironmentModule,
     LocalStoreModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    {
+      provide: HIGHLIGHT_OPTIONS,
+      useValue: {
+        languages: getHighlightLanguages(),
+        lineNumbers: true
+      }
+    },
+    {
+      provide: env,
+      useValue: environment
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- libs/two/dashboard/src/lib/pages/status/status.module.ts
+++ libs/two/dashboard/src/lib/pages/status/status.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 import { StatusComponent } from './status.component';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: StatusComponent
+  }
+];
 
 @NgModule({
   declarations: [StatusComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class StatusModule { }
