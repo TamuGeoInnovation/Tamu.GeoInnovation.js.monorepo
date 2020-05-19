88eebe2d1c164fd49fd8235589e0933b2fdb6338
atharmon <atharmon@tamu.edu>
2020-05-19 11:37:50 -0300

    Updating the StatusComponent's html

---
+++ libs/two/dashboard/src/lib/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'two-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class TWOHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
