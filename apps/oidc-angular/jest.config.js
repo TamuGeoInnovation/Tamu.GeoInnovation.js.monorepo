0b3bb04bb03d2ff678353c9b48c73cc3e55ab666
Edher <imacleopard2010@gmail.com>
2020-05-22 02:05:08 -0500

    Base angular application

---
+++ apps/oidc-angular/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: 'oidc-angular',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/oidc-angular',
+  snapshotSerializers: ['jest-preset-angular/AngularSnapshotSerializer.js', 'jest-preset-angular/HTMLCommentSerializer.js']
+};
