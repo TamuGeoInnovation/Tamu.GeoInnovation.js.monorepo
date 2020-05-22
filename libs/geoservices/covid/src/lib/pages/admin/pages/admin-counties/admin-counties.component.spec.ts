7581427aac3702a558eba96a590f5b1b6d1f5084
Isaiah Villagomez <itv66@tamu.edu>
2020-05-21 22:42:01 -0500

    Progresses on scafolding

--- libs/geoservices/covid/src/lib/pages/admin/pages/admin-counties/admin-counties.component.spec.ts
+++ libs/geoservices/covid/src/lib/pages/admin/pages/admin-counties/admin-counties.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdminCountiesComponent } from './admin-counties.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { EnvironmentModule, env } from '@tamu-gisc/common/ngx/environment';
 
 describe('AdminCountiesComponent', () => {
   let component: AdminCountiesComponent;
@@ -8,15 +10,24 @@ describe('AdminCountiesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, EnvironmentModule],
+      providers: [
+        {
+          provide: env, 
+          useValue: { covid_api_url : 'https://' }
+        }
+      ],
       declarations: [ AdminCountiesComponent ]
     })
     .compileComponents();
   }));
-
+ 
   beforeEach(() => {
+    
     fixture = TestBed.createComponent(AdminCountiesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    
   });
 
   it('should create', () => {
--- libs/geoservices/data-access/src/lib/guards/auth.guard.spec.ts
+++ libs/geoservices/data-access/src/lib/guards/auth.guard.spec.ts
@@ -1,15 +1,29 @@
 import { TestBed, async, inject } from '@angular/core/testing';
 
 import { AuthGuard } from './auth.guard';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { EnvironmentModule, env } from '@tamu-gisc/common/ngx/environment';
+import { SettingsModule } from '@tamu-gisc/common/ngx/settings';
 
 describe('AuthGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthGuard]
+      imports: [HttpClientTestingModule, RouterTestingModule, EnvironmentModule, SettingsModule],
+      providers: [
+        AuthGuard,
+        {
+          provide: env,
+          useValue: { api_url: 'api' }
+        }
+      ]
+      
     });
   });
 
   it('should ...', inject([AuthGuard], (guard: AuthGuard) => {
     expect(guard).toBeTruthy();
   }));
+
 });
+ 
