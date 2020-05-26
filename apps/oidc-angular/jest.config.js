75d3fe515875f5e1afee6cbee98016c0ac1ac909
Edher <imacleopard2010@gmail.com>
2020-05-26 14:33:59 -0500

    Removed manual oidc-express and oidc-angular
    Will be replaced by auto-generated projects

--- apps/oidc-angular/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  name: 'oidc-angular',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/oidc-angular',
-  snapshotSerializers: ['jest-preset-angular/AngularSnapshotSerializer.js', 'jest-preset-angular/HTMLCommentSerializer.js']
-};
--- apps/oidc-express/jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  name: 'oidc-express',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/oidc-express'
-};
