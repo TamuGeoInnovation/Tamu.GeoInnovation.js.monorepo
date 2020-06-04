c38e265fb2faa81486472cf97c6ac3a7fa14ffea
Edher <imacleopard2010@gmail.com>
2020-06-04 17:36:50 -0500

    Ignoring some custom types for the draggable directive

--- apps/aggiemap-angular/src/environments/environment.ts
+++ apps/aggiemap-angular/src/environments/environment.ts
@@ -14,6 +14,8 @@ import { Connections, Definitions as d } from './definitions';
 
 import { Popups } from '@tamu-gisc/aggiemap';
 
+import esri = __esri;
+
 export * from './definitions';
 export * from './notification-events';
 export * from './polygons';
@@ -22,7 +24,7 @@ const commonLayerProps = {
   outFields: ['*'],
   minScale: 100000,
   maxScale: 0,
-  elevationInfo: { mode: 'relative-to-ground', offset: 1 },
+  elevationInfo: { mode: 'relative-to-ground', offset: 1 } as esri.FeatureLayerElevationInfo,
   popupEnabled: false
 };
 
