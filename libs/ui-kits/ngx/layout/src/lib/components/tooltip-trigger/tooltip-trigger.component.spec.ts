ea10cf801e1f87978b8e71c32b48f3606f75c094
Isaiah Villagomez <itv66@tamu.edu>
2020-06-24 17:15:51 -0500

    Working on Unit-Testing, Commiting before Syncing with Upstream

--- libs/ui-kits/ngx/layout/src/lib/components/tooltip-trigger/tooltip-trigger.component.spec.ts
+++ libs/ui-kits/ngx/layout/src/lib/components/tooltip-trigger/tooltip-trigger.component.spec.ts
@@ -5,21 +5,33 @@ import { TooltipTriggerComponent } from './tooltip-trigger.component';
 describe('TooltipTriggerComponent', () => {
   let component: TooltipTriggerComponent;
   let fixture: ComponentFixture<TooltipTriggerComponent>;
+  let inputEl: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TooltipTriggerComponent ]
-    })
-    .compileComponents();
+      declarations: [TooltipTriggerComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TooltipTriggerComponent);
     component = fixture.componentInstance;
+    inputEl = fixture.nativeElement.querySelector('button');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should repsond to Mouse Event', () => {
+    const yeet = spyOn(component.triggerActivate, 'emit');
+    const e: MouseEvent = new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+      view: window
+    });
+    component.click(e);
+
+    expect(yeet).toHaveBeenCalled();
   });
 });
