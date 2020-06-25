cc813f12e61f976bcaa0612300dfcc332d9b717a
atharmon <atharmon@tamu.edu>
2020-06-25 14:39:52 -0300

    Fixed module/provider issue

--- libs/oidc/provider-nest/src/lib/modules/interaction/interaction.module.ts
+++ libs/oidc/provider-nest/src/lib/modules/interaction/interaction.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { InteractionController } from '../../controllers/interaction/interaction.controller';
+import { UserService } from '../../services/user/user.service';
 
 @Module({
     imports: [],
     controllers: [InteractionController],
-    providers: [],
+    providers: [UserService],
     exports: [],
 })
 export class InteractionModule {}
