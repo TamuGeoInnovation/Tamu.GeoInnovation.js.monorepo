69d5bdcd6453b779c91582a768115d73edc5e385
Isaiah Villagomez <itv66@tamu.edu>
2020-06-29 12:04:24 -0500

    Working on attempting to test first Directive, and progressing on Component testing

--- libs/geoservices/covid/src/lib/pages/maps/maps.component.spec.ts
+++ libs/geoservices/covid/src/lib/pages/maps/maps.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MapsComponent } from './maps.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('MapsComponent', () => {
   let component: MapsComponent;
@@ -8,6 +9,7 @@ describe('MapsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [MapsComponent]
     }).compileComponents();
   }));
