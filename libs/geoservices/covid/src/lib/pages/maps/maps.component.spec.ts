d675c97aebd9a56df6fea964673909d5c431f835
Isaiah Villagomez <itv66@tamu.edu>
2020-06-29 12:04:24 -0500

    Working on attempting to test first Directive, and progressing on Component testing

--- libs/geoservices/covid/src/lib/pages/maps/maps.component.spec.ts
+++ libs/geoservices/covid/src/lib/pages/maps/maps.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { MapsComponent } from './maps.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('MapsComponent', () => {
   let component: MapsComponent;
--- libs/ui-kits/ngx/interactions/scroll-to/src/lib/directives/scroll-to.directive.spec.ts
+++ libs/ui-kits/ngx/interactions/scroll-to/src/lib/directives/scroll-to.directive.spec.ts
@@ -1,8 +1,31 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { ScrollToDirective } from './scroll-to.directive';
+import { Component, DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+/* WORK IN PROGRESS */
+
+/* Dummy Testing Component */
+@Component({
+  template: `
+    <input type="text" scrollTo />
+  `
+})
+class TestScrollToComponent {}
 
 describe('ScrollToDirective', () => {
+  let component: TestScrollToComponent;
+  let fixture: ComponentFixture<TestScrollToComponent>;
+  describe('Directive: ScrollToDirective', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        declarations: [ScrollToDirective, TestScrollToComponent]
+      });
+      fixture = TestBed.createComponent(TestScrollToComponent);
+      component = fixture.componentInstance;
+    });
+  });
   it('should create an instance', () => {
-    const directive = new ScrollToDirective();
-    expect(directive).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 });
