245bde2334eafe432f0d3d86175a2e2fb388cfb2
Edher <imacleopard2010@gmail.com>
2020-06-30 11:45:25 -0500

    Tweaked panels, changed language

--- libs/cpa/common/ngx/src/lib/modules/create/create.module.ts
+++ libs/cpa/common/ngx/src/lib/modules/create/create.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
         path: '',
         component: OverviewComponent,
         data: {
-          breadcrumb: 'Builder'
+          breadcrumb: 'Home'
         }
       },
       {
