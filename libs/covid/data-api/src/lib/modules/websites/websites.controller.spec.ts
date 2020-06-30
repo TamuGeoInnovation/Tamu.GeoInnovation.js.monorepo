d0cf77ce82a22822bf82a82baeb86275bbf0dba1
Isaiah Villagomez <itv66@tamu.edu>
2020-06-30 15:45:19 -0500

    Progressing on nestJS controller coverage

--- libs/covid/data-api/src/lib/modules/websites/websites.controller.spec.ts
+++ libs/covid/data-api/src/lib/modules/websites/websites.controller.spec.ts
@@ -1,32 +1,55 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { getRepositoryToken } from '@nestjs/typeorm';
-
-import { Repository } from 'typeorm';
-
-import { County, CountyClaim, CategoryValue, CountyClaimInfo } from '@tamu-gisc/covid/common/entities';
 
 import { WebsitesController } from './websites.controller';
 import { WebsitesService } from './websites.service';
 
+jest.mock('../websites/websites.service');
+
 describe('Websites Controller', () => {
+  let service: WebsitesService;
+  let module: TestingModule;
   let controller: WebsitesController;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        WebsitesService,
-        { provide: getRepositoryToken(CategoryValue), useClass: Repository },
-        { provide: getRepositoryToken(County), useClass: Repository },
-        { provide: getRepositoryToken(CountyClaim), useClass: Repository },
-        { provide: getRepositoryToken(CountyClaimInfo), useClass: Repository }
-      ],
+    module = await Test.createTestingModule({
+      providers: [WebsitesService],
       controllers: [WebsitesController]
     }).compile();
-
+    service = module.get<WebsitesService>(WebsitesService);
     controller = module.get<WebsitesController>(WebsitesController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('Get Websites for County', () => {
+    it('should return expected Result', async () => {
+      const expectedResult = { status: 0, success: true, message: 'yeet' };
+      const mockEmailToSatisfyParameters = 'yeet';
+      jest.spyOn(service, 'getWebsitesForCounty').mockResolvedValue(expectedResult);
+      expect(await controller.getWebsitesForCounty(mockEmailToSatisfyParameters)).toBe(expectedResult);
+    });
+  });
+
+  describe('Get Websites for Claim Info', () => {
+    it('should return expected Result', async () => {
+      const expectedResult = [];
+      const mockEmailToSatisfyParameters = 'yeet';
+      jest.spyOn(service, 'getWebsitesForClaimInfo').mockResolvedValue(expectedResult);
+      expect(await controller.getWebsitesForClaimInfo(mockEmailToSatisfyParameters)).toBe(expectedResult);
+    });
+  });
+
+  describe('Store Phone Number', () => {
+    it('should throw error', async (done) => {
+      await controller
+        .storePhoneNumber(undefined)
+        .then(() => done.fail(''))
+        .catch((error) => {
+          expect(error.message).toStrictEqual({ message: 'Not implemented', status: 501, success: false });
+          done();
+        });
+    });
   });
 });
--- libs/geoservices/covid/src/lib/pages/maps/maps.component.spec.ts
+++ libs/geoservices/covid/src/lib/pages/maps/maps.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 
 import { MapsComponent } from './maps.component';
 import { RouterTestingModule } from '@angular/router/testing';
