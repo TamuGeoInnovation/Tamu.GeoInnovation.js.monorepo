e07268f9f6fbb97cb8aba77b5a6e933b7d224915
Isaiah Villagomez <itv66@tamu.edu>
2020-06-30 18:32:02 -0500

    NestJS Controller Testing

--- libs/covid/data-api/src/lib/modules/field-categories/field-categories.controller.spec.ts
+++ libs/covid/data-api/src/lib/modules/field-categories/field-categories.controller.spec.ts
@@ -1,31 +1,77 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { getRepositoryToken } from '@nestjs/typeorm';
-
-import { Repository } from 'typeorm';
-
-import { FieldCategory, FieldType, CategoryValue } from '@tamu-gisc/covid/common/entities';
 
 import { FieldCategoriesController } from './field-categories.controller';
 import { FieldCategoriesService } from './field-categories.service';
+import { FieldCategory } from '@tamu-gisc/covid/common/entities';
+
+jest.mock('./field-categories.service');
 
 describe('FieldCategories Controller', () => {
+  let service: FieldCategoriesService;
   let controller: FieldCategoriesController;
+  let module: TestingModule;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        FieldCategoriesService,
-        { provide: getRepositoryToken(FieldCategory), useClass: Repository },
-        { provide: getRepositoryToken(FieldType), useClass: Repository },
-        { provide: getRepositoryToken(CategoryValue), useClass: Repository }
-      ],
+    module = await Test.createTestingModule({
+      providers: [FieldCategoriesService],
       controllers: [FieldCategoriesController]
     }).compile();
-
+    service = module.get<FieldCategoriesService>(FieldCategoriesService);
     controller = module.get<FieldCategoriesController>(FieldCategoriesController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('getFieldTypesForCategory', () => {
+    it('should return expected Result', async () => {
+      const expectedValue = new FieldCategory();
+      jest.spyOn(service, 'getFieldTypesForCategory').mockResolvedValue(expectedValue);
+      expect(await controller.getFieldTypesForCategory('foo')).toMatchObject(expectedValue);
+    });
+  });
+
+  describe('addFieldTypeToCategory', () => {
+    it('should return expected Result', async () => {
+      const expectedValue = new FieldCategory();
+      jest.spyOn(service, 'addFieldTypeToCategory').mockResolvedValue(expectedValue);
+      expect(await controller.addFieldTypeToCategory('foo', 'bar')).toMatchObject(expectedValue);
+    });
+  });
+
+  describe('getCategoryValues', () => {
+    it('should return expected Result', async () => {
+      const expectedValue = [new FieldCategory()];
+      jest.spyOn(service, 'getCategoryWithValues').mockResolvedValue(expectedValue);
+      expect(await controller.getCategoryValues('foo')).toMatchObject(expectedValue);
+    });
+  });
+
+  describe('addValueToType', () => {
+    it('should return expected Result', async () => {
+      const expectedValue = new FieldCategory();
+      jest.spyOn(service, 'addValueToCategory').mockResolvedValue(expectedValue);
+      expect(await controller.addValueToType('foo', 'bar')).toMatchObject(expectedValue);
+    });
+  });
+  describe('getCategories', () => {
+    it('should return expected Result', async () => {
+      const expectedValue = [new FieldCategory()];
+      jest.spyOn(service, 'getAllCategoriesWithTypes').mockResolvedValue(expectedValue);
+      expect(await controller.getCategories()).toMatchObject(expectedValue);
+    });
+  });
+
+  describe('deleteCategory', () => {
+    it('should return expected Result', async () => {
+      const expectedValue = {
+        status: 501,
+        success: false,
+        message: 'Not implemented'
+      };
+
+      expect(await controller.deleteCategory()).toMatchObject(expectedValue);
+    });
   });
 });
