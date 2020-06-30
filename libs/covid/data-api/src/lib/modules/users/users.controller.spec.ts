14d1c76ca3b930564d1fb10b56887421325ff6e1
Isaiah Villagomez <itv66@tamu.edu>
2020-06-30 11:50:54 -0500

    Working with nestJS Modules

--- libs/covid/data-api/src/lib/modules/users/users.controller.spec.ts
+++ libs/covid/data-api/src/lib/modules/users/users.controller.spec.ts
@@ -1,30 +1,70 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { getRepositoryToken } from '@nestjs/typeorm';
-
-import { Repository } from 'typeorm';
-
-import { User, TestingSite } from '@tamu-gisc/covid/common/entities';
 
 import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import { UsersService } from '../users/users.service';
+import { User } from '@tamu-gisc/covid/common/entities';
+
+jest.mock('../users/users.service');
 
 describe('Users Controller', () => {
+  let usersService: UsersService;
+  let module: TestingModule;
   let controller: UsersController;
-
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        UsersService,
-        { provide: getRepositoryToken(User), useClass: Repository },
-        { provide: getRepositoryToken(TestingSite), useClass: Repository }
-      ],
-      controllers: [UsersController]
+    module = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [UsersService]
     }).compile();
-
+    usersService = module.get<UsersService>(UsersService);
     controller = module.get<UsersController>(UsersController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  afterEach(() => {
+    jest.resetAllMocks();
   });
+
+  describe('Verify Email', () => {
+    it('should return expected Result', async () => {
+      const expectedResult = {
+        statusCode: 500,
+        success: false,
+        message: 'Input parameter missing'
+      };
+      const mockEmailToSatisfyParameters = 'yeet';
+      jest.spyOn(usersService, 'verifyEmail').mockResolvedValue(expectedResult);
+      expect(await controller.verifyEmail(mockEmailToSatisfyParameters)).toBe(expectedResult);
+    });
+  });
+
+  describe('Register Email', () => {
+    it('should return expected Result', async () => {
+      const expectedResult = {
+        status: 500,
+        success: false,
+        message: 'Input parameter missing'
+      };
+      const mockEmailToSatisfyParameters = 'yeet';
+      jest.spyOn(usersService, 'registerEmail').mockResolvedValue(expectedResult);
+      expect(await controller.registerEmail(mockEmailToSatisfyParameters)).toBe(expectedResult);
+    });
+  });
+  /*describe('Get Users', () => {
+    it('should return expected Result', async () => {
+      const expectedResult 
+      const mockEmailToSatisfyParameters = 'yeet';
+      jest.spyOn(usersService, 'getUsers').mockResolvedValue(expectedResult);
+      expect(await controller.verifyEmail(mockEmailToSatisfyParameters)).toBe(expectedResult);
+    });
+
+    it('should catch error, return expected Result', async () => {
+      const expectedResult = {
+        statusCode: 500,
+        success: false,
+        message: 'Input parameter missing'
+      };
+      const mockEmailToSatisfyParameters = 'yeet';
+      jest.spyOn(usersService, 'verifyEmail').mockResolvedValue(expectedResult);
+      expect(await controller.verifyEmail(mockEmailToSatisfyParameters)).toBe(expectedResult);
+    });
+  });*/
 });
