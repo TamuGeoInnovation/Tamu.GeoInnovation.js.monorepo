fc4a9ff8151c0b9f1c59d9e2b45ad6a67d52cea9
Isaiah Villagomez <itv66@tamu.edu>
2020-07-02 18:54:40 -0500

    Workin

--- libs/covid/data-api/src/lib/modules/field-categories/field-categories.service.spec.ts
+++ libs/covid/data-api/src/lib/modules/field-categories/field-categories.service.spec.ts
@@ -9,21 +9,79 @@ import { FieldCategoriesService } from './field-categories.service';
 
 describe('FieldCategoriesService', () => {
   let service: FieldCategoriesService;
-
+  let FieldCategoryMockRepo: MockType<Repository<FieldCategory>>;
+  let FieldTypeMockRepo: MockType<Repository<FieldType>>;
+  let CategoryValueMockRepo: MockType<Repository<CategoryValue>>;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         FieldCategoriesService,
-        { provide: getRepositoryToken(FieldCategory), useClass: Repository },
-        { provide: getRepositoryToken(FieldType), useClass: Repository },
-        { provide: getRepositoryToken(CategoryValue), useClass: Repository }
+        { provide: getRepositoryToken(FieldCategory), useFactory: repositoryMockFactory },
+        { provide: getRepositoryToken(FieldType), useFactory: repositoryMockFactory },
+        { provide: getRepositoryToken(CategoryValue), useFactory: repositoryMockFactory }
       ]
     }).compile();
 
     service = module.get<FieldCategoriesService>(FieldCategoriesService);
+    FieldCategoryMockRepo = module.get(getRepositoryToken(FieldCategory));
+    FieldTypeMockRepo = module.get(getRepositoryToken(FieldType));
+    CategoryValueMockRepo = module.get(getRepositoryToken(CategoryValue));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('getFieldTypesForCategory', () => {
+    it('should handle undefined inputs ', async () => {
+      expect(await service.getFieldTypesForCategory(undefined)).toMatchObject({
+        status: 400,
+        success: false,
+        message: 'Input parameter missing.'
+      });
+    });
+    it('should handle catagorey inputs ', async () => {
+      FieldCategoryMockRepo.findOne.mockReturnValue({});
+      expect(await service.getFieldTypesForCategory(9)).toMatchObject({});
+    });
+
+    it('should handle catagorey inputs ', async () => {
+      FieldCategoryMockRepo.findOne.mockReturnValue(undefined);
+      expect(await service.getFieldTypesForCategory(9)).toMatchObject({
+        status: 400,
+        success: false,
+        message: 'Invalid category ID.'
+      });
+    });
+  });
+
+  describe('addFieldTypeToCategory', () => {
+    it('should handle undefined inputs ', async () => {
+      expect(await service.getFieldTypesForCategory(undefined)).toMatchObject({
+        status: 400,
+        success: false,
+        message: 'Input parameter missing.'
+      });
+    });
+    it('should handle catagorey inputs ', async () => {
+      FieldCategoryMockRepo.findOne.mockReturnValue({});
+      expect(await service.getFieldTypesForCategory(9)).toMatchObject({});
+    });
+
+    it('should handle catagorey inputs ', async () => {
+      FieldCategoryMockRepo.findOne.mockReturnValue(undefined);
+      expect(await service.getFieldTypesForCategory(9)).toMatchObject({
+        status: 400,
+        success: false,
+        message: 'Invalid category ID.'
+      });
+    });
   });
 });
+
+// @ts-ignore
+export const repositoryMockFactory: () => MockType<Repository<any>> = jest.fn(() => ({
+  findOne: jest.fn(),
+  find: jest.fn(),
+  update: jest.fn(),
+  save: jest.fn()
+}));
+export type MockType<T> = {
+  [P in keyof T]: jest.Mock<{}>;
+};
