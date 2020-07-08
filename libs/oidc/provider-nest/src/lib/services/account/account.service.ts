b337855b339509b43d4b9ad0f301db38ee573c75
atharmon <atharmon@tamu.edu>
2020-07-08 18:19:42 -0300

    User can now get role claim

--- libs/oidc/provider-nest/src/lib/services/account/account.service.ts
+++ libs/oidc/provider-nest/src/lib/services/account/account.service.ts
@@ -1,3 +1,19 @@
+import { getConnection } from 'typeorm';
 import { CommonService } from '../common/common.service';
+import { User } from '../../entities/all.entity';
 
-export class StaticAccountService extends CommonService {}
+export class StaticAccountService extends CommonService {
+  public static async getUserRoles(accountGuid: string) {
+    return getConnection()
+      .getRepository(User)
+      .createQueryBuilder('user')
+      .leftJoinAndSelect('user.account', 'account')
+      .leftJoinAndSelect('user.userRoles', 'userRoles')
+      .leftJoinAndSelect('userRoles.role', 'role')
+      .leftJoinAndSelect('userRoles.client', 'client')
+      .where(`account.guid = :accountGuid`, {
+        accountGuid: accountGuid
+      })
+      .getOne();
+  }
+}
