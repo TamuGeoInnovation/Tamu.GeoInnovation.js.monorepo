9ac0735139982b6aac13d600514710d9a5f38162
atharmon <atharmon@tamu.edu>
2020-07-09 16:28:31 -0300

    Role for the current clientId attached to claims

--- libs/oidc/provider-nest/src/lib/services/account/account.service.ts
+++ libs/oidc/provider-nest/src/lib/services/account/account.service.ts
@@ -3,7 +3,7 @@ import { CommonService } from '../common/common.service';
 import { User } from '../../entities/all.entity';
 
 export class StaticAccountService extends CommonService {
-  public static async getUserRoles(accountGuid: string) {
+  public static async getUserRoles(accountGuid: string, clientName: string) {
     return getConnection()
       .getRepository(User)
       .createQueryBuilder('user')
@@ -11,8 +11,9 @@ export class StaticAccountService extends CommonService {
       .leftJoinAndSelect('user.userRoles', 'userRoles')
       .leftJoinAndSelect('userRoles.role', 'role')
       .leftJoinAndSelect('userRoles.client', 'client')
-      .where(`account.guid = :accountGuid`, {
-        accountGuid: accountGuid
+      .where(`account.guid = :accountGuid AND client.clientName = :clientName`, {
+        accountGuid: accountGuid,
+        clientName: clientName
       })
       .getOne();
   }
