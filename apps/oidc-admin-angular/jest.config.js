142d6ac8e3bf045f61f168d7d91e0da99d23ba37
atharmon <atharmon@tamu.edu>
2020-08-11 18:04:48 -0300

    Creation of angular + nest apps

---
+++ apps/oidc-admin-angular/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: 'oidc-admin-angular',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/oidc-admin-angular',
+  snapshotSerializers: ['jest-preset-angular/AngularSnapshotSerializer.js', 'jest-preset-angular/HTMLCommentSerializer.js']
+};
