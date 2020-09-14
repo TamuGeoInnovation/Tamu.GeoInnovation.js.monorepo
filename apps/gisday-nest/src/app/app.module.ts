cdd6854cb44c34a079a83bdc0a670c8abd9d0b37
atharmon <atharmon@tamu.edu>
2020-09-14 18:06:36 -0300

    gisday-nest lib added

--- apps/gisday-nest/src/app/app.module.ts
+++ apps/gisday-nest/src/app/app.module.ts
@@ -1,10 +1,36 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { EventController, EventModule } from '@tamu-gisc/gisday/nest';
+import { OidcClientModule, OidcClientController, ClaimsMiddleware } from '@tamu-gisc/oidc/client';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OIDC_ISSUER } from '../environments/oidc-client-config';
+import { localDbConfig } from '../environments/ormconfig';
 
 @Module({
-  imports: [],
+  imports: [
+    // OidcClientModule.forRoot({
+    //   host: OIDC_ISSUER
+    // }),
+    TypeOrmModule.forRoot({
+      ...localDbConfig,
+      entities: []
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  public configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(ClaimsMiddleware)
+      .exclude(
+        { path: 'oidc/login', method: RequestMethod.GET },
+        { path: 'oidc/logout', method: RequestMethod.GET },
+        { path: 'oidc/auth/callback', method: RequestMethod.GET }
+      )
+      .forRoutes(OidcClientController);
+  }
+}
---
+++ libs/gisday/nest/src/lib/gisday-nest.ts
@@ -0,0 +1,5 @@
+// Controllers
+export * from './controllers/event/event.controller';
+
+// Modules
+export * from './modules/event/event.module';
