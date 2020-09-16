4d84fbb0e2cb11e8f90d8b6737ea8f07fefaf32f
atharmon <atharmon@tamu.edu>
2020-09-16 14:00:29 -0300

    Setup controller / provider for each entity

--- libs/gisday/nest/src/lib/controllers/tag/tag.controller.ts
+++ libs/gisday/nest/src/lib/controllers/tag/tag.controller.ts
@@ -1,4 +1,12 @@
 import { Controller } from '@nestjs/common';
 
+import { Tag } from '../../entities/all.entity';
+import { TagProvider } from '../../providers/tag/tag.provider';
+import { BaseController } from '../../controllers/_base/base.controller';
+
 @Controller('tag')
-export class TagController {}
+export class TagController extends BaseController<Tag> {
+  constructor(private readonly tagProvider: TagProvider) {
+    super(tagProvider);
+  }
+}
--- libs/gisday/nest/src/lib/providers/speaker/speaker.provider.ts
+++ libs/gisday/nest/src/lib/providers/speaker/speaker.provider.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
+import { Speaker, SpeakerRepo } from '../../entities/all.entity';
+import { BaseProvider } from '../../providers/_base/base-provider';
 @Injectable()
-export class SpeakerProvider {}
+export class SpeakerProvider extends BaseProvider<Speaker> {
+  constructor(private readonly speakerRepo: SpeakerRepo) {
+    super(speakerRepo);
+  }
+}
