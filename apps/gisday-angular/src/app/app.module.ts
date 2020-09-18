29b9282c8e36952926b0b5234a3b9e95d2bcf643
Edher <imacleopard2010@gmail.com>
2020-09-18 10:33:10 -0500

    Routing changes

--- apps/gisday-angular/src/app/app.module.ts
+++ apps/gisday-angular/src/app/app.module.ts
@@ -3,28 +3,29 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { FooterModule, HeaderModule } from '@tamu-gisc/gisday/angular';
+import { EnvironmentModule, env } from '@tamu-gisc/common/ngx/environment';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+
+import * as environment from '../environments/environment';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('@tamu-gisc/gisday/angular').then((m) => m.GISDayAngularModule)
-  },
-  {
-    path: 'sessions',
-    loadChildren: () => import('@tamu-gisc/gisday/angular').then((m) => m.SessionsModule)
-  },
-  {
-    path: 'faq',
-    loadChildren: () => import('@tamu-gisc/gisday/angular').then((m) => m.SessionsModule)
   }
 ];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule.forRoot(routes), FooterModule, HeaderModule],
-  providers: [],
+  imports: [BrowserModule, RouterModule.forRoot(routes), FooterModule, HeaderModule, EnvironmentModule, HttpClientModule],
+  providers: [
+    {
+      provide: env,
+      useValue: environment
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
