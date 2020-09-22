c60970d2c823bb95dbe9fb9a1f57183e7aef6bf8
atharmon <atharmon@tamu.edu>
2020-09-22 18:42:07 -0300

    Added sponsor module / missing sponsor logos

--- libs/gisday/ngx/src/index.ts
+++ libs/gisday/ngx/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/gisday-ngx.module';
 export * from './lib/pages/landing/landing.module';
 export * from './lib/pages/sessions/sessions.module';
 export * from './lib/pages/faq/faq.module';
+export * from './lib/pages/sponsors/sponsors.module';
