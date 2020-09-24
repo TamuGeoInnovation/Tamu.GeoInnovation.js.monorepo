bf9be835ff5c157a21159e408b4ee2bf736b2dd5
atharmon <atharmon@tamu.edu>
2020-09-24 20:04:43 -0300

    Storing speaker photos works

--- libs/gisday/data-api/src/lib/providers/speaker/speaker.provider.ts
+++ libs/gisday/data-api/src/lib/providers/speaker/speaker.provider.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
-import { Speaker, SpeakerRepo } from '../../entities/all.entity';
+import { Speaker, SpeakerPhoto, SpeakerRepo, SpeakerPhotoRepo } from '../../entities/all.entity';
 import { BaseProvider } from '../../providers/_base/base-provider';
 @Injectable()
 export class SpeakerProvider extends BaseProvider<Speaker> {
-  constructor(private readonly speakerRepo: SpeakerRepo) {
+  constructor(private readonly speakerRepo: SpeakerRepo, public readonly speakerPhotoRepo: SpeakerPhotoRepo) {
     super(speakerRepo);
+  }
+
+  async insertPhoto(speakerGuid: string, file: any) {
+    const _photo: Partial<SpeakerPhoto> = {
+      speakerGuid: speakerGuid,
+      blob: file.buffer
+    };
+    const photo = await this.speakerPhotoRepo.create(_photo);
+    debugger;
+    return this.speakerPhotoRepo.insert(photo);
   }
 }
