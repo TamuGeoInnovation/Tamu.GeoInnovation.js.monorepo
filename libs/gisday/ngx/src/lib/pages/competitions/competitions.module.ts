ffd5ce5e4318ebca302261a7ac686f1e0713a5e7
atharmon <atharmon@tamu.edu>
2020-09-24 18:02:45 -0300

    Stormwater added

--- libs/gisday/ngx/src/lib/pages/competitions/competitions.module.ts
+++ libs/gisday/ngx/src/lib/pages/competitions/competitions.module.ts
@@ -9,6 +9,11 @@ import { PostersComponent } from './posters/posters.component';
 import { VgiComponent } from './vgi/vgi.component';
 
 import { SignageComponent } from './signage/signage.component';
+import { StormwaterComponent } from './stormwater/stormwater.component';
+import { SidewalkComponent } from './sidewalk/sidewalk.component';
+import { BuildingBountyComponent } from './building-bounty/building-bounty.component';
+import { AggieAccessibilityComponent } from './aggie-accessibility/aggie-accessibility.component';
+import { ManholeMappingComponent } from './manhole-mapping/manhole-mapping.component';
 
 const routes: Routes = [
   {
@@ -34,36 +39,41 @@ const routes: Routes = [
   {
     path: 'signage',
     component: SignageComponent
+  },
+  {
+    path: 'stormwater',
+    component: StormwaterComponent
+  },
+  {
+    path: 'sidewalk',
+    component: SidewalkComponent
+  },
+  {
+    path: 'building-bounty',
+    component: BuildingBountyComponent
+  },
+  {
+    path: 'aggie-accessibility',
+    component: AggieAccessibilityComponent
+  },
+  {
+    path: 'manhole',
+    component: ManholeMappingComponent
   }
-  // {
-  //   path: 'stormwater',
-  //   component: SignageComponent
-  // },
-  // {
-  //   path: 'sidewalk',
-  //   component: SignageComponent
-  // },
-  // {
-  //   path: 'building-bounty',
-  //   component: SignageComponent
-  // },
-  // {
-  //   path: 'aggie-accessibility',
-  //   component: SignageComponent
-  // },
-  // {
-  //   path: 'manhole',
-  //   component: SignageComponent
-  // },
 ];
 
 @NgModule({
   declarations: [
+    AggieAccessibilityComponent,
+    BuildingBountyComponent,
     CompetitionsComponent,
     FeedbackComponent,
+    ManholeMappingComponent,
     PapersComponent,
     PostersComponent,
+    SidewalkComponent,
     SignageComponent,
+    StormwaterComponent,
     VgiComponent
   ],
   imports: [CommonModule, RouterModule.forChild(routes)],
