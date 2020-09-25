f9594aa7c1dd8ac9658d39c96a55f90f37af4ded
atharmon <atharmon@tamu.edu>
2020-09-25 19:06:22 -0300

    Insert array of tags

--- libs/gisday/data-api/src/lib/providers/tag/tag.provider.ts
+++ libs/gisday/data-api/src/lib/providers/tag/tag.provider.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
-
+import { Request } from 'express';
 import { Tag, TagRepo } from '../../entities/all.entity';
 import { BaseProvider } from '../../providers/_base/base-provider';
 @Injectable()
 export class TagProvider extends BaseProvider<Tag> {
   constructor(private readonly tagRepo: TagRepo) {
     super(tagRepo);
+  }
+
+  async insertTags(req: Request) {
+    const _tags: Partial<Tag>[] = [];
+    req.body.tags.map((value: Tag) => {
+      const tag: Partial<Tag> = {
+        name: value.name
+      };
+      _tags.push(tag);
+    });
+    const tags = this.tagRepo.create(_tags);
+
+    return this.tagRepo.insert(tags);
   }
 }
