770ebac7b1187d45744327bfe0eb2a4ee379277d
atharmon <atharmon@tamu.edu>
2020-09-25 19:09:04 -0300

    Created SessionDetailModule / Component

---
+++ libs/gisday/ngx/src/lib/pages/sessions/session-detail/session-detail.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SessionDetailComponent } from './session-detail.component';
+
+
+
+@NgModule({
+  declarations: [SessionDetailComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SessionDetailModule { }
