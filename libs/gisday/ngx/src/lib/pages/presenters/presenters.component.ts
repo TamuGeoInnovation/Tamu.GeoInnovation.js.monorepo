6c1214793c1338215b2c053b260cbc56f18aa034
atharmon <atharmon@tamu.edu>
2020-09-26 00:20:13 -0300

    Updated Presenter component with good routing

--- libs/gisday/ngx/src/lib/pages/presenters/presenters.component.ts
+++ libs/gisday/ngx/src/lib/pages/presenters/presenters.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-
-import { Observable } from 'rxjs';
-import { SpeakerService } from '@tamu-gisc/gisday/data-access';
-import { Speaker } from '@tamu-gisc/gisday/data-api';
 
 @Component({
   selector: 'tamu-gisc-presenters',
@@ -11,22 +6,7 @@ import { Speaker } from '@tamu-gisc/gisday/data-api';
   styleUrls: ['./presenters.component.scss']
 })
 export class PresentersComponent implements OnInit {
-  public $presenters: Observable<Array<Partial<Speaker>>>;
-  constructor(private router: Router, private speakerService: SpeakerService) {
-    this.fetchPresenters();
-  }
+  constructor() {}
 
   public ngOnInit(): void {}
-
-  public fetchPresenters() {
-    this.$presenters = this.speakerService.getPresenters();
-  }
-
-  public onPresenterClicked(presenter: Speaker) {
-    this.router.navigate(['presenters/', presenter.guid]);
-  }
-
-  public getPresenterImageUrl(presenter: Speaker) {
-    return `../assets/images/presenters/${presenter.lastName}${presenter.firstName}.jpg`;
-  }
 }
