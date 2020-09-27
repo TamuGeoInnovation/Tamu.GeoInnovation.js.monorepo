5f09a43410766eb51baa2f1547a67d2f00d22309
atharmon <atharmon@tamu.edu>
2020-09-27 15:07:22 -0300

    Added page not found / not authed components

---
+++ libs/gisday/ngx/src/lib/pages/not-authed/not-authed.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NotAuthedComponent } from './not-authed.component';
+
+
+
+@NgModule({
+  declarations: [NotAuthedComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class NotAuthedModule { }
---
+++ libs/gisday/ngx/src/lib/pages/not-found/not-found.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NotFoundComponent } from './not-found.component';
+
+
+
+@NgModule({
+  declarations: [NotFoundComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class NotFoundModule { }
