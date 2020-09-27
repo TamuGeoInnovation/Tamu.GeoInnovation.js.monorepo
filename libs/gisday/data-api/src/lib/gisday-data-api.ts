73d56c750749b14e6866ddd6dc7886c8e6bdfe0c
atharmon <atharmon@tamu.edu>
2020-09-27 14:29:17 -0300

    Added other entitie modules to app.module

--- libs/gisday/data-api/src/lib/gisday-data-api.ts
+++ libs/gisday/data-api/src/lib/gisday-data-api.ts
@@ -5,12 +5,18 @@
 export * from './entities/all.entity';
 
 // Modules
+export * from './modules/check-in/check-in.module';
 export * from './modules/class/class.module';
+export * from './modules/course-credit/course-credit.module';
 export * from './modules/event/event.module';
 export * from './modules/session/session.module';
 export * from './modules/speaker/speaker.module';
 export * from './modules/sponsor/sponsor.module';
+export * from './modules/submission-type/submission-type.module';
 export * from './modules/tag/tag.module';
+export * from './modules/user-class/user-class.module';
+export * from './modules/user-rsvp/user-rsvp.module';
+export * from './modules/user-submission/user-submission.module';
 
 // Providers
 export * from './providers/class/class.provider';
