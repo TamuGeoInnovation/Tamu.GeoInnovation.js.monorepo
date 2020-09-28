f7eafb60daf1f4c245fd6865b2e65da9690c13e8
atharmon <atharmon@tamu.edu>
2020-09-28 02:20:52 -0300

    oidc-client lib works on gisday

--- libs/gisday/ngx/src/lib/modules/count-down/count-down.component.ts
+++ libs/gisday/ngx/src/lib/modules/count-down/count-down.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import * as $ from 'jquery';
-import FlipClock from 'flipclock';
+// import * as $ from 'jquery';
+// import FlipClock from 'flipclock';
 
 @Component({
   selector: 'tamu-gisc-count-down',
@@ -11,14 +11,14 @@ export class CountDownComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    document.onreadystatechange = (ev) => {
-      const start = Date.parse('18 Nov 2020 09:00:00 GMT-6');
-      const now = Date.now();
-      const flipClockEl = document.querySelector('.flipclock');
-      const flipClock = new FlipClock(flipClockEl, (start - now) / 1000, {
-        clockFace: 'DailyCounter',
-        countDown: true
-      });
-    };
+    // document.onreadystatechange = (ev) => {
+    //   const start = Date.parse('18 Nov 2020 09:00:00 GMT-6');
+    //   const now = Date.now();
+    //   const flipClockEl = document.querySelector('.flipclock');
+    //   const flipClock = new FlipClock(flipClockEl, (start - now) / 1000, {
+    //     clockFace: 'DailyCounter',
+    //     countDown: true
+    //   });
+    // };
   }
 }
