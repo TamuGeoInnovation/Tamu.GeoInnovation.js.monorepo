eb8484001cbce18a761680bcae0e6dd8552d3425
atharmon <atharmon@tamu.edu>
2020-09-29 17:48:52 -0300

    added admin-event, admin-rsvp-type

---
+++ libs/gisday/ngx/src/lib/pages/admin/admin.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminComponent } from './admin.component';
+
+
+
+@NgModule({
+  declarations: [AdminComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminModule { }
---
+++ libs/gisday/ngx/src/lib/pages/admin/rsvp-type/admin-rsvp-type.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RsvpTypeComponent } from './admin-rsvp-type.component';
+
+@NgModule({
+  declarations: [RsvpTypeComponent],
+  imports: [CommonModule]
+})
+export class AdminRsvpTypeModule {}
