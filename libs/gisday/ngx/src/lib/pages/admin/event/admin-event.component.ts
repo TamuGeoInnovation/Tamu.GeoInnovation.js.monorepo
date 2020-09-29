167a1de78bba18c76e04aafb87ee1346625209ec
atharmon <atharmon@tamu.edu>
2020-09-29 17:48:52 -0300

    added admin-event, admin-rsvp-type

---
+++ libs/gisday/ngx/src/lib/pages/admin/event/admin-event.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tamu-gisc-admin-event',
+  templateUrl: './admin-event.component.html',
+  styleUrls: ['./admin.event.component.scss']
+})
+export class AdminEventComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ libs/gisday/ngx/src/lib/pages/admin/event/admin-event.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminEventComponent } from './admin-event.component';
+
+@NgModule({
+  declarations: [AdminEventComponent],
+  imports: [CommonModule]
+})
+export class AdminEventModule {}
