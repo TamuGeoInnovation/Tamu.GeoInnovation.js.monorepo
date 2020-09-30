3015ceb7d869061e0e0432a7b2ad81e61bffe180
atharmon <atharmon@tamu.edu>
2020-09-30 00:20:23 -0300

    Added additonal admin-class views

---
+++ libs/gisday/ngx/src/lib/pages/admin/class/add-classes/admin-add-classes.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminAddClassesComponent } from './admin-add-classes.component';
+
+@NgModule({
+  declarations: [AdminAddClassesComponent],
+  imports: [CommonModule]
+})
+export class AdminAddClassesModule {}
