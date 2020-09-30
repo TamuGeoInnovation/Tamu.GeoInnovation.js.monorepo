43e3ec0264dadb05a5c02327b0dec81c935c306f
atharmon <atharmon@tamu.edu>
2020-09-30 17:02:14 -0300

    Additonal html skeleton placed

--- libs/gisday/ngx/src/lib/pages/admin/pages/submission-type/admin-edit-submission-types/admin-edit-submission-types.component.ts
+++ libs/gisday/ngx/src/lib/pages/admin/pages/submission-type/admin-edit-submission-types/admin-edit-submission-types.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'tamu-gisc-admin-edit-submission-types',
   templateUrl: './admin-edit-submission-types.component.html',
-  styleUrls: ['./admin-edit-submission-types.component.css']
+  styleUrls: ['./admin-edit-submission-types.component.scss']
 })
 export class AdminEditSubmissionTypesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
