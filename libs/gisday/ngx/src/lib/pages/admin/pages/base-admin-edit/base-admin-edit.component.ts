5a871c5223bf769da2bfed0c41833622ee37ef12
atharmon <atharmon@tamu.edu>
2020-10-01 00:48:19 -0300

    base-admin-edit/add added

--- libs/gisday/ngx/src/lib/pages/admin/pages/base-admin-edit/base-admin-edit.component.ts
+++ libs/gisday/ngx/src/lib/pages/admin/pages/base-admin-edit/base-admin-edit.component.ts
@@ -1,12 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { OnDestroy, OnInit } from '@angular/core';
+import { BaseService } from '@tamu-gisc/gisday/data-access';
+import { GuidIdentity } from '@tamu-gisc/gisday/data-api';
+import { Observable, Subject } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
 
-@Component({
-  selector: 'tamu-gisc-base-admin-edit',
-  templateUrl: './base-admin-edit.component.html',
-  styleUrls: ['./base-admin-edit.component.scss']
-})
-export class BaseAdminEditComponent implements OnInit {
-  constructor() {}
+export abstract class BaseAdminEditComponent<T extends GuidIdentity, K extends BaseService<T>>
+  implements IBaseAdminEditComponent {
+  public $entities: Observable<Array<Partial<T>>>;
+  private _$destroy: Subject<boolean> = new Subject();
 
-  ngOnInit(): void {}
+  constructor(private readonly entityService: K) {}
+
+  ngOnInit(): void {
+    this.fetchEntities();
+  }
+
+  ngOnDestroy() {
+    this._$destroy.next();
+    this._$destroy.complete();
+  }
+
+  fetchEntities() {
+    this.$entities = this.entityService.getEntities().pipe(shareReplay(1));
+  }
+
+  deleteEntity(entity: T) {
+    console.log('deleteEntity', entity);
+    this.entityService.deleteEntity(entity.guid).subscribe((deleteStatus) => {
+      console.log('Deleted ', entity.guid);
+      this.fetchEntities();
+    });
+  }
+}
+
+export interface IBaseAdminEditComponent extends OnInit, OnDestroy {
+  ngOnInit(): void;
+  ngOnDestroy(): void;
+  fetchEntities(): void;
+  deleteEntity(entity): void;
 }
--- libs/gisday/ngx/src/lib/pages/admin/pages/tag/admin-edit-tags/admin-edit-tags.component.ts
+++ libs/gisday/ngx/src/lib/pages/admin/pages/tag/admin-edit-tags/admin-edit-tags.component.ts
@@ -1,39 +1,44 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { shareReplay } from 'rxjs/operators';
-import { Observable, Subject } from 'rxjs';
+import { Component } from '@angular/core';
 
 import { TagService } from '@tamu-gisc/gisday/data-access';
 import { Tag } from '@tamu-gisc/gisday/data-api';
+import { BaseAdminEditComponent } from '../../base-admin-edit/base-admin-edit.component';
 
 @Component({
   selector: 'tamu-gisc-admin-edit-tags',
   templateUrl: './admin-edit-tags.component.html',
   styleUrls: ['./admin-edit-tags.component.scss']
 })
-export class AdminEditTagsComponent implements OnInit, OnDestroy {
-  public $tags: Observable<Array<Partial<Tag>>>;
-  private _$destroy: Subject<boolean> = new Subject();
-
-  constructor(private readonly tagService: TagService) {}
-
-  ngOnInit(): void {
-    this.fetchEntities();
-  }
-
-  public ngOnDestroy() {
-    this._$destroy.next();
-    this._$destroy.complete();
-  }
-
-  public fetchEntities() {
-    this.$tags = this.tagService.getEntities().pipe(shareReplay(1));
-  }
-
-  public deleteEntity(entity: Tag) {
-    console.log('deleteEntity', entity);
-    this.tagService.deleteEntity(entity.guid).subscribe((deleteStatus) => {
-      console.log('Deleted ', entity.guid);
-      this.fetchEntities();
-    });
+export class AdminEditTagsComponent extends BaseAdminEditComponent<Tag, TagService> {
+  constructor(private readonly tagService: TagService) {
+    super(tagService);
   }
 }
+
+// export class AdminEditTagsComponent implements OnInit, OnDestroy {
+//   public $tags: Observable<Array<Partial<Tag>>>;
+//   private _$destroy: Subject<boolean> = new Subject();
+
+//   constructor(private readonly tagService: TagService) {}
+
+//   ngOnInit(): void {
+//     this.fetchEntities();
+//   }
+
+//   public ngOnDestroy() {
+//     this._$destroy.next();
+//     this._$destroy.complete();
+//   }
+
+//   public fetchEntities() {
+//     this.$tags = this.tagService.getEntities().pipe(shareReplay(1));
+//   }
+
+//   public deleteEntity(entity: Tag) {
+//     console.log('deleteEntity', entity);
+//     this.tagService.deleteEntity(entity.guid).subscribe((deleteStatus) => {
+//       console.log('Deleted ', entity.guid);
+//       this.fetchEntities();
+//     });
+//   }
+// }
