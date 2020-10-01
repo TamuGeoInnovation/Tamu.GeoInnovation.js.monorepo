1f50ad69eeb7282812c946916df4d16a9d27cff9
atharmon <atharmon@tamu.edu>
2020-10-01 20:22:48 -0300

    All pages should have admin-detail working

--- libs/gisday/ngx/src/lib/pages/admin/pages/class/edit-classes/detail-class/admin-detail-class.component.ts
+++ libs/gisday/ngx/src/lib/pages/admin/pages/class/edit-classes/detail-class/admin-detail-class.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ClassService } from '@tamu-gisc/gisday/data-access';
+import { Class } from '@tamu-gisc/gisday/data-api';
+import { BaseAdminDetailComponent } from '../../../base-admin-detail/base-admin-detail.component';
+import { formConfig } from '../../add-classes/admin-add-classes.component';
 
 @Component({
   selector: 'tamu-gisc-detail-class',
   templateUrl: './admin-detail-class.component.html',
   styleUrls: ['./admin-detail-class.component.scss']
 })
-export class AdminDetailClassComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class AdminDetailClassComponent extends BaseAdminDetailComponent<Class, ClassService> {
+  constructor(private fb1: FormBuilder, private route1: ActivatedRoute, private classService: ClassService) {
+    super(fb1, route1, classService, formConfig);
+  }
 }
