f38267191d1b838e2d3ce629744f42449187c273
atharmon <atharmon@tamu.edu>
2020-10-01 00:32:00 -0300

    base-admin-add added

--- libs/gisday/ngx/src/lib/pages/admin/pages/tag/admin-add-tags/admin-add-tags.component.ts
+++ libs/gisday/ngx/src/lib/pages/admin/pages/tag/admin-add-tags/admin-add-tags.component.ts
@@ -1,24 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { TagService } from '@tamu-gisc/gisday/data-access';
+import { Tag } from '@tamu-gisc/gisday/data-api';
+import { BaseAdminAddComponent } from '../../base-admin-add/base-admin-add.component';
+
+const config = {
+  name: ['']
+};
 
 @Component({
   selector: 'tamu-gisc-admin-add-tags',
   templateUrl: './admin-add-tags.component.html',
   styleUrls: ['./admin-add-tags.component.scss']
 })
-export class AdminAddTagsComponent implements OnInit {
-  public form: FormGroup;
-
-  constructor(private fb: FormBuilder, private tagService: TagService) {}
-
-  ngOnInit(): void {
-    this.form = this.fb.group({
-      name: ['', Validators.required]
-    });
-  }
-
-  public submitNewTag() {
-    this.tagService.createEntity(this.form.value);
+export class AdminAddTagsComponent extends BaseAdminAddComponent<Tag, TagService> {
+  constructor(private fb1: FormBuilder, private tagService: TagService) {
+    super(fb1, tagService, config);
   }
 }
+
+// export class AdminAddTagsComponent implements OnInit {
+//   public form: FormGroup;
+
+//   constructor(private fb: FormBuilder, private tagService: TagService) {}
+
+//   ngOnInit(): void {
+//     this.form = this.fb.group({
+//       name: ['', Validators.required]
+//     });
+//   }
+
+//   public submitNewTag() {
+//     this.tagService.createEntity(this.form.value);
+//   }
+// }
