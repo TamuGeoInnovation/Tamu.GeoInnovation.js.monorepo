0e94ad9fcaf9259d28b175e55c54dabe440fd8bb
atharmon <atharmon@tamu.edu>
2020-11-02 15:52:22 -0300

    Added map to other competitions

--- libs/gisday/ngx/src/lib/pages/competitions/stormwater/stormwater.module.ts
+++ libs/gisday/ngx/src/lib/pages/competitions/stormwater/stormwater.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { MapsMapboxModule } from '@tamu-gisc/maps/mapbox';
+
 import { StormwaterComponent } from './stormwater.component';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: StormwaterComponent
+  }
+];
 
 @NgModule({
   declarations: [StormwaterComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule.forChild(routes), MapsMapboxModule],
+  exports: [RouterModule]
 })
-export class StormwaterModule { }
+export class StormwaterModule {}
