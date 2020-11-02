440c7015b801fb46bd9162be23963cd4e19e2e90
atharmon <atharmon@tamu.edu>
2020-11-02 13:26:38 -0300

    Wayback provider / controller, geojson interceptor

---
+++ libs/gisday/data-api/src/lib/interceptors/geo-json/geo-json.interceptor.spec.ts
@@ -0,0 +1,7 @@
+import { GeoJsonInterceptor } from './geo-json.interceptor';
+
+describe('GeoJsonInterceptor', () => {
+  it('should be defined', () => {
+    expect(new GeoJsonInterceptor()).toBeDefined();
+  });
+});
---
+++ libs/gisday/data-api/src/lib/providers/wayback-competition/wayback-competition.provider.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
+import {
+  SignageSubmissionRepo,
+  StormwaterSubmissionRepo,
+  SidewalkSubmissionRepo,
+  ManholeSubmissionRepo
+} from '../../entities/all.entity';
+
+@Injectable()
+export class WaybackCompetitionProvider {
+  constructor(
+    private readonly signageRepo: SignageSubmissionRepo,
+    private readonly stormwaterRepo: StormwaterSubmissionRepo,
+    private readonly sidewalkRepo: SidewalkSubmissionRepo,
+    private readonly manholeRepo: ManholeSubmissionRepo
+  ) {}
+
+  public async getSignageSubmissions() {
+    return this.signageRepo.find();
+  }
+
+  public async getStormwaterSubmissions() {
+    return this.stormwaterRepo.find();
+  }
+
+  public async getSidewalkSubmissions() {
+    return this.sidewalkRepo.find();
+  }
+
+  public async getManholeSubmissions() {
+    return this.manholeRepo.find();
+  }
+}
