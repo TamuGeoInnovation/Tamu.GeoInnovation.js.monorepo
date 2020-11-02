a5d68f707daba028b8f4d607f0a7490ccfc8656a
atharmon <atharmon@tamu.edu>
2020-11-02 15:52:22 -0300

    Added map to other competitions

--- libs/gisday/ngx/src/lib/pages/competitions/sidewalk/sidewalk.module.ts
+++ libs/gisday/ngx/src/lib/pages/competitions/sidewalk/sidewalk.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { MapsMapboxModule } from '@tamu-gisc/maps/mapbox';
+
 import { SidewalkComponent } from './sidewalk.component';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: SidewalkComponent
+  }
+];
 
 @NgModule({
   declarations: [SidewalkComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule.forChild(routes), MapsMapboxModule],
+  exports: [RouterModule]
 })
-export class SidewalkModule { }
+export class SidewalkModule {}
