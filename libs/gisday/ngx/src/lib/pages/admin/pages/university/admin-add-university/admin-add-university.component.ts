cb0a64b8f4ec95d5f3598de5a4c349a452d90a02
atharmon <atharmon@tamu.edu>
2020-11-05 00:57:57 -0300

    Various university admin pages

---
+++ libs/gisday/ngx/src/lib/pages/admin/pages/university/admin-add-university/admin-add-university.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tamu-gisc-admin-add-university',
+  templateUrl: './admin-add-university.component.html',
+  styleUrls: ['./admin-add-university.component.scss']
+})
+export class AdminAddUniversityComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ libs/gisday/ngx/src/lib/pages/admin/pages/university/admin-edit-university/admin-edit-university.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tamu-gisc-admin-edit-university',
+  templateUrl: './admin-edit-university.component.html',
+  styleUrls: ['./admin-edit-university.component.scss']
+})
+export class AdminEditUniversityComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
