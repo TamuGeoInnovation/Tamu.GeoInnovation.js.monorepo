bfc0363e5059d7a85c1c2db74eadf6595dfd2d2b
atharmon <atharmon@tamu.edu>
2020-11-06 15:06:52 -0300

    University admin pages done

--- libs/gisday/ngx/src/lib/pages/admin/pages/university/admin-add-university/admin-add-university.component.ts
+++ libs/gisday/ngx/src/lib/pages/admin/pages/university/admin-add-university/admin-add-university.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UniversityService } from '@tamu-gisc/gisday/data-access';
+import { University } from '@tamu-gisc/gisday/data-api';
+import { BaseAdminAddComponent } from '../../base-admin-add/base-admin-add.component';
+
+export const formConfig = {
+  guid: [''],
+  name: [''],
+  acronym: [''],
+  hexTriplet: ['']
+};
 
 @Component({
   selector: 'tamu-gisc-admin-add-university',
   templateUrl: './admin-add-university.component.html',
   styleUrls: ['./admin-add-university.component.scss']
 })
-export class AdminAddUniversityComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class AdminAddUniversityComponent extends BaseAdminAddComponent<University, UniversityService> {
+  constructor(private fb1: FormBuilder, private universityService: UniversityService) {
+    super(fb1, universityService, formConfig);
+  }
 }
---
+++ libs/gisday/ngx/src/lib/pages/admin/pages/university/admin-view-university/admin-view-university.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { UniversityService } from '@tamu-gisc/gisday/data-access';
+import { University } from '@tamu-gisc/gisday/data-api';
+import { BaseAdminViewComponent } from '../../base-admin-view/base-admin-view.component';
+
+@Component({
+  selector: 'tamu-gisc-admin-view-university',
+  templateUrl: './admin-view-university.component.html',
+  styleUrls: ['./admin-view-university.component.scss']
+})
+export class AdminViewUniversityComponent extends BaseAdminViewComponent<University, UniversityService> {
+  constructor(private readonly universityService: UniversityService) {
+    super(universityService);
+  }
+}
