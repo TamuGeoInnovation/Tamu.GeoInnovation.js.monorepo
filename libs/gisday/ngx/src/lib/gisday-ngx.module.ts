027baad56ced817372dcdd653c6886e115944776
Edher <imacleopard2010@gmail.com>
2020-11-05 20:11:22 -0600

    Added some mobile styles and tile nav
    Using mobile styles from the original site but not all are working as expected. Might need to rewrite those rules.
    
    Using tile nav, but does not route anywhere right now

--- libs/gisday/ngx/src/lib/gisday-ngx.module.ts
+++ libs/gisday/ngx/src/lib/gisday-ngx.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthService } from '@tamu-gisc/gisday/data-access';
+import { UITileNavigationModule } from '@tamu-gisc/ui-kits/ngx/navigation/mobile-tile';
+import { UINavigationTriggersModule } from '@tamu-gisc/ui-kits/ngx/navigation/triggers';
 
 import { FooterComponent } from './modules/footer/footer.component';
 import { HeaderComponent } from './modules/header/header.component';
@@ -10,7 +12,7 @@ import { LandingModule } from './pages/landing/landing.module';
 import { EventModule } from './pages/events/event.module';
 
 @NgModule({
-  imports: [CommonModule, LandingModule, EventModule],
+  imports: [CommonModule, LandingModule, EventModule, UITileNavigationModule, UINavigationTriggersModule],
   declarations: [FooterComponent, HeaderComponent],
   providers: [AuthService],
   exports: [FooterComponent, HeaderComponent, LandingModule, EventModule]
