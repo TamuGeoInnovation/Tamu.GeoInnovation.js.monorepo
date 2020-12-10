262a3cfe1c539486fc9fb115ee1bfdf22f34498b
Sapir Carlo Lastimoza-Dooley <sapirdooley@users.noreply.github.com>
2020-12-09 20:30:56 -0600

    add feature tab and routing tab tests

---
+++ apps/aggiemap-angular-e2e/src/integration/features-search.spec.ts
@@ -0,0 +1,58 @@
+describe('Search', () => {
+  before(() => {
+    cy.server()
+    cy.route('POST', '*')
+      .as('apiCheck')
+    cy.visit('https://aggiemap.tamu.edu/map/d')
+    cy.wait('@apiCheck')
+  })
+  it('Create Test Search', () => {
+    cy.get('.margin-left')
+      .clear()
+      .type('Computing Services Annex')
+      .click()
+    cy.get('tamu-gisc-search')
+      .find('p.focusable')
+      .eq(0)
+      .click()
+  })
+
+  it('Title', () => {
+    cy.contains(':nth-child(1) > .feature-style-1', 'Computing Services Annex')
+  })
+  it('Building Number', () => {
+    cy.contains('tamu-gisc-buildling-popup-component.ng-star-inserted > :nth-child(1) > :nth-child(3)', '733 Lamar St, College Station, TX 77843')
+  })
+  it('Copy Text', () => {
+    cy.contains('.copy-text', 'https://aggiemap.tamu.edu/?bldg=0517')
+      .should('have.attr', 'role', 'button')
+  })
+  it('Copy Button', () => {
+    cy.contains('.copy-button', 'Copy')
+      .should('have.attr', 'role', 'button')
+  })
+  it('Directions Button', () => {
+    cy.contains('.button', 'Directions To Here')
+      .should('have.attr', 'role', 'button')
+      .should('have.attr', 'building-number', '0517')
+  })
+  it('Building Proctors Button', () => {
+    cy.contains('#building-proctors', 'Building Proctors')
+      .should('have.attr', 'building', '0517')
+    cy.get('.no-decor')
+      .should('have.attr', 'href', 'https://proctorlist.tamu.edu/MainProctorBuildings/Details/0517?utm_source=aggiemap')
+  })
+  it('Info Header', () => {
+    cy.contains(':nth-child(3) > .feature-style-1', 'Departments & Offices')
+  })
+  it('Department Name', () => {
+    cy.contains('.department-name', 'Geography')
+  })
+  it('College Name', () => {
+    cy.contains('.college-name', 'College Of Geosciences')
+  })
+  it('Building Image', () => {
+    cy.get('.image > img')
+      .should('have.attr', 'src', 'https://aggiemap.tamu.edu/images/compressed_test/0517.jpg')
+  })
+})
---
+++ apps/aggiemap-angular-e2e/src/integration/routing-options.spec.ts
@@ -0,0 +1,37 @@
+describe('Routing Options', () => {
+  beforeEach(() => {
+    cy.visit('https://aggiemap.tamu.edu/map/d/trip/options')
+  })
+  it('Title Logo', () => {
+    cy.get('tamu-gisc-tamu-block > img')
+      .should('have.attr', 'alt', 'Texas A&M University Logo')
+      .and('have.attr', 'src', 'assets/images/logo/TAM-PrimaryMarkBB.svg') 
+  })
+  it('Return Button', () => {
+    cy.get('#options-return')
+      .should('have.attr', 'role', 'button')
+      .and('have.attr', 'Title', 'Return to trip planner')
+      .and('contain', 'arrow_back')
+  })
+  it('Option Title', () => {
+    cy.contains('.sidebar-component-name > p', 'General')
+  })
+  it('Option Label', () => {
+    cy.contains('label', 'Accessible Friendly Directions')
+  })
+  it('Option Description', () => {
+    cy.contains('.description.ng-star-inserted', 'Will attempt to calculate a route with accessible access such as ramps, doors, etc.')
+  })
+  it('Test Checkbox', () => {
+    cy.get('tamu-gisc-checkbox')
+      .as('option')
+    cy.get('@option')
+      .find('i')
+      .as('checkBox')
+      .should('have.class', 'material-icons')
+    cy.get('@option')
+      .click()
+    cy.get('@checkBox')
+      .should('have.class', 'material-icons active')
+  })
+})
