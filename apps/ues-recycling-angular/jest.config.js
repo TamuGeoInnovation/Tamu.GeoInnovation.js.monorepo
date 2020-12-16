f1faa1a5f07504bf0f90e4b261821a6d5851b380
Edher <imacleopard2010@gmail.com>
2020-12-16 09:24:35 -0600

    Added base angular and nest data api apps

---
+++ apps/ues-recycling-angular/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: 'ues-recycling-angular',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/ues-recycling-angular',
+  snapshotSerializers: ['jest-preset-angular/AngularSnapshotSerializer.js', 'jest-preset-angular/HTMLCommentSerializer.js']
+};
---
+++ apps/ues-recycling-data-api-nest/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'ues-recycling-data-api-nest',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/ues-recycling-data-api-nest'
+};
