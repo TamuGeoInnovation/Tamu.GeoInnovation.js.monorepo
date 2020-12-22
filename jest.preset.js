75e891abad21d639fe956b51874abdeb0f5cae83
Edher <imacleopard2010@gmail.com>
2020-12-21 19:54:51 -0600

    Added arcgis-js-api type to app tsconfigs

--- jest.preset.js
+++ jest.preset.js
@@ -1,6 +1,6 @@
 const nxPreset = require('@nrwl/jest/preset');
 const { pathsToModuleNameMapper } = require('ts-jest/utils');
-const { compilerOptions } = require('./tsconfig.json');
+const { compilerOptions } = require('./tsconfig.base.json');
 
 module.exports = {
   ...nxPreset,
