e3cb49dd0149a328163d9ab52efb201717a07be0
Edgar <imacleopard2010@gmail.com>
2021-01-26 12:43:06 -0600

    Added table valve filter and ratio bar

--- libs/ues/cold-water/ngx/src/lib/modules/info/pages/list/list.component.ts
+++ libs/ues/cold-water/ngx/src/lib/modules/info/pages/list/list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
+import { filter, map, shareReplay, switchMap } from 'rxjs/operators';
 
 import { ColdWaterValvesService, MappedValve } from '../../../core/services/cold-water-valves/cold-water-valves.service';
 
@@ -10,10 +11,73 @@ import { ColdWaterValvesService, MappedValve } from '../../../core/services/cold
 })
 export class ListComponent implements OnInit {
   public valves: Observable<Array<MappedValve>>;
+  public ratio: Observable<ValveStateRatio>;
+  public filtered: Observable<Array<MappedValve>>;
+
+  public filterOpen: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  public filterClosed: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
   constructor(private valveService: ColdWaterValvesService) {}
 
   public ngOnInit(): void {
-    this.valves = this.valveService.valves;
+    this.valves = this.valveService.valves.pipe(shareReplay());
+
+    this.filtered = combineLatest([this.filterOpen, this.filterClosed]).pipe(
+      switchMap(([fo, fc]) => {
+        return this.valves.pipe(
+          filter((v) => v !== undefined),
+          map((valves) => {
+            return valves.filter((v) => {
+              if (v.attributes.State === 'open' && fo === false) {
+                return true;
+              } else if (v.attributes.State === 'closed' && fc === false) {
+                return true;
+              }
+
+              return false;
+            });
+          })
+        );
+      })
+    );
+
+    this.ratio = this.valves.pipe(
+      filter((v) => v !== undefined),
+      map((valves) => {
+        return valves.reduce(
+          (acc, curr, index, arr) => {
+            if (curr.attributes.State === 'closed') {
+              acc.closed++;
+            } else {
+              acc.open++;
+            }
+
+            if (index === arr.length - 1) {
+              acc.open = (acc.open / arr.length) * 100;
+              acc.closed = (acc.closed / arr.length) * 100;
+            }
+
+            return acc;
+          },
+          {
+            open: 0,
+            closed: 0
+          }
+        );
+      })
+    );
   }
+
+  public tableFilter(toggle: 'closed' | 'open') {
+    if (toggle === 'closed') {
+      this.filterClosed.next(!this.filterClosed.getValue());
+    } else {
+      this.filterOpen.next(!this.filterOpen.getValue());
+    }
+  }
+}
+
+interface ValveStateRatio {
+  open: number;
+  closed: number;
 }
