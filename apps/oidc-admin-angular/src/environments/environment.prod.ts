6cf9c08a13fad67246c7e090c083d6ebf19d0952
Edgar <imacleopard2010@gmail.com>
2021-03-09 19:29:14 -0600

    Fixed missing required HttpClientModule
    Updated the api url for production

--- apps/oidc-admin-angular/src/environments/environment.prod.ts
+++ apps/oidc-admin-angular/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api_url: 'http://localhost:4001/oidc'
+  api_url: 'https://idp.geoservices.tamu.edu/api'
 };
 
-export const api_url = 'http://localhost:4001/oidc';
+export const api_url = 'https://idp.geoservices.tamu.edu/api';
