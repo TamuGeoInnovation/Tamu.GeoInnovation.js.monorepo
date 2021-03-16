5fbf5569f0bf24340a854634dde361cc4c78817c
Edgar <imacleopard2010@gmail.com>
2021-03-16 11:28:23 -0500

    Removed hard coded willacy county boundary

--- apps/cpa-angular/src/environments/common.ts
+++ apps/cpa-angular/src/environments/common.ts
@@ -9,27 +9,6 @@ export const SearchSources = [];
 
 export const LayerSources: LayerSource[] = [
   {
-    type: 'feature',
-    id: 'county-boundary',
-    title: 'County',
-    url: 'https://texasatlas.arch.tamu.edu/arcgis/rest/services/PubTX/TA_AdminBdys/MapServer/7',
-    native: {
-      ...commonLayerProps,
-      definitionExpression: 'GEOID = 48489',
-      renderer: {
-        type: 'simple',
-        symbol: {
-          type: 'simple-line',
-          color: 'red',
-          style: 'short-dash',
-          join: 'round',
-          cap: 'round',
-          width: '2pt'
-        }
-      }
-    }
-  },
-  {
     type: 'graphics',
     id: 'drawing-layer',
     title: 'Drawn Features',
