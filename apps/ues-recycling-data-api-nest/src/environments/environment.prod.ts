da73db28f38705bb00a594d5b519ff5f05b756db
Edgar <imacleopard2010@gmail.com>
2021-04-14 09:19:47 -0500

    Added CORS allowed origins

--- apps/ues-recycling-data-api-nest/src/environments/environment.prod.ts
+++ apps/ues-recycling-data-api-nest/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   port: 28052,
   globalPrefix: '',
-  allowedOrigins: ['https://dev.aggiemap.tamu.edu']
+  allowedOrigins: ['https://dev.aggiemap.tamu.edu', 'https://ues-dev.geoservices.tamu.edu', 'https://maps.apogee.tamu.edu']
 };
 
 export { productionDbConfig as dbConfig } from './ormconfig';
