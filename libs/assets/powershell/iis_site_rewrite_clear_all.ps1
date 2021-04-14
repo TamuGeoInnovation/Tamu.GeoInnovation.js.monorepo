be5e05c64bc54adbdbbfa68895f63f9ed827afcb
Edgar <imacleopard2010@gmail.com>
2021-04-14 15:01:48 -0500

    Added iis rewrite clear ps scripts

---
+++ libs/assets/powershell/iis_site_rewrite_clear_all.ps1
@@ -0,0 +1,20 @@
+﻿param( 
+    [parameter(Position=0, Mandatory=$true, HelpMessage="Value of the target sites IIS binding.")]
+    [string]$SiteBinding
+)
+
+$path = "MACHINE/WEBROOT/APPHOST/$SiteBinding";
+$baseRulesFilter= "/system.webServer/rewrite/rules";
+$baseRuleFilter= "$baseRulesFilter/rule";
+$filter = "$baseRuleFilter";
+
+write-host "";
+write-host("Path: $path");
+write-host("Base Rules Filter: $baseRulesFilter");
+write-host("Base Rule Filter: $baseRuleFilter");
+write-host("Filter: $filter");
+write-host "";
+
+Clear-WebConfiguration -PSPath $path -Filter $filter;
+
+Write-Host("All rewrite rules have been cleared for $path");
