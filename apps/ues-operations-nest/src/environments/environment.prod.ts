d9d10513089559262ae55eefb940ff9d3b4b3305
Edgar <imacleopard2010@gmail.com>
2021-05-11 15:20:59 -0500

    Implemented IDP client into ues operations nest app

--- apps/ues-operations-nest/src/environments/environment.prod.ts
+++ apps/ues-operations-nest/src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  port: 28050,
+  globalPrefix: '',
+  allowedOrigins: ['https://maps.apogee.tamu.edu']
 };
+
+export { productionClientConfig as idpConfig } from './oidc-client-config';
--- apps/ues-operations-nest/src/environments/environment.ts
+++ apps/ues-operations-nest/src/environments/environment.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: false
+  production: false,
+  port: 3333,
+  globalPrefix: '',
+  allowedOrigins: ['http://localhost:4200']
 };
+
+export { localClientConfig as idpConfig } from './oidc-client-config';
