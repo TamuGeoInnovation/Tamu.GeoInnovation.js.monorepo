f4da96cc1e9f80a4b32c1e7f7ff1182c6b86bfc9
minhb121 <minhb121@tamu.edu>
2021-07-13 16:57:27 -0500

    began testing of mobile layers by importing mobile specific resolutions

--- apps/aggiemap-angular-e2e/src/integration/mobile-layers.spec.ts
+++ apps/aggiemap-angular-e2e/src/integration/mobile-layers.spec.ts
@@ -1,10 +1,27 @@
 /// <reference path="../support/index.d.ts" />
-import mobileSizes from './mobile-menu.spec';
+import {mobileSizes} from "./resolutions";
+
 mobileSizes.forEach((size) => {
-  describe(`Test Elements on Mobile Layers Page: ${size} resolution`, () => {
+  describe(`Test Elements on Mobile Layers Page: ${size} resolution`, function() {
     beforeEach(() => {
       cy.viewport(size[0], size[1])
+      cy.intercept("GET", "**/TAMU_BaseMap/**").as("basemap")
+      cy.intercept("GET", "**/Construction_2018/**").as("construction")
+      cy.intercept("GET", "**/Physical_Distancing_Tents/**").as("tents")
+      cy.visit('https://aggiemap.tamu.edu/map/m')
+      cy.get('canvas').should('be.visible', {timeout: 5000})
+      // open menu
+      cy.get('.material-icons.left').trigger('mouseover').click()
+      // open layer menu
+      cy.contains('Layers').click()
     })
-    
+
+    it('Construction Zone', function() {
+      cy.wait('@construction')
+      cy.checkLayer('8', 'Construction Zone')
+      cy.checkLegend('8', 'Construction Zone')
+    })
   })
 })
+
+
