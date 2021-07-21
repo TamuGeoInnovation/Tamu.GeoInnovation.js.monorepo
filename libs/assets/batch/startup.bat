3424f6c473198ed2fc32e0cfd7cf9de3987d310b
IsaiahV98 <itv66@tamu.edu>
2021-07-21 14:57:28 -0500

    Added checks to verify that PM2 process was running.

--- libs/assets/batch/startup.bat
+++ libs/assets/batch/startup.bat
@@ -1,9 +1,8 @@
-sc query "PM2" | find "RUNNING"
+pm2 status | find /i "Current process list running is not in sync with saved list. App effluent-api dispatch-api recycling-api operations-api differs. Type 'pm2 save' to synchronize."
 
-if "%ERRORLEVEL%"=="0" (
-    echo Already Running
-    pause
+if not errorlevel 1 (
+    pm2 kill
+    pm2 resurect 
 ) else (
-    pm2 resurrect
-    pause
+    echo Already Running
 )
