766417ab962b6c94c3f56a3d62efcdfa9c8c9567
minhb121 <minhb121@tamu.edu>
2021-07-28 12:32:42 -0500

    added test for search results and pop-up feautres when building is searched

--- apps/aggiemap-angular-e2e/src/integration/mobile-search.spec.ts
+++ apps/aggiemap-angular-e2e/src/integration/mobile-search.spec.ts
@@ -8,17 +8,37 @@ mobileSizes.forEach((size) => {
       cy.intercept("GET", "**/TAMU_BaseMap/**").as("basemap")
       cy.intercept("GET", "**/Construction_2018/**").as("construction")
       cy.intercept("GET", "**/Physical_Distancing_Tents/**").as("tents")
+    })
+
+    it('Open Aggie Map', () => {
       cy.visit('https://aggiemap.tamu.edu/map/m')
       cy.get('canvas').should('be.visible', {timeout: 5000})
     })
 
     it('Input Building Search', () => {
-        cy.get('tamu-gisc-search-mobile').click()
-        cy.get('.margin-left').type('Rudder Tower')
-        cy.get('.search-results-container').should('be.visible')
-        cy.contains('Rudder Tower (0446)').click()
-        cy.get('.feature-style-1').should('contain.text', 'Rudder Tower')
-      })
+      cy.get('tamu-gisc-search-mobile').click()
+      cy.get('.margin-left').type('Rudder Tower')
+    })
+
+    it('Search Results Displayed', () => {
+      cy.get('.search-results-container').should('be.visible')
+      //TODO: Change to expect
+      cy.get('.focusable').should('contain.text', 'Rudder Tower (0446)')
+    })
+      
+    it('Click Search Result', () => {
+      cy.contains('Rudder Tower (0446)').click()
+      cy.get('.feature-style-1').should('contain.text', 'Rudder Tower')
+    })
+
+    it('Drag Pop-up Into User View', () => {
+      cy.get('tamu-gisc-feature-mobile-popup').should('be.visible')
+      cy.get('.popup').move({ x: 0, y: -600, position: 'center', force: true })
+      // check if correct URL is present
+      cy.get('tamu-gisc-copy-field')
+        .should('be.visible')
+        .and('contain.text', 'https://aggiemap.tamu.edu/?bldg=0446')
+    })
 
   })
 })
