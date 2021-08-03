188a80c0c4a2d75593dfd3e5582a7defec28b7ea
Edgar <imacleopard2010@gmail.com>
2021-08-03 06:17:58 -0500

    Added x axis scrolling to snap/scen navigator
    All snapshot and scenario labels no longer break lines to preserve the height of the element with multi-line labels.
    
    This caused the navigator content to overflow. Added scrolling and hidding of the scroll bar to minimize footprint. Need to develop alternative scrolling.

--- libs/cpa/common/ngx/src/lib/modules/viewer/components/snapshot-navigator/snapshot-navigator.component.ts
+++ libs/cpa/common/ngx/src/lib/modules/viewer/components/snapshot-navigator/snapshot-navigator.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostBinding, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { combineLatest, interval, Observable, of } from 'rxjs';
+import { distinctUntilChanged, map, switchMap } from 'rxjs/operators';
 
 import { TypedSnapshotOrScenario, ViewerService } from '../../services/viewer.service';
 
@@ -9,11 +10,54 @@ import { TypedSnapshotOrScenario, ViewerService } from '../../services/viewer.se
   templateUrl: './snapshot-navigator.component.html',
   styleUrls: ['./snapshot-navigator.component.scss']
 })
-export class SnapshotNavigatorComponent {
+export class SnapshotNavigatorComponent implements OnInit {
   public snapshots: Observable<Array<TypedSnapshotOrScenario>> = this.vs.snapshotsAndScenarios;
   public index: Observable<number> = this.vs.selectionIndex;
 
-  constructor(public router: ActivatedRoute, private vs: ViewerService) {}
+  /**
+   * Represents the inner width of the navigator component
+   */
+  public innerWidth: Observable<number>;
+
+  /**
+   * Represents the width of the time line (the actual line going through snapshot and scenario icons).
+   * 
+   * This width is never larger than the innerWidth
+   */
+  public timelineWidth: Observable<number>;
+
+  /**
+   * Represents a left offset for the time line (actual line) so that it displays centered
+   */
+  public timelineLeftOffset: Observable<number>;
+
+  constructor(public router: ActivatedRoute, private vs: ViewerService, private el: ElementRef) {}
+
+  @HostBinding('style.maxWidth')
+  public get maxContainerWidth() {
+    return `${window.innerWidth - window.innerWidth * 0.2}px`;
+  }
+
+  public ngOnInit(): void {
+    this.innerWidth = interval(500).pipe(
+      switchMap(() => {
+        return of(Math.floor((this.el.nativeElement as HTMLElement).scrollWidth));
+      }),
+      distinctUntilChanged()
+    );
+
+    this.timelineWidth = this.innerWidth.pipe(
+      map((n) => {
+        return n - 75;
+      })
+    );
+
+    this.timelineLeftOffset = combineLatest([this.innerWidth, this.timelineWidth]).pipe(
+      map(([inner, timeline]) => {
+        return (inner - timeline) / 2;
+      })
+    );
+  }
 
   public navigate(index: number) {
     this.vs.updateSelectionIndex(index);
