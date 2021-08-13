abb576ebe8c7b518f3ae2e0f756c6a8fdb04a70a
Edgar <imacleopard2010@gmail.com>
2021-08-13 04:16:22 -0500

    Tooltip component fixed positioning support
    Added the ability for the tooltip to calculate the fixed position relative to the trigger. This positining mode prevents overflow in containers where the tooltips would extend past the initial container constraints and create scroll bars.
    
    Need to validate absolute positining still works or update all locations where absolute positining is used.
    
    Also need to add in smarter positioning calculation logic to ensure tooltip content is never positioned outside of the viewport.

--- libs/ui-kits/ngx/layout/src/lib/components/tooltip/tooltip.component.ts
+++ libs/ui-kits/ngx/layout/src/lib/components/tooltip/tooltip.component.ts
@@ -6,11 +6,12 @@ import {
   HostListener,
   ContentChild,
   AfterContentInit,
-  OnDestroy
+  OnDestroy,
+  ViewChild
 } from '@angular/core';
-import { TooltipTriggerComponent } from '../tooltip-trigger/tooltip-trigger.component';
-import { takeUntil } from 'rxjs/operators';
-import { Subject } from 'rxjs';
+import { TooltipTriggerComponent } from './components/tooltip-trigger/tooltip-trigger.component';
+import { debounceTime, map, shareReplay, takeUntil } from 'rxjs/operators';
+import { BehaviorSubject, combineLatest, Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'tamu-gisc-tooltip',
@@ -18,25 +19,83 @@ import { Subject } from 'rxjs';
   styleUrls: ['./tooltip.component.scss']
 })
 export class TooltipComponent implements OnInit, OnDestroy, AfterContentInit {
+  private _isVisible: BehaviorSubject<boolean> = new BehaviorSubject(false);
+
   @Input()
   public width: number;
 
   @Input()
   public height: number;
 
+  /**
+   * The positioning of the tooltip content container.
+   *
+   * Defaults to 'absolute'. Is possible to overflow in undesired locations.
+   *
+   * 'fixed' strategy is experimental. Calculates the fixed positioning relative to the trigger.
+   */
   @Input()
-  public position: string;
+  public positioningStrategy: 'absolute' | 'fixed' = 'absolute';
+
+  /**
+   * The relative position of the trigger.
+   */
+  @Input()
+  public position: 'left' | 'right' | 'bottom' | 'top';
 
   @Input()
-  public isVisible = false;
+  public set isVisible(value) {
+    this._isVisible.next(value);
+  }
+
+  /**
+   * The value passed in from the tooltip trigger to initiate the hiding/showing
+   * of the tooltip content.
+   */
+  public get isVisible() {
+    return this._isVisible.getValue();
+  }
 
   @ContentChild(TooltipTriggerComponent, { static: true })
   public trigger: TooltipTriggerComponent;
 
+  @ContentChild(TooltipTriggerComponent, { static: true, read: ElementRef })
+  public triggerElem: ElementRef;
+
+  @ViewChild('tooltipContainer') set content(c: ElementRef) {
+    if (c) {
+      // Container will be set when the *ngIf that determines its rendered state
+      // is true
+      this._container.next(c.nativeElement);
+    }
+  }
+
+  /**
+   * The container that wraps the projected tooltip content.
+   */
+  private _container: Subject<HTMLElement> = new Subject();
+
+  /**
+   * X (left) and Y (top) offsets relative to the trigger
+   * calculated to position the tooltip content close to the trigger.
+   *
+   * Calculated when using the fixed positioning strategy
+   */
+  public containerPosition: Observable<{ x: string; y: string }>;
+
+  /**
+   * Controls when the tooltip content becomes visible. Is a delayed-emitting observable
+   * on the `isVisible` member.
+   *
+   * This is done AFTER it has been added to the DOM and the position
+   * offsets applied when using the fixed positioning strategy.
+   */
+  public tooltipContentVisible: Observable<boolean>;
+
   private _$destroy: Subject<boolean> = new Subject();
 
   /**
-   * Detects if interaction clicks are within the component
+   * Detects if interaction clicks are within the component to reset the visibility
    */
   @HostListener('document:mousedown', ['$event'])
   public clickOutside(event) {
@@ -47,11 +106,31 @@ export class TooltipComponent implements OnInit, OnDestroy, AfterContentInit {
 
   constructor(private elementRef: ElementRef) {}
 
-  public ngOnInit() {}
+  public ngOnInit() {
+    // Set debounce time otherwise there will be two changes in the change detection cycle.
+    this.containerPosition = combineLatest([this._container.pipe(debounceTime(0))]).pipe(
+      map(([c]) => {
+        const triggerRects: DOMRect = this.triggerElem.nativeElement.getBoundingClientRect();
+        const containerRects: DOMRect = c.getBoundingClientRect();
 
-  public ngOnDestroy() {
-    this._$destroy.next();
-    this._$destroy.complete();
+        return [containerRects, triggerRects];
+      }),
+      map(([cr, tr]) => {
+        const padding = 10;
+        const leftOffset = tr.left - cr.width / 2;
+        const topOffset = tr.top - cr.height - padding;
+        return { x: `${leftOffset}px`, y: `${topOffset}px` };
+      }),
+      shareReplay()
+    );
+
+    // Because of the containerPosition debounce, the container will flash positions:
+    // One when the element is first rendered in its initial location
+    // And again when the offsets are applied.
+    //
+    // Tooltips are hidden by default and set to visible AFTER the offsets are applied to avoid
+    // the position flashing.
+    this.tooltipContentVisible = this._isVisible.pipe(debounceTime(1));
   }
 
   public ngAfterContentInit() {
@@ -60,5 +139,10 @@ export class TooltipComponent implements OnInit, OnDestroy, AfterContentInit {
         this.isVisible = !this.isVisible;
       });
     }
+  }
+
+  public ngOnDestroy() {
+    this._$destroy.next();
+    this._$destroy.complete();
   }
 }
