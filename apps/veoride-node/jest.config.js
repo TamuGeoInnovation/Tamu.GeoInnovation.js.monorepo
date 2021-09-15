6c7116988c5ebbaee639cdb016df19b786c45e80
Edgar <imacleopard2010@gmail.com>
2021-09-15 00:18:34 -0500

    veoride-node boilerplate

---
+++ apps/veoride-node/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'veoride-node',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json'
+    }
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/veoride-node'
+};
--- jest.config.js
+++ jest.config.js
@@ -123,6 +123,7 @@ module.exports = {
     '<rootDir>/libs/ues/common/types',
     '<rootDir>/libs/ues/cold-water/data-api',
     '<rootDir>/libs/ues/common/nest',
-    '<rootDir>/libs/maps/feature/data'
+    '<rootDir>/libs/maps/feature/data',
+    '<rootDir>/apps/veoride-node'
   ]
 };
