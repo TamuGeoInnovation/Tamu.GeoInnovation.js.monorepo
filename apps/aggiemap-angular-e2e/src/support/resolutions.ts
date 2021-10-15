862bba7d7ec9a2968fc513955ffacd8e75def5ee
minhb121 <minh.a.bui97@gmail.com>
2021-10-15 10:18:06 -0500

    updated resolution list for desktop and mobile to test only on one resolution for each

--- apps/aggiemap-angular-e2e/src/support/resolutions.ts
+++ apps/aggiemap-angular-e2e/src/support/resolutions.ts
@@ -1,2 +1,2 @@
-export const desktopSizes = [[1920, 1080], [1366, 768], [1440, 900]];
-export const mobileSizes = [[375, 812], [768, 1024], [411, 731], [360, 640]]
+export const desktopSizes = [1920, 1080]
+export const mobileSizes = [414, 896]
