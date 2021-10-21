bdbd26b909c0f4e9ba121c182ed2e598ee75bc20
Edgar <imacleopard2010@gmail.com>
2021-10-21 00:07:13 -0500

    Moved participant component to viewer module
    It was in it's own module for completely uncessary reasons. This changes makes the component structure more uniform with the viewer, scenario/snapshot timeline, and the upcomign admin controls component.

--- libs/cpa/common/ngx/src/lib/modules/viewer/viewer.module.ts
+++ libs/cpa/common/ngx/src/lib/modules/viewer/viewer.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { UILayoutModule } from '@tamu-gisc/ui-kits/ngx/layout';
 import { SidebarModule } from '@tamu-gisc/common/ngx/ui/sidebar';
 import { LayerListComponent, LayerListModule } from '@tamu-gisc/maps/feature/layer-list';
 import { LegendComponent, LegendModule } from '@tamu-gisc/maps/feature/legend';
+import { UIFormsModule } from '@tamu-gisc/ui-kits/ngx/forms';
+import { MapDrawingModule } from '@tamu-gisc/maps/feature/draw';
+import { FeatureSelectorModule } from '@tamu-gisc/maps/feature/feature-selector';
 
-import { CPAFormsModule } from '../forms/cpaForms.module';
-import { ParticipantComponent } from '../forms/components/participant/participant.component';
+import { ParticipantComponent } from './components/participant/participant.component';
 import { ViewerComponent } from './viewer.component';
 import { SnapshotNavigatorComponent } from './components/snapshot-navigator/snapshot-navigator.component';
 
@@ -38,13 +41,17 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    CPAFormsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    UIFormsModule,
+    MapDrawingModule,
+    FeatureSelectorModule,
     UILayoutModule,
     SidebarModule,
     LayerListModule,
     LegendModule
   ],
-  declarations: [ViewerComponent, SnapshotNavigatorComponent],
+  declarations: [ViewerComponent, SnapshotNavigatorComponent, ParticipantComponent],
   exports: [RouterModule]
 })
 export class CpaViewerModule {}
