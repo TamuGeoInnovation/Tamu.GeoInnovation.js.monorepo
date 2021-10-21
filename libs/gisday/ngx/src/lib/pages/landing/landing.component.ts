3491d903792b84637cce47f56c4ed09aee8105d5
atharmon <atharmon@tamu.edu>
2021-10-21 13:49:08 -0500

    Implemented a first day countdown

--- libs/gisday/ngx/src/lib/pages/landing/landing.component.ts
+++ libs/gisday/ngx/src/lib/pages/landing/landing.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
+import { interval, Subscription } from 'rxjs';
+
 @Component({
   selector: 'tamu-gisc-landing',
   templateUrl: './landing.component.html',
@@ -8,14 +10,27 @@ import { Title } from '@angular/platform-browser';
 })
 export class LandingComponent implements OnInit {
   private title = 'TxGIS Day 2021';
+  private rightNow: Date;
+  private firstDay: Date;
+  public timeTill: Date = new Date();
+  public subscription: Subscription;
+  private source = interval(1000);
 
   constructor(private titleService: Title) {}
 
   public ngOnInit() {
     this.titleService.setTitle(this.title);
 
+    this.rightNow = new Date(Date.now());
+    this.firstDay = new Date(2021, 11, 15, 14, 30, 0, 0);
+
     this.loadCountdown();
   }
 
-  public loadCountdown() {}
+  public loadCountdown() {
+    this.subscription = this.source.subscribe(() => {
+      this.rightNow = new Date(Date.now());
+      this.timeTill = new Date(this.firstDay.getTime() - this.rightNow.getTime());
+    });
+  }
 }
