76763b43a7dfbdb9c27ca919d29822f71ee8df92
minhb121 <minh.a.bui97@gmail.com>
2021-10-25 19:57:56 -0500

    imported desktop sizes and setup application state to run aggiemap locally for side-features.spec

--- apps/aggiemap-angular-e2e/src/integration/main-menu/sidebar-features.spec.ts
+++ apps/aggiemap-angular-e2e/src/integration/main-menu/sidebar-features.spec.ts
@@ -1,1 +1,13 @@
 import { desktopSizes } from '../../support/resolutions';
+
+// runs same the same tests on different resolutions
+desktopSizes.forEach((size) => {
+  describe(`Sidebar Features on ${size} Resolution`, () => {
+    beforeEach(() => {
+      cy.viewport(size[0], size[1]);
+      cy.visit('https://localhost:4200'); // setup to run aggiemap locally
+      cy.get('canvas').should('be.visible');
+    });
+    // Tests begin here
+  });
+});
