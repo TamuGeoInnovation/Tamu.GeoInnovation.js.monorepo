d5a839e17eab5e601f8b69bdee0cc6bd39dab448
atharmon <atharmon@tamu.edu>
2021-11-09 12:57:04 -0600

    Added TypeORM to gisday-competitions-nest

--- apps/gisday-competitions-nest/src/app/app.module.ts
+++ apps/gisday-competitions-nest/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { CompetitionSubmission, SubmissionLocation, SubmissionMedia } from '@tamu-gisc/gisday/common';
 import { GisdayCompetitionsModule } from '@tamu-gisc/gisday/competitions';
 
+import { config } from '../environments/ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const entities = [CompetitionSubmission, SubmissionLocation, SubmissionMedia];
+
 @Module({
-  imports: [GisdayCompetitionsModule],
+  imports: [TypeOrmModule.forRoot({ ...config, entities }), GisdayCompetitionsModule],
   controllers: [AppController],
   providers: [AppService]
 })
