5149cee340d999f291cf9cbfde0304dd93039696
Edgar <imacleopard2010@gmail.com>
2021-11-14 14:36:28 -0600

    Removed login device guard
    To allow passthrough for other routes that do not require a device guard.

--- apps/gisday-competitions-angular/src/app/pages/public/public.module.ts
+++ apps/gisday-competitions-angular/src/app/pages/public/public.module.ts
@@ -44,7 +44,7 @@ const routes: Routes = [
       {
         path: 'login',
         loadChildren: () => import('./login/login.module').then((m) => m.LoginModule),
-        canActivate: [DeviceGuard],
+        canActivate: [],
         data: {
           deviceModes: ['standalone'],
           deviceFailRedirect: '/install'
