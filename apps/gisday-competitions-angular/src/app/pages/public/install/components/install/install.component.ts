327302429f8dedec2ad9068da3a4b7fecf18e24f
Edgar Hernandez <imacleopard2010@gmail.com>
2021-11-14 18:18:28 -0600

    Added location troubleshooting steps for iOS
    Included screenshots and verification interactive button.

--- apps/gisday-competitions-angular/src/app/pages/public/install/components/install/install.component.ts
+++ apps/gisday-competitions-angular/src/app/pages/public/install/components/install/install.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NotificationService } from '@tamu-gisc/common/ngx/ui/notification';
+import { BehaviorSubject } from 'rxjs';
+import { catchError, take } from 'rxjs/operators';
 
 import { Device, IDeviceOSVersion } from '@tamu-gisc/common/utils/device';
+import { TrackLocation } from '@tamu-gisc/common/utils/geometry/generic';
 
 @Component({
   selector: 'tamu-gisc-install',
@@ -10,10 +14,38 @@ import { Device, IDeviceOSVersion } from '@tamu-gisc/common/utils/device';
 export class InstallComponent implements OnInit {
   public device: Device;
   public version: IDeviceOSVersion;
-  constructor() {}
+
+  public locationRequested: BehaviorSubject<boolean> = new BehaviorSubject(false);
+
+  constructor(private readonly ns: NotificationService) {}
 
   public ngOnInit() {
     this.device = new Device();
     this.version = this.device.OSVersion();
+  }
+
+  public initiateLocationRequest() {
+    if (this.locationRequested.getValue() === false) {
+      this.locationRequested.next(true);
+
+      const l = new TrackLocation({ enableHighAccuracy: true, maximumAge: 10000, timeout: 2500 })
+        .track()
+        .pipe(
+          take(1),
+          catchError((err) => {
+            this.ns.preset('no_gps');
+
+            throw new Error('Location forbidden');
+          })
+        )
+        .subscribe((res) => {
+          this.ns.toast({
+            id: 'location-request-success',
+            title: 'Location Access Granted',
+            message:
+              'Your device has successfully granted location access to this application! You can now upload submissions'
+          });
+        });
+    }
   }
 }
