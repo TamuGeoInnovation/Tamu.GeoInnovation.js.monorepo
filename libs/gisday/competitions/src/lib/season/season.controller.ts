2abf2878bcb3def83c3ed40eaf9cc9f9ae354d73
Edgar <imacleopard2010@gmail.com>
2021-12-07 01:45:15 -0600

    Added season statistics endpoint
    Returns a count and breakdown of season form responses.

--- libs/gisday/competitions/src/lib/season/season.controller.ts
+++ libs/gisday/competitions/src/lib/season/season.controller.ts
@@ -1,16 +1,17 @@
-import { Body, Controller, Post, UploadedFiles, UseInterceptors } from '@nestjs/common';
-import { AnyFilesInterceptor, FilesInterceptor } from '@nestjs/platform-express';
-
-import { DeepPartial } from 'typeorm';
+import { Controller, Get, Param } from '@nestjs/common';
 
 import { CompetitionSeason } from '../entities/all.entities';
-
 import { BaseController } from '../_base/base.controller';
-import { SeasonService } from './season.service';
+import { GetSeasonStatisticsDto, SeasonService } from './season.service';
 
 @Controller('season')
 export class SeasonController extends BaseController<CompetitionSeason> {
   constructor(private service: SeasonService) {
     super(service);
+  }
+
+  @Get(':season/statistics')
+  public getStatisticsForSeason(@Param() params: GetSeasonStatisticsDto) {
+    return this.service.getSeasonStatistics(params.season);
   }
 }
--- libs/gisday/competitions/src/lib/season/season.service.ts
+++ libs/gisday/competitions/src/lib/season/season.service.ts
@@ -1,14 +1,80 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { getRepository, Repository } from 'typeorm';
 
-import { DeepPartial, Repository } from 'typeorm';
+import { IsNotEmpty } from 'class-validator';
 
-import { CompetitionSeason } from '../entities/all.entities';
-
+import { CompetitionSeason, CompetitionSubmission } from '../entities/all.entities';
 import { BaseService } from '../_base/base.service';
+
 @Injectable()
 export class SeasonService extends BaseService<CompetitionSeason> {
   constructor(@InjectRepository(CompetitionSeason) private seasonRepo: Repository<CompetitionSeason>) {
     super(seasonRepo);
   }
+
+  public async getSeasonStatistics(season: string) {
+    const existing = await this.seasonRepo.findOne({
+      year: season
+    });
+
+    const responses = await getRepository(CompetitionSubmission)
+      .createQueryBuilder('response')
+      .where('response.season = :seasonGuid')
+      .setParameter('seasonGuid', existing.guid)
+      .getMany();
+
+    const total = responses.length;
+
+    const dictionary = responses.reduce((acc, response) => {
+      const responseValue = response.value;
+
+      // For each response, go through the value keys and append them to the accumulated object.
+      Object.entries(responseValue).forEach(([key, value]) => {
+        if (!acc[key]) {
+          // Each value key represents a question, and each question has a number of different responses.
+          // If the key does not exist in the accumulated value yet, then create an other dictionary inside of it.
+          // It will contain a tally of all the individual unique responses for that question.
+          acc[key] = {};
+        }
+
+        // Check if the inner question object has an entry for the current question value.
+        // If it does, increase the tally, otherwise create the entry.
+        if (acc[key].hasOwnProperty(value)) {
+          acc[key][value]++;
+        } else {
+          acc[key][value] = 1;
+        }
+      });
+
+      return acc;
+    }, {});
+
+    return {
+      total,
+      breakdown: dictionary
+    };
+  }
+}
+
+export class GetSeasonStatisticsDto {
+  @IsNotEmpty()
+  public season: string;
+}
+
+export class SeasonStatisticsDto {
+  /**
+   * Total number of responses for a given season.
+   */
+  public total: number;
+
+  /**
+   * A dictionary where the keys represent the question id in a season form. The inner object inside each
+   * represents the count of individual response values for that question.
+   */
+  public breakdown: {
+    [question_id: string]: {
+      [count: string]: number;
+    };
+  };
 }
