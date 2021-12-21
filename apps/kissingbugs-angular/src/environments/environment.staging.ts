39bbfa1d6de695f6f2a13f127839d7d5ddc12c84
atharmon <atharmon@tamu.edu>
2021-12-21 11:53:59 -0600

    Added staging stage to azure-pipelines, workspace.json, and added environement file for kissingbugs-angular staging

---
+++ apps/kissingbugs-angular/src/environments/environment.staging.ts
@@ -0,0 +1,12 @@
+import { LayerSource } from '@tamu-gisc/common/types';
+
+export const environment = {
+  production: false
+};
+
+export const api_url = 'https://kissingbug-stage.geoservices.tamu.edu/api';
+
+export const email_server_url = 'http://localhost:4005';
+
+export const SearchSources = [];
+export const LayerSources: LayerSource[] = [];
