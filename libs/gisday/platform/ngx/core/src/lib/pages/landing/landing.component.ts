21231ab0b00e59d5c9a939f2fdfb57cbb289e068
atharmon <atharmon@tamu.edu>
2022-03-25 14:57:26 -0500

    Fixed nav header location issue on desktop Made countdown more accurate

--- libs/gisday/platform/ngx/core/src/lib/pages/landing/landing.component.ts
+++ libs/gisday/platform/ngx/core/src/lib/pages/landing/landing.component.ts
@@ -9,10 +9,11 @@ import { interval, Subscription } from 'rxjs';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
-  private title = 'TxGIS Day 2021';
+  private title = 'TxGIS Day 2022';
   private rightNow: Date;
   private firstDay: Date;
   public timeTill: Date = new Date();
+  public daysTill: string;
   public subscription: Subscription;
   private source = interval(1000);
 
@@ -22,7 +23,7 @@ export class LandingComponent implements OnInit {
     this.titleService.setTitle(this.title);
 
     this.rightNow = new Date(Date.now());
-    this.firstDay = new Date(2021, 11, 15, 14, 30, 0, 0);
+    this.firstDay = new Date(2022, 10, 13, 14, 30, 0, 0); // Idk why but we have to take a month and a day off for the numbers to add up - Aaron H (3/26/22)
 
     this.loadCountdown();
   }
@@ -31,6 +32,8 @@ export class LandingComponent implements OnInit {
     this.subscription = this.source.subscribe(() => {
       this.rightNow = new Date(Date.now());
       this.timeTill = new Date(this.firstDay.getTime() - this.rightNow.getTime());
+      const milliseconds = this.firstDay.getTime() - this.rightNow.getTime();
+      this.daysTill = (milliseconds / 1000 / 86400).toFixed(0); // Converts from milliseconds to seconds to days (86400 seconds in a day)
     });
   }
 }
