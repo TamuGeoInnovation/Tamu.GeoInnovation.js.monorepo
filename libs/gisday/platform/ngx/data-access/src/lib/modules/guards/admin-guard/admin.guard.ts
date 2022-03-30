6c6ecbb310f43f8e76d51ba1fe21e9608d99ae48
atharmon <atharmon@tamu.edu>
2022-03-30 15:42:11 -0500

    Fixed routing issue with not-authed route Changed AdminGuard to reflect JWT process

--- libs/gisday/platform/ngx/data-access/src/lib/modules/guards/admin-guard/admin.guard.ts
+++ libs/gisday/platform/ngx/data-access/src/lib/modules/guards/admin-guard/admin.guard.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, UrlTree } from '@angular/router';
 
+import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { AuthService } from '../../services/auth/auth.service';
@@ -9,16 +11,31 @@ import { AuthService } from '../../services/auth/auth.service';
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate {
-  constructor(private readonly authService: AuthService) {}
+  // constructor(private readonly authService: AuthService) {}
+  constructor(private readonly oidcSecurityService: OidcSecurityService, private router: Router) {}
 
-  public canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.authService.getUserRole().pipe(
-      map((value) => {
-        if (value.role.level_role === '99') {
+  // public canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+  //   return this.authService.getUserRole().pipe(
+  //     map((value) => {
+  //       if (value.role.level_role === '99') {
+  //         return true;
+  //       } else {
+  //         return false;
+  //       }
+  //     })
+  //   );
+  // }
+
+  public canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.oidcSecurityService.isAuthenticated$.pipe(
+      map(({ isAuthenticated }) => {
+        // allow navigation if authenticated
+        if (isAuthenticated) {
           return true;
-        } else {
-          return false;
         }
+
+        // redirect if not authenticated
+        return this.router.parseUrl('/forbidden');
       })
     );
   }
