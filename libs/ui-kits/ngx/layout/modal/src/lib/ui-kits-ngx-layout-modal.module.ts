eec41aa77f388d191d150527dc2bf216c415e240
Edgar <imacleopard2010@gmail.com>
2022-04-16 03:46:12 -0500

    Added modal lib documentation

--- libs/ui-kits/ngx/layout/modal/src/lib/ui-kits-ngx-layout-modal.module.ts
+++ libs/ui-kits/ngx/layout/modal/src/lib/ui-kits-ngx-layout-modal.module.ts
@@ -5,7 +5,6 @@ import { ModalHostComponent } from './components/modal-host/modal-host.component
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ModalHostComponent],
-  providers: []
+  declarations: [ModalHostComponent]
 })
 export class UiKitsNgxLayoutModalModule {}
