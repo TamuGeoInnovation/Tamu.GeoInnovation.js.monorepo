9a55b4f1e4bdb45320754b5bef4918e8e1813920
Edgar <imacleopard2010@gmail.com>
2022-04-18 20:01:52 -0500

    Added boilerplate beta prompt modal component

---
+++ libs/aggiemap/ngx/ui/shared/src/lib/modules/modals/components/beta-prompt/beta-prompt.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tamu-gisc-beta-prompt',
+  templateUrl: './beta-prompt.component.html',
+  styleUrls: ['./beta-prompt.component.scss']
+})
+export class BetaPromptComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ libs/aggiemap/ngx/ui/shared/src/lib/modules/modals/modals.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BetaPromptComponent } from './components/beta-prompt/beta-prompt.component';
+
+@NgModule({
+  declarations: [BetaPromptComponent],
+  imports: [CommonModule]
+})
+export class ModalsModule {}
+
