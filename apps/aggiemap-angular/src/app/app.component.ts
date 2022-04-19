d2dc8d7730516da03d173b42ecd1a18727fc9035
Edgar <imacleopard2010@gmail.com>
2022-04-18 19:59:47 -0500

    Registered global view container
    Done for aggiemap-angular project

--- apps/aggiemap-angular/src/app/app.component.ts
+++ apps/aggiemap-angular/src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
 import { Angulartics2GoogleAnalytics } from 'angulartics2';
+
+import { ModalService } from '@tamu-gisc/ui-kits/ngx/layout/modal';
 
 @Component({
   selector: 'tamu-gisc-aggiemap-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor(public analytics: Angulartics2GoogleAnalytics) {
+export class AppComponent implements OnInit {
+  constructor(
+    public analytics: Angulartics2GoogleAnalytics,
+    private readonly vcr: ViewContainerRef,
+    private readonly ms: ModalService
+  ) {
     analytics.startTracking();
+  }
+
+  public ngOnInit(): void {
+    this.ms.registerGlobalViewRef(this.vcr);
   }
 }
