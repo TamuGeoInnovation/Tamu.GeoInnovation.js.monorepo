3c2b3bd1d7c790c5fd22e84efa96141c4678655d
atharmon <atharmon@tamu.edu>
2022-04-20 11:41:11 -0500

    Login / logout guards now use angular-auth-oidc-client

--- libs/gisday/platform/ngx/data-access/src/lib/modules/guards/login-guard/login-guard.guard.ts
+++ libs/gisday/platform/ngx/data-access/src/lib/modules/guards/login-guard/login-guard.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 import { map } from 'rxjs/operators';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 import { AuthService } from '../../services/auth/auth.service';
 
@@ -9,18 +10,33 @@ import { AuthService } from '../../services/auth/auth.service';
   providedIn: 'root'
 })
 export class LoginGuard implements CanActivate {
-  constructor(private readonly authService: AuthService) {}
+  // constructor(private readonly authService: AuthService) {}
+  constructor(public oidcSecurityService: OidcSecurityService) {}
 
   public canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.authService.state().pipe(
-      map((value) => {
-        if (value === false) {
-          window.location.href = route.data['externalUrl'];
-          return true;
-        } else {
-          return true;
-        }
-      })
-    );
+    // return this.oidcSecurityService.isAuthenticated();
+    // if (this.oidcSecurityService.isAuthenticated) {
+    //   console.log('is authenticated somehow');
+    //   return true;
+    // } else {
+    //   console.log('not authenticated, redirecting?');
+    //   window.location.href = route.data['externalUrl'];
+    //   return true;
+    // }
+
+    // const $isLoading = this.oidcSecurityService.isLoading$;
+    this.oidcSecurityService.authorize();
+    return true;
+    // this.oidcSecurityService.authorize();
+    //   return this.authService.state().pipe(
+    //     map((value) => {
+    //       if (value === false) {
+    //         window.location.href = route.data['externalUrl'];
+    //         return true;
+    //       } else {
+    //         return true;
+    //       }
+    //     })
+    //   );
   }
 }
--- libs/gisday/platform/ngx/data-access/src/lib/modules/guards/logout-guard/logout-guard.guard.ts
+++ libs/gisday/platform/ngx/data-access/src/lib/modules/guards/logout-guard/logout-guard.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
+import { OidcSecurityService } from 'angular-auth-oidc-client';
 import { map } from 'rxjs/operators';
 
 import { AuthService } from '../../services/auth/auth.service';
@@ -9,18 +10,21 @@ import { AuthService } from '../../services/auth/auth.service';
   providedIn: 'root'
 })
 export class LogoutGuard implements CanActivate {
-  constructor(private readonly authService: AuthService) {}
+  // constructor(private readonly authService: AuthService) {}
+  constructor(public oidcSecurityService: OidcSecurityService) {}
 
   public canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.authService.state().pipe(
-      map((value) => {
-        if (value === true) {
-          window.location.href = route.data['externalUrl'];
-          return true;
-        } else {
-          return false;
-        }
-      })
-    );
+    // return this.authService.state().pipe(
+    //   map((value) => {
+    //     if (value === true) {
+    //       window.location.href = route.data['externalUrl'];
+    //       return true;
+    //     } else {
+    //       return false;
+    //     }
+    //   })
+    // );
+    this.oidcSecurityService.logoff();
+    return true;
   }
 }
