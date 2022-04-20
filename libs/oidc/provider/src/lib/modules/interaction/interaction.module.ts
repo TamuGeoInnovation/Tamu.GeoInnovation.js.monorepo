c40c1d769ff3143bdff9a670f8d1000c489d6ee9
atharmon <atharmon@tamu.edu>
2022-04-20 15:11:43 -0500

    Merged interaction.controller files, updated interaction.module

--- libs/oidc/provider/src/lib/modules/interaction/interaction.module.ts
+++ libs/oidc/provider/src/lib/modules/interaction/interaction.module.ts
@@ -3,10 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UserModule, UserService } from '@tamu-gisc/oidc/common';
 
-import { InteractionController } from '../../controllers/interaction/interaction.new.controller';
+import { InteractionController } from '../../controllers/interaction/interaction.controller';
 import { UserLoginModule } from '../user-login/user-login.module';
 import { OidcProviderService } from '../../services/provider/provider.service';
-import { AccountRepo, RoleRepo } from '../../../../../common/src/lib/entities/all.entity';
+import { AccountRepo, RoleRepo } from '@tamu-gisc/oidc/common';
 import { OidcModule } from '../oidc/oidc.module';
 
 @Module({
