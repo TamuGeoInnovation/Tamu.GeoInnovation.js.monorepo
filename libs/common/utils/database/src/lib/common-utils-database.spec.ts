a68d434ddef7897c0564ce8577729a26d9dc898b
Edgar <imacleopard2010@gmail.com>
2022-04-20 20:32:13 -0500

    Fixed common-utils-database linting issues

--- libs/common/utils/database/src/lib/common-utils-database.spec.ts
+++ libs/common/utils/database/src/lib/common-utils-database.spec.ts
@@ -1,7 +1,7 @@
-import { makeWhere } from '@tamu-gisc/common/utils/database';
+import { makeWhere } from './common-utils-database';
 
 describe('makeWhere', () => {
-  it('should throw errors on invalid basic inputs', function() {
+  it('should throw errors on invalid basic inputs', function () {
     expect(() => makeWhere(null, [], [])).toThrowError(new Error('Input Parameter Missing'));
     expect(() => makeWhere([], null, [])).toThrowError(new Error('Input Parameter Missing'));
     expect(() => makeWhere([], [], null)).toThrowError(new Error('Input Parameter Missing'));
@@ -13,23 +13,23 @@ describe('makeWhere', () => {
     );
   });
 
-  it('should throw an error on invalid number of transformations', function() {
+  it('should throw an error on invalid number of transformations', function () {
     expect(() => makeWhere([], [], [], [], ['off by one'])).toThrowError(
       new Error('Number of transformations does not equal the number of keys.')
     );
   });
 
-  it('should throw an error on mismatching counts of inputs', function() {
+  it('should throw an error on mismatching counts of inputs', function () {
     expect(() => makeWhere([''], [], [])).toThrowError(new Error('Keys, values, and operators are not of equal length.'));
     expect(() => makeWhere([], [''], [])).toThrowError(new Error('Keys, values, and operators are not of equal length.'));
     expect(() => makeWhere([], [], [''])).toThrowError(new Error('Keys, values, and operators are not of equal length.'));
   });
 
-  it('should handle trivial input', function() {
+  it('should handle trivial input', function () {
     expect(makeWhere(['key1', 'key2'], ['value1', 'value2'], ['=', '='])).toEqual(`key1 = 'VALUE1' OR key2 = 'VALUE2'`);
   });
 
-  it('should handle wildcards', function() {
+  it('should handle wildcards', function () {
     expect(
       makeWhere(
         ['key1', 'key2', 'key3'],
@@ -40,7 +40,7 @@ describe('makeWhere', () => {
     ).toEqual(`key1 = 'VALUE1%' OR key2 = '%VALUE2' OR key3 = '%VALUE3%'`);
   });
 
-  it('should handle transformations', function() {
+  it('should handle transformations', function () {
     expect(makeWhere(['key1'], ['value1'], ['='], null, ['test'])).toEqual(`TEST(key1) = 'VALUE1'`);
   });
 });
