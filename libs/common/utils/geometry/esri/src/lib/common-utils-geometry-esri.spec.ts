87cc8cc5663a390cd092b88f62e142536e06d12e
Edgar <imacleopard2010@gmail.com>
2022-04-20 20:48:24 -0500

    Fixed common-utils-geometry-esri linting issues

--- libs/common/utils/geometry/esri/src/lib/common-utils-geometry-esri.spec.ts
+++ libs/common/utils/geometry/esri/src/lib/common-utils-geometry-esri.spec.ts
@@ -1,4 +1,5 @@
 import esri = __esri;
+
 import {
   centroidFromGeometry,
   centroidFromPolygonGeometry,
@@ -7,43 +8,54 @@ import {
   pointFromMultiPointGeometry,
   pointFromPointGeometry,
   pointFromPolylineGeometry
-} from '@tamu-gisc/common/utils/geometry/esri';
+} from './common-utils-geometry-esri';
 
 describe('centroidFromGeometry', () => {
   it('should error on invalid input', () => {
-    expect(() => centroidFromGeometry(({} as unknown) as FeatureUnion)).toThrowError(
+    expect(() => centroidFromGeometry({} as unknown as FeatureUnion)).toThrowError(
       new Error('Could not get centroid from search geometry because type could not be identified.')
     );
   });
 
   it('should work for valid inputs', () => {
-    expect(centroidFromGeometry(({ rings: [[[20, 4], [20, 4], [20, 4], [20, 4]]] } as unknown) as esri.Polygon)).toEqual({
-      latitude: 4,
-      longitude: 20
-    });
-    expect(centroidFromGeometry(({ points: [[20, 4]] } as unknown) as esri.Multipoint)).toEqual({
-      latitude: 4,
-      longitude: 20
-    });
-    expect(centroidFromGeometry(({ y: 4, x: 20 } as unknown) as esri.Point)).toEqual({ latitude: 4, longitude: 20 });
     expect(
-      centroidFromGeometry(({
+      centroidFromGeometry({
+        rings: [
+          [
+            [20, 4],
+            [20, 4],
+            [20, 4],
+            [20, 4]
+          ]
+        ]
+      } as unknown as esri.Polygon)
+    ).toEqual({
+      latitude: 4,
+      longitude: 20
+    });
+    expect(centroidFromGeometry({ points: [[20, 4]] } as unknown as esri.Multipoint)).toEqual({
+      latitude: 4,
+      longitude: 20
+    });
+    expect(centroidFromGeometry({ y: 4, x: 20 } as unknown as esri.Point)).toEqual({ latitude: 4, longitude: 20 });
+    expect(
+      centroidFromGeometry({
         paths: [],
         extent: { center: { latitude: 1, longitude: 2 } }
-      } as unknown) as esri.Polyline)
+      } as unknown as esri.Polyline)
     ).toEqual({ latitude: 1, longitude: 2 });
   });
 });
 
 describe('centroidFromPolygonGeometry', () => {
   it('should error on invalid input', () => {
-    expect(() => centroidFromPolygonGeometry(({ centroid: null, rings: null } as unknown) as esri.Polygon)).toThrowError(
+    expect(() => centroidFromPolygonGeometry({ centroid: null, rings: null } as unknown as esri.Polygon)).toThrowError(
       new Error('Feature provided does not contain rings.')
     );
   });
 
   it('should work for esri.Polygon', () => {
-    expect(centroidFromPolygonGeometry(({ centroid: { latitude: 4, longitude: 20 } } as unknown) as esri.Polygon)).toEqual({
+    expect(centroidFromPolygonGeometry({ centroid: { latitude: 4, longitude: 20 } } as unknown as esri.Polygon)).toEqual({
       latitude: 4,
       longitude: 20
     });
@@ -51,7 +63,16 @@ describe('centroidFromPolygonGeometry', () => {
 
   it('should work for Turf Point', () => {
     expect(
-      centroidFromPolygonGeometry(({ rings: [[[20, 4], [20, 4], [20, 4], [20, 4]]] } as unknown) as esri.Polygon)
+      centroidFromPolygonGeometry({
+        rings: [
+          [
+            [20, 4],
+            [20, 4],
+            [20, 4],
+            [20, 4]
+          ]
+        ]
+      } as unknown as esri.Polygon)
     ).toEqual({
       latitude: 4,
       longitude: 20
@@ -64,37 +85,37 @@ describe('getGeometryType', () => {
     expect(() => getGeometryType(null)).toThrowError(
       new Error('Could not determine geometry type because geometry was not provided.')
     );
-    expect(() => getGeometryType(({} as unknown) as esri.Geometry)).toThrowError(
+    expect(() => getGeometryType({} as unknown as esri.Geometry)).toThrowError(
       new Error('Could not resolve geometry type.')
     );
   });
 
   it('should return "point"', () => {
-    expect(getGeometryType(({ latitude: 4, longitude: 20 } as unknown) as esri.Geometry)).toEqual('point');
+    expect(getGeometryType({ latitude: 4, longitude: 20 } as unknown as esri.Geometry)).toEqual('point');
   });
 
   it('should return "multipoint"', () => {
-    expect(getGeometryType(({ points: [] } as unknown) as esri.Geometry)).toEqual('multipoint');
+    expect(getGeometryType({ points: [] } as unknown as esri.Geometry)).toEqual('multipoint');
   });
 
   it('should return "polygon"', () => {
-    expect(getGeometryType(({ rings: [] } as unknown) as esri.Geometry)).toEqual('polygon');
+    expect(getGeometryType({ rings: [] } as unknown as esri.Geometry)).toEqual('polygon');
   });
 
   it('should return "polyline"', () => {
-    expect(getGeometryType(({ paths: [] } as unknown) as esri.Geometry)).toEqual('polyline');
+    expect(getGeometryType({ paths: [] } as unknown as esri.Geometry)).toEqual('polyline');
   });
 });
 
 describe('pointFromMultiPointGeometry', () => {
   it('should error on invalid input', () => {
-    expect(() => pointFromMultiPointGeometry(({ points: null } as unknown) as esri.Multipoint)).toThrowError(
+    expect(() => pointFromMultiPointGeometry({ points: null } as unknown as esri.Multipoint)).toThrowError(
       new Error('Feature provided does not contain points.')
     );
   });
 
   it('should work for basic input', () => {
-    expect(pointFromMultiPointGeometry(({ points: [[20, 4]] } as unknown) as esri.Multipoint)).toEqual({
+    expect(pointFromMultiPointGeometry({ points: [[20, 4]] } as unknown as esri.Multipoint)).toEqual({
       latitude: 4,
       longitude: 20
     });
@@ -112,24 +133,24 @@ describe('pointFromPointGeometry', () => {
   });
 
   it('should work for esri.Point', () => {
-    expect(pointFromPointGeometry(({ y: 4, x: 20 } as unknown) as esri.Point)).toEqual({ latitude: 4, longitude: 20 });
+    expect(pointFromPointGeometry({ y: 4, x: 20 } as unknown as esri.Point)).toEqual({ latitude: 4, longitude: 20 });
   });
 });
 
 describe('pointFromPolylineGeometry', () => {
   it('should error on invalid input', () => {
     expect(() => pointFromPolylineGeometry(null)).toThrowError(new Error('Feature provided does not contain paths.'));
-    expect(() => pointFromPolylineGeometry(({ paths: undefined } as unknown) as esri.Polyline)).toThrowError(
+    expect(() => pointFromPolylineGeometry({ paths: undefined } as unknown as esri.Polyline)).toThrowError(
       new Error('Feature provided does not contain paths.')
     );
   });
 
   it('should error on invalid input', () => {
     expect(
-      pointFromPolylineGeometry(({
+      pointFromPolylineGeometry({
         paths: [],
         extent: { center: { latitude: 1, longitude: 2 } }
-      } as unknown) as esri.Polyline)
+      } as unknown as esri.Polyline)
     ).toEqual({ latitude: 1, longitude: 2 });
   });
 });
