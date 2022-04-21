4b9867c58247ca159e90a1d12cf8afb57c4aec22
Edgar <imacleopard2010@gmail.com>
2022-04-20 22:44:20 -0500

    Fixed common-utils-string linting issues

--- libs/common/utils/string/src/lib/common-utils-string.ts
+++ libs/common/utils/string/src/lib/common-utils-string.ts
@@ -30,12 +30,12 @@ export class TemplateRenderer {
    */
   public render(): string {
     if (this.template && this.replacement) {
-      return this.template.replace(/\{.*?\}/g, (match: string, index: number, orig: string) => {
+      return this.template.replace(/\{.*?\}/g, () => {
         // Replace captured block with the provided replacement string.
         return this.replacement;
       });
     } else if (this.template && this.lookup) {
-      return this.template.replace(/\{.*?\}/g, (match: string, index: number, orig: string) => {
+      return this.template.replace(/\{.*?\}/g, (match: string) => {
         // Remove template braces before setting value from lookup object.
         return getPropertyValue<string>(this.lookup, match.replace('{', '').replace('}', ''));
       });
