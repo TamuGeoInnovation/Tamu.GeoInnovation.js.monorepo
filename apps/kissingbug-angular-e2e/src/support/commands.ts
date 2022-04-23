84f55149e494146781df2e244a18f7c7e9e14980
Edgar <imacleopard2010@gmail.com>
2022-04-22 22:32:09 -0500

    Fixed kissingbug-angular-e2e linting issues

--- apps/kissingbug-angular-e2e/src/support/commands.ts
+++ apps/kissingbug-angular-e2e/src/support/commands.ts
@@ -8,6 +8,7 @@
 // https://on.cypress.io/custom-commands
 // ***********************************************
 
+// eslint-disable-next-line @typescript-eslint/no-namespace
 declare namespace Cypress {
   interface Chainable<Subject> {
     login(email: string, password: string): void;
