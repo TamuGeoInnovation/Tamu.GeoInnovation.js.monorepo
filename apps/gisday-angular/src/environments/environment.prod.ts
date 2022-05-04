3888785d13867642c63c1e22f17888e44cf3d9ea
atharmon <atharmon@tamu.edu>
2022-05-04 11:18:43 -0500

    Updated production environment file

--- apps/gisday-angular/src/environments/environment.prod.ts
+++ apps/gisday-angular/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
   production: true
 };
+
+export const client_id = 'gisday';
+export const api_url = 'http://localhost:3333/api';
+export const idp_dev_url = 'https://idp-dev.geoservices.tamu.edu/oidc';
