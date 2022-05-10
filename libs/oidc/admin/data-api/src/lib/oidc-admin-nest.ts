a45fe438bc26c8c34b99134e4cad96979a93004f
atharmon <atharmon@tamu.edu>
2022-05-10 11:32:15 -0500

    Removed old access-token controller / service

--- libs/oidc/admin/data-api/src/lib/oidc-admin-nest.ts
+++ libs/oidc/admin/data-api/src/lib/oidc-admin-nest.ts
@@ -1,13 +1,10 @@
 // Controllers
 export * from './controllers/stats/stats.controller';
-export * from './controllers/access-token/access-token.controller';
 
 // Modules
 export * from './modules/stats/stats.module';
-export * from './modules/access-token/access-token.module';
 export * from './modules/client/client.module';
 
 // Services
 export * from './services/stats/stats.service';
-export * from './services/access-token/access-token.service';
 export * from './services/client/client.service';
