c4ddfab694a4bd2fd7fc9975e8341ceefbfc4691
atharmon <atharmon@tamu.edu>
2022-05-13 11:34:43 -0500

    Removed Angular AuthorizationGuard (uneeded)

--- libs/oidc/ngx/src/index.ts
+++ libs/oidc/ngx/src/index.ts
@@ -1,6 +1,5 @@
 // Guards
 export * from './lib/guards/authentication/authentication.guard';
-export * from './lib/guards/authorization/authorization.guard';
 
 // Modules
 export * from './lib/modules/auth-redirect/auth-routing.module';
