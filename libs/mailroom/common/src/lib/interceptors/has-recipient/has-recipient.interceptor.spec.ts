2db7d80d452348337e4a6d164212bae2793ba4ab
atharmon <atharmon@tamu.edu>
2022-06-28 15:37:16 -0500

    Removed HasRecipientInterceptor and OutboundPipe

--- libs/mailroom/common/src/lib/interceptors/has-recipient/has-recipient.interceptor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { HasRecipientInterceptor } from './has-recipient.interceptor';
-
-describe('HasRecipientInterceptor', () => {
-  it('should be defined', () => {
-    expect(new HasRecipientInterceptor()).toBeDefined();
-  });
-});
--- libs/mailroom/common/src/lib/pipes/outbound.pipe.spec.ts
+++
@@ -1,7 +0,0 @@
-import { OutboundPipe } from './outbound.pipe';
-
-describe('OutboundPipe', () => {
-  it('should be defined', () => {
-    expect(new OutboundPipe()).toBeDefined();
-  });
-});
