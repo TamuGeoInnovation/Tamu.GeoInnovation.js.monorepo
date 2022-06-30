3727c4afeaff5a0323025b01bb3ed408ae68c5e3
atharmon <atharmon@tamu.edu>
2022-06-30 16:50:33 -0500

    Added some basic operations to mailroom-nest controller and service

--- apps/mailroom-nest/src/app/app.controller.ts
+++ apps/mailroom-nest/src/app/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UploadedFiles, UseFilters, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UploadedFiles, UseFilters, UseInterceptors } from '@nestjs/common';
 import { AnyFilesInterceptor } from '@nestjs/platform-express';
 
 import 'multer';
@@ -6,11 +6,25 @@ import 'multer';
 import { IMailroomEmailOutbound, LogToDatabaseInterceptor, MailroomExceptionFilter } from '@tamu-gisc/mailroom/common';
 import { Mailer } from '@tamu-gisc/oidc/common';
 
+import { AppService } from './app.service';
+
 @Controller()
 export class AppController {
+  constructor(public readonly service: AppService) {}
+
   @Get()
   public isRunning() {
     return 200;
+  }
+
+  @Get('all')
+  public getAllEmails() {
+    return this.service.getAllEmails();
+  }
+
+  @Get(':id')
+  public getEmail(@Param('id') id) {
+    return this.service.getEmail(id);
   }
 
   @Post()
--- apps/mailroom-nest/src/app/app.service.ts
+++ apps/mailroom-nest/src/app/app.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Repository } from 'typeorm';
+
+import { MailroomAttachment, MailroomEmail, MailroomReject } from '@tamu-gisc/mailroom/common';
 
 @Injectable()
-export class AppService {}
+export class AppService {
+  constructor(
+    @InjectRepository(MailroomEmail) private emailRepo: Repository<MailroomEmail>,
+    @InjectRepository(MailroomAttachment) private attachmentRepo: Repository<MailroomAttachment>,
+    @InjectRepository(MailroomReject) private rejectRepo: Repository<MailroomReject>
+  ) {}
+
+  public getAllEmails() {
+    return this.emailRepo.find();
+  }
+
+  public getEmail(id: string) {
+    return this.emailRepo.findOne({
+      where: {
+        id
+      },
+      relations: ['attachments']
+    });
+  }
+}
