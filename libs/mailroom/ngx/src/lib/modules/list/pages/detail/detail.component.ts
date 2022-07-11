9b24c871e6d17cba887efad9a5d40cadb321ddbf
atharmon <atharmon@tamu.edu>
2022-07-11 12:48:10 -0500

    Delete email from detail component Added 'back to inbox' button

--- libs/mailroom/ngx/src/lib/modules/list/pages/detail/detail.component.ts
+++ libs/mailroom/ngx/src/lib/modules/list/pages/detail/detail.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Buffer } from 'buffer';
 
 import { map, Observable, shareReplay, switchMap } from 'rxjs';
 
 import { EmailService } from '@tamu-gisc/mailroom/data-access';
 import { MailroomAttachment, MailroomEmail } from '@tamu-gisc/mailroom/common';
+import { NotificationService } from '@tamu-gisc/common/ngx/ui/notification';
 
 @Component({
   selector: 'tamu-gisc-detail',
@@ -14,14 +17,20 @@ import { MailroomAttachment, MailroomEmail } from '@tamu-gisc/mailroom/common';
 })
 export class DetailComponent implements OnInit {
   public $email: Observable<MailroomEmail>;
+  private emailId: number;
 
-  constructor(private route: ActivatedRoute, private service: EmailService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private service: EmailService,
+    private ns: NotificationService,
+    private location: Location
+  ) {}
 
   public ngOnInit() {
     this.$email = this.route.params.pipe(
       map((params) => params['id']),
       switchMap((id) => {
-        console.log(id);
+        this.emailId = id;
         return this.service.getEmailWithAttachment(id);
       }),
       shareReplay(1)
@@ -31,5 +40,14 @@ export class DetailComponent implements OnInit {
   public attachmentToImage(attachment: MailroomAttachment) {
     const base64 = Buffer.from(attachment.blob.data as Uint8Array).toString('base64');
     return `"data:${attachment.mimeType};base64,${base64}"`;
+  }
+
+  public deleteEmail() {
+    console.log('Deleting email...');
+    this.service.deleteEmail(this.emailId).subscribe(() => {
+      this.ns.preset('deleted_email_response');
+
+      this.location.back();
+    });
   }
 }
