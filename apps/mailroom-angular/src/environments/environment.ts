78c2d5c49f3772cb42a56c2c54c4709e8fa502f9
atharmon <atharmon@tamu.edu>
2022-07-21 12:31:47 -0500

    Fixed duplicate notifications across env files

--- apps/mailroom-angular/src/environments/environment.ts
+++ apps/mailroom-angular/src/environments/environment.ts
@@ -1,13 +1,4 @@
-import { NotificationProperties } from '@tamu-gisc/common/ngx/ui/notification';
-
-const NotificationEvents: NotificationProperties[] = [
-  {
-    id: 'deleted_email_response',
-    title: 'Conversation removed',
-    acknowledge: false,
-    message: ''
-  }
-];
+import { NotificationEvents } from './notifications';
 
 export const environment = {
   production: false,
---
+++ apps/mailroom-angular/src/environments/notifications.ts
@@ -0,0 +1,10 @@
+import { NotificationProperties } from '@tamu-gisc/common/ngx/ui/notification';
+
+export const NotificationEvents: NotificationProperties[] = [
+  {
+    id: 'deleted_email_response',
+    title: 'Conversation removed',
+    acknowledge: false,
+    message: ''
+  }
+];
