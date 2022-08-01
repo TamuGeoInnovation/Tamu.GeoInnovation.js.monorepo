cc4005ce45a5c715019ed80a5b32738f1aafb2f4
Edgar Hernandez <imacleopard2010@gmail.com>
2022-08-01 18:22:36 -0500

    Linting pass

--- libs/geoservices/ngx/src/lib/core/modules/forms/submit-bug-form/submit-bug-form.component.ts
+++ libs/geoservices/ngx/src/lib/core/modules/forms/submit-bug-form/submit-bug-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { NotificationService } from '@tamu-gisc/common/ngx/ui/notification';
 
 @Component({
   selector: 'tamu-gisc-submit-bug-form',
@@ -6,7 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./submit-bug-form.component.scss']
 })
 export class SubmitBugFormComponent implements OnInit {
-  constructor() {}
+  public form: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(private readonly fb: FormBuilder, private readonly ns: NotificationService) {}
+
+  public ngOnInit(): void {
+    this.form = this.fb.group({
+      fullName: ['', Validators.required],
+      email: ['', Validators.required],
+      subject: ['Bug report', Validators.required],
+      body: ['', Validators.required]
+    });
+  }
+
+  public sendMessage() {
+    this.form.disable();
+
+    this.ns.toast({
+      id: 'bug-report-message-sent',
+      title: 'Bug report sent successfully',
+      message: 'Thank you for your feedback!'
+    });
+  }
 }
