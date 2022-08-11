cfd7f8ec4638f796b806c5e66f57418fc00c2227
Edgar Hernandez <imacleopard2010@gmail.com>
2022-08-11 03:37:19 -0500

    Clone geocoding into address processing

--- libs/geoservices/ngx/src/lib/api/pages/address-processing/address-processing.component.ts
+++ libs/geoservices/ngx/src/lib/api/pages/address-processing/address-processing.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
 
+import { AddressParser } from '@tamu-gisc/common/utils/geometry/geoprocessing';
+
 @Component({
   selector: 'tamu-gisc-address-processing',
   templateUrl: './address-processing.component.html',
   styleUrls: ['./address-processing.component.scss']
 })
-export class AddressProcessingComponent {}
+export class AddressProcessingComponent {
+  public apiVersion: '4.01' = '4.01';
+  public url = 'https://geoservices.tamu.edu/Services/AddressNormalization/WebService/v04_01/Rest';
+
+  public apiKey = 'demo';
+
+  public runner: AddressParser = new AddressParser({
+    apiKey: this.apiKey,
+    version: '4.10',
+    nonParsedStreetAddress: '9355 Burton Way'
+  });
+}
