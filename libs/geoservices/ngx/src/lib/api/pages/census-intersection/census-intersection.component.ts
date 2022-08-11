9271bce46a1ca6ba1bcfae0c6d8647e771418ef3
Edgar Hernandez <imacleopard2010@gmail.com>
2022-08-11 02:50:00 -0500

    Added boilerplate api pages for remaining services

---
+++ libs/geoservices/ngx/src/lib/api/pages/census-intersection/census-intersection.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tamu-gisc-census-intersection',
+  templateUrl: './census-intersection.component.html',
+  styleUrls: ['./census-intersection.component.scss']
+})
+export class CensusIntersectionComponent {}
---
+++ libs/geoservices/ngx/src/lib/api/pages/reverse-geocoding/reverse-geocoding.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReverseGeocodingComponent } from './reverse-geocoding.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ReverseGeocodingComponent]
+})
+export class ReverseGeocodingModule {}
