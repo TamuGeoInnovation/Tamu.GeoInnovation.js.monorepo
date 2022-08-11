5c991e7000478affadad8af6d78489af111caaf1
Edgar Hernandez <imacleopard2010@gmail.com>
2022-08-11 02:54:37 -0500

    Added basic routing for all service api pages

--- libs/geoservices/ngx/src/lib/api/pages/census-intersection/census-intersection.module.ts
+++ libs/geoservices/ngx/src/lib/api/pages/census-intersection/census-intersection.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
 import { CensusIntersectionComponent } from './census-intersection.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: CensusIntersectionComponent
+  }
+];
+
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule.forChild(routes)],
   declarations: [CensusIntersectionComponent]
 })
 export class CensusIntersectionModule {}
