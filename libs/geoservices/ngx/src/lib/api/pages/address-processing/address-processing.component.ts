ee5b2a7c00afd60bde0223da2e8f62d4b6136a87
Edgar Hernandez <imacleopard2010@gmail.com>
2022-08-16 13:00:32 -0500

    Add address processing parameters

--- libs/geoservices/ngx/src/lib/api/pages/address-processing/address-processing.component.ts
+++ libs/geoservices/ngx/src/lib/api/pages/address-processing/address-processing.component.ts
@@ -8,7 +8,7 @@ import { AddressParser } from '@tamu-gisc/common/utils/geometry/geoprocessing';
   styleUrls: ['./address-processing.component.scss']
 })
 export class AddressProcessingComponent {
-  public apiVersion: '4.01' = '4.01';
+  public apiVersion: '4.10' = '4.10';
   public url = 'https://geoservices.tamu.edu/Services/AddressNormalization/WebService/v04_01/Rest';
 
   public apiKey = 'demo';
@@ -19,3 +19,4 @@ export class AddressProcessingComponent {
     nonParsedStreetAddress: '9355 Burton Way'
   });
 }
+
