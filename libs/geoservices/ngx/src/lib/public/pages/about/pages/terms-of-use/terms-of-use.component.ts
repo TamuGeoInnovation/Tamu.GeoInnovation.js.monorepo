ae7c28939f90f22b7772f98e4bbe72b2f0cc8559
Edgar Hernandez <imacleopard2010@gmail.com>
2022-09-14 03:59:34 -0500

    Add about/termsofuse page

---
+++ libs/geoservices/ngx/src/lib/public/pages/about/pages/terms-of-use/terms-of-use.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tamu-gisc-terms-of-use',
+  templateUrl: './terms-of-use.component.html',
+  styleUrls: ['./terms-of-use.component.scss']
+})
+export class TermsOfUseComponent {}
+
--- libs/geoservices/ngx/src/lib/public/pages/contact/contact.module.ts
+++ libs/geoservices/ngx/src/lib/public/pages/contact/contact.module.ts
@@ -2,37 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ContactComponent } from './contact.component';
-
 const routes: Routes = [
   {
+    path: 'address-correction',
+    loadChildren: () => import('./pages/address-correction/address-correction.module').then((m) => m.AddressCorrectionModule)
+  },
+  {
+    path: 'bug-report',
+    loadChildren: () => import('./pages/bug-report/bug-report.module').then((m) => m.BugReportModule)
+  },
+  {
+    path: 'partnership',
+    loadChildren: () => import('./pages/partner/partner.module').then((m) => m.PartnerModule)
+  },
+  {
     path: '',
-    component: ContactComponent,
-    children: [
-      {
-        path: 'address-correction',
-        loadChildren: () =>
-          import('./pages/address-correction/address-correction.module').then((m) => m.AddressCorrectionModule)
-      },
-      {
-        path: 'bug-report',
-        loadChildren: () => import('./pages/bug-report/bug-report.module').then((m) => m.BugReportModule)
-      },
-      {
-        path: 'partnership',
-        loadChildren: () => import('./pages/partner/partner.module').then((m) => m.PartnerModule)
-      },
-      {
-        path: '',
-        pathMatch: 'full',
-        loadChildren: () => import('./pages/contact-us/contact-us.module').then((m) => m.ContactUsModule)
-      }
-    ]
+    pathMatch: 'full',
+    loadChildren: () => import('./pages/contact-us/contact-us.module').then((m) => m.ContactUsModule)
   }
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes)],
-  declarations: [ContactComponent]
+  imports: [CommonModule, RouterModule.forChild(routes)]
 })
 export class ContactModule {}
