25dfd038091c7e6d704f26706951c50b2f6e4e9a
Edgar Hernandez <imacleopard2010@gmail.com>
2022-09-19 23:08:15 -0500

    Generated geoprocessing v4 lib

---
+++ libs/geoprocessing/v4/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'geoprocessing-v4',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json'
+    }
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/geoprocessing/v4'
+};
