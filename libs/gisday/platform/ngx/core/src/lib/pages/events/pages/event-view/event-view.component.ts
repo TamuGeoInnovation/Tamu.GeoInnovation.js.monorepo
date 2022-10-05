70847561dac14b97fcb7c51e1291cfc634c296a1
atharmon <atharmon@tamu.edu>
2022-10-05 17:09:12 -0500

    Little bit of code clean up event-view

--- libs/gisday/platform/ngx/core/src/lib/pages/events/pages/event-view/event-view.component.ts
+++ libs/gisday/platform/ngx/core/src/lib/pages/events/pages/event-view/event-view.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { Observable, Subject } from 'rxjs';
-import { concatMap, filter, mergeAll, shareReplay, switchMap, takeUntil, tap, toArray } from 'rxjs/operators';
+import { filter, shareReplay, switchMap, takeUntil, toArray } from 'rxjs/operators';
 
 import { Event, Tag } from '@tamu-gisc/gisday/platform/data-api';
-import { EventResponse, EventService, TagService } from '@tamu-gisc/gisday/platform/ngx/data-access';
-import { flatten } from '@nestjs/common';
+import { EventService, TagService } from '@tamu-gisc/gisday/platform/ngx/data-access';
 
 @Component({
   selector: 'tamu-gisc-event-view',
-  templateUrl: './event-view-new.component.html',
+  templateUrl: './event-view.component.html',
   styleUrls: ['./event-view.component.scss']
 })
 export class EventViewComponent implements OnInit, OnDestroy {
@@ -31,13 +30,7 @@ export class EventViewComponent implements OnInit, OnDestroy {
   }
 
   public fetchEvents() {
-    this.$events = this.eventService.getEvents().pipe(
-      // switchMap((events) => events),
-      // tap((event) => console.log(event.day)),
-      // concatMap(),
-      takeUntil(this._$destroy),
-      shareReplay(1)
-    );
+    this.$events = this.eventService.getEvents().pipe(takeUntil(this._$destroy), shareReplay(1));
   }
 
   public fetchTags() {
@@ -58,19 +51,6 @@ export class EventViewComponent implements OnInit, OnDestroy {
     }
   }
 
-  /**
-   * Returns an observable of Event[] that only contains those Events of the day specified (day 1, day 2, day 3)
-   * @param dayNum
-   * @returns
-   */
-  public getDay(dayNum: number) {
-    return this.$events.pipe(
-      switchMap((events) => events),
-      filter((event) => event.day == dayNum),
-      toArray()
-    );
-  }
-
   public filterEventsByFilterTags(event: Event) {
     // if no filter tags are applied, show all events
     if (this.filterTags.length === 0) {
@@ -86,6 +66,20 @@ export class EventViewComponent implements OnInit, OnDestroy {
         }
       });
     });
+
     return ret;
+  }
+
+  /**
+   * Returns an observable of Event[] that only contains those Events of the day specified (day 1, day 2, day 3)
+   * @param dayNum
+   * @returns
+   */
+  public getDay(dayNum: number) {
+    return this.$events.pipe(
+      switchMap((events) => events),
+      filter((event) => event.day == dayNum),
+      toArray()
+    );
   }
 }
