7365632458859e2fcabfbbe8bae5e7aa30ae6e0e
Edgar Hernandez <imacleopard2010@gmail.com>
2022-10-05 01:12:58 -0500

    Update reverse geocoding docs to use attr accordion component

--- libs/geoservices/ngx/src/lib/api/geoservices-api.component.ts
+++ libs/geoservices/ngx/src/lib/api/geoservices-api.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { ResponsiveService } from '@tamu-gisc/dev-tools/responsive';
 
 @Component({
   selector: 'tamu-gisc-api',
   templateUrl: './geoservices-api.component.html',
   styleUrls: ['./geoservices-api.component.scss']
 })
-export class GeoservicesApiComponent {}
+export class GeoservicesApiComponent implements OnInit {
+  public mobile: Observable<boolean>;
+
+  constructor(private readonly rs: ResponsiveService) {}
+  public ngOnInit(): void {
+    this.mobile = this.rs.isMobile;
+  }
+}
