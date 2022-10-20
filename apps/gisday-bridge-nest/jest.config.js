e14bc105d633b8a89f8ab5fc5d57ea0918f4790a
atharmon <atharmon@tamu.edu>
2022-10-20 12:51:57 -0500

    Initial creation of gisday-bridge-nest

---
+++ apps/gisday-bridge-nest/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'gisday-bridge-nest',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json'
+    }
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/gisday-bridge-nest'
+};
