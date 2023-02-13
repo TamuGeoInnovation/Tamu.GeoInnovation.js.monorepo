8772c5de85fff6788d4897c9db07a265bafbaf0e
Edgar Hernandez <imacleopard2010@gmail.com>
2023-02-13 16:17:01 -0600

    Fix env url endpoints

--- apps/geoservices-angular/src/environments/environment.prod.ts
+++ apps/geoservices-angular/src/environments/environment.prod.ts
@@ -3,8 +3,7 @@ export const environment = {
 };
 
 export const api_url = `https://geoservices.tamu.edu/api/`;
-export const legacy_api_url = `https://geoservices.geoservices.tamu.edu/rest/`;
-export const accounts_url = `https://geoservices.geoservices.tamu.edu`;
+export const legacy_api_url = `https://geoservices.tamu.edu/rest/`;
+export const accounts_url = `https://geoservices.tamu.edu`;
 
 export * from './definitions';
-
