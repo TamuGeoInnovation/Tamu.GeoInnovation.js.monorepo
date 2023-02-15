1e50774dd8a1035c20d2e019c6561562385032ac
Edgar Hernandez <imacleopard2010@gmail.com>
2023-02-15 16:05:23 -0600

    Interactive reverse geocoding component

---
+++ libs/geoservices/ngx/src/lib/core/modules/interactive/components/reverse-geocoding/basic/reverse-geocoding-basic/reverse-geocoding-basic.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReverseGeocodingBasicComponent } from './reverse-geocoding-basic.component';
+
+describe('ReverseGeocodingBasicComponent', () => {
+  let component: ReverseGeocodingBasicComponent;
+  let fixture: ComponentFixture<ReverseGeocodingBasicComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ReverseGeocodingBasicComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ReverseGeocodingBasicComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
