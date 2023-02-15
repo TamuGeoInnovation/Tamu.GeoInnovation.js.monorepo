6e1bb6f872d93fb901582899571b7ca14d73fc3a
Edgar Hernandez <imacleopard2010@gmail.com>
2023-02-15 04:41:34 -0600

    Update more relative app urls

--- libs/geoservices/ngx/src/lib/public/geoservices-public.component.ts
+++ libs/geoservices/ngx/src/lib/public/geoservices-public.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
 
 import { ResponsiveService } from '@tamu-gisc/dev-tools/responsive';
+import { EnvironmentService } from '@tamu-gisc/common/ngx/environment';
 
 @Component({
   selector: 'tamu-gisc-geoservices-public',
   templateUrl: './geoservices-public.component.html',
   styleUrls: ['./geoservices-public.component.scss']
 })
-export class GeoservicesPublicComponent {
+export class GeoservicesPublicComponent implements OnInit {
   public mobileNavToggle: Subject<boolean> = new Subject();
 
-  constructor(public rp: ResponsiveService) {}
+  public url: string;
+
+  constructor(public rp: ResponsiveService, private readonly env: EnvironmentService) {}
+
+  public ngOnInit() {
+    this.url = this.env.value('accounts_url');
+  }
 }
