c790bad162ae605c48288237acf66cfb0b33b2ad
Edgar Hernandez <imacleopard2010@gmail.com>
2023-03-28 15:13:00 -0500

    Establish repeating reactive processing state
    - Using dummy response values for reverse geocoding result

--- libs/geoservices/ngx/src/lib/core/modules/interactive/components/reverse-geocoding/basic/reverse-geocoding-basic/reverse-geocoding-basic.component.ts
+++ libs/geoservices/ngx/src/lib/core/modules/interactive/components/reverse-geocoding/basic/reverse-geocoding-basic/reverse-geocoding-basic.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { delay, map, mapTo, Observable, pipe, shareReplay, startWith, Subject, switchMap, tap } from 'rxjs';
+import { Observable, of, pipe, Subject, merge } from 'rxjs';
+import { map, catchError, shareReplay, startWith, switchMap, delay, timeout, mapTo } from 'rxjs/operators';
 
 import { STATES_TITLECASE } from '@tamu-gisc/common/datasets/geographic';
 import { ReverseGeocode } from '@tamu-gisc/geoprocessing-v5';
@@ -29,33 +30,39 @@ export class ReverseGeocodingBasicComponent implements OnInit {
       state: [null]
     });
 
-    // this.result = this.querySubmit.pipe(this.getQuery(this.form.getRawValue()), shareReplay());
-    this.result = this.querySubmit.pipe(
-      delay(2500),
-      mapTo({
-        address: '1234 Main St',
-        city: 'Austin',
-        state: 'TX',
-        zip: '78701'
-      }),
-      tap((r) => {
-        this.querySubmit.next(undefined);
-      })
-    );
+    this.result = this.querySubmit.pipe(this.getQuery(), shareReplay());
 
     this.processing = this.querySubmit.pipe(
-      map((r) => {
-        console.log('sffs');
-
-        return r === null;
+      switchMap(() => {
+        return of(true).pipe(
+          this.getQuery(),
+          // Once the query has resolved, set processing state to false.
+          map(() => {
+            return false;
+          }),
+          // If the query errors, set processing state to false.
+          catchError(() => of(false)),
+          // Set initial processing state to true while waiting for resolution of the query.
+          startWith(true)
+        );
       }),
+      // Set initial processing state to false. This is the value used before any queries are submitted.
       startWith(false),
+      // This stream is used by the template and other subscribers in the component. We want to ensure that the stream
+      // is shared and replayed so that the value is not re-emitted on each subscription.
       shareReplay()
     );
 
-    this.buttonLanguage = this.processing.pipe(
+    this.buttonLanguage = merge(this.processing, this.result).pipe(
       map((processing) => {
-        return processing ? 'Processing...' : 'Solve';
+        if (typeof processing === 'boolean') {
+          return processing ? 'Processing...' : 'Solve';
+        } else {
+          return 'Solve';
+        }
+      }),
+      catchError(() => {
+        return of('Error. Please try again.');
       })
     );
   }
@@ -64,17 +71,55 @@ export class ReverseGeocodingBasicComponent implements OnInit {
     this.querySubmit.next(null);
   }
 
-  public getQuery(fv: any) {
+  public getQuery() {
     return pipe(
       switchMap(() => {
-        return new ReverseGeocode({
-          apiKey: 'demo',
-          latitude: fv.lat,
-          longitude: fv.lon,
-          state: fv.state || undefined
-        }).asObservable();
+        return of({
+          statusCode: 200,
+          message: 'Success',
+          error: '',
+          data: {
+            version: {
+              major: 5,
+              minor: 0,
+              build: 0,
+              revision: -1,
+              majorRevision: -1,
+              minorRevision: -1
+            },
+            timeTaken: 171.88160000000002,
+            transactionGuid: '31405f77-a6c9-444a-82dc-22ef1eee86d7',
+            apiHost: 'geoservices.tamu.edu',
+            clientHost: 'geoservices.tamu.edu',
+            queryStatusCode: 'Success',
+            results: [
+              {
+                timeTaken: 0,
+                exceptionOccurred: false,
+                errorMessage: '',
+                apn: '4342011022',
+                streetAddress: '9309 Burton Way',
+                city: 'Los Angeles',
+                state: 'CA',
+                zip: '90209',
+                zipPlus4: '3605'
+              }
+            ]
+          }
+        }).pipe(delay(1500));
+
+        const fv = this.form.getRawValue();
+
+        // return new ReverseGeocode({
+        //   apiKey: 'demo',
+        //   latitude: fv.lat,
+        //   longitude: fv.lon,
+        //   state: fv.state || undefined
+        // }).asObservable();
+      }),
+      catchError((e) => {
+        return of(e);
       })
     );
   }
 }
-
