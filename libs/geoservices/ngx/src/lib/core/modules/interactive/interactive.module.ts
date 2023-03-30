2c1a0aac8db05d9a04fc61f9f7084203643435a9
Edgar Hernandez <imacleopard2010@gmail.com>
2023-03-29 19:57:30 -0500

    Add results table to interactive component
    Add results and map tab

--- libs/geoservices/ngx/src/lib/core/modules/interactive/interactive.module.ts
+++ libs/geoservices/ngx/src/lib/core/modules/interactive/interactive.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { UIFormsModule } from '@tamu-gisc/ui-kits/ngx/forms';
-
-import { ReverseGeocodingBasicComponent } from './components/reverse-geocoding/basic/reverse-geocoding-basic/reverse-geocoding-basic.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { UIFormsModule } from '@tamu-gisc/ui-kits/ngx/forms';
+import { UILayoutModule } from '@tamu-gisc/ui-kits/ngx/layout';
+import { EsriMapModule } from '@tamu-gisc/maps/esri';
+
+import { ReverseGeocodingBasicComponent } from './components/reverse-geocoding/basic/reverse-geocoding-basic/reverse-geocoding-basic.component';
+
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule, UIFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, UIFormsModule, UILayoutModule, EsriMapModule],
   declarations: [ReverseGeocodingBasicComponent],
   exports: [ReverseGeocodingBasicComponent]
 })
 export class GeoservicesCoreInteractiveModule {}
-
