30ff82ed1d1b8806bc90928a81a41afcfedde74c
Edgar Hernandez <imacleopard2010@gmail.com>
2023-04-06 00:07:05 -0500

    Add boilerplate interactive route pages
    These are the pages that will have the full/advanced interactive geoprocessing tools

---
+++ libs/geoservices/ngx/src/lib/public/pages/services/pages/census-intersection/pages/interactive/interactive.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InteractiveComponent } from './interactive.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [InteractiveComponent]
+})
+export class InteractiveModule {}
+
---
+++ libs/geoservices/ngx/src/lib/public/pages/services/pages/geocoding/pages/interactive/interactive.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tamu-gisc-interactive',
+  templateUrl: './interactive.component.html',
+  styleUrls: ['./interactive.component.scss']
+})
+export class InteractiveComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
+
