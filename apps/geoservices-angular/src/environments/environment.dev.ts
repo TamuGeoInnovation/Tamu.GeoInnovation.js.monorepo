fb7f3d01e5010d8c06de812ed37701e51d1d746e
Edgar Hernandez <imacleopard2010@gmail.com>
2023-05-17 15:24:29 -0500

    Update gsvcs dev and prod endpoints

--- apps/geoservices-angular/src/environments/environment.dev.ts
+++ apps/geoservices-angular/src/environments/environment.dev.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true
 };
 
-export const api_url = `https://geoservices.tamu.edu/api/`;
-export const legacy_api_url = `https://dev.geoservices.tamu.edu/rest/`;
-export const accounts_url = `https://dev.geoservices.tamu.edu`;
+export const api_url = `https://nodes.geoservices.tamu.edu/api/transport/`;
+export const legacy_api_url = `https://geoservices-dev.geoservices.tamu.edu/rest/`;
+export const accounts_url = `https://geoservices-dev.geoservices.tamu.edu`;
 
 export * from './definitions';
