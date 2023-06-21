be7a2df547b8f7d2d681afa660171340a1dbe9d7
Edgar Hernandez <imacleopard2010@gmail.com>
2023-06-21 16:31:38 -0500

    Add boilerplate sidebar component

---
+++ libs/aggiemap/ngx/ui/desktop/src/lib/modules/sidebar/components/sidebar-menu/sidebar-menu.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tamu-gisc-sidebar-menu',
+  templateUrl: './sidebar-menu.component.html',
+  styleUrls: ['./sidebar-menu.component.scss']
+})
+export class SidebarMenuComponent {}
+
