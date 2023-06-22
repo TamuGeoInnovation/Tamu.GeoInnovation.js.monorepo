4c4bbbf36ff05020e207c0b6f8e1eaa97a59ab8b
Edgar Hernandez <imacleopard2010@gmail.com>
2023-06-22 01:48:20 -0500

    Add suport for browser history navigation through levels
    Allows for stateless link loading

--- libs/aggiemap/ngx/ui/desktop/src/lib/modules/sidebar/components/sidebar-menu/sidebar-menu.component.ts
+++ libs/aggiemap/ngx/ui/desktop/src/lib/modules/sidebar/components/sidebar-menu/sidebar-menu.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, ReplaySubject, Subject, map, startWith, switchMap } from 'rxjs';
+import { Observable, Subject, map, of, shareReplay, switchMap } from 'rxjs';
 
 import {
   CategoryEntry,
@@ -9,6 +9,7 @@ import {
   LocationService
 } from '@tamu-gisc/aggiemap/ngx/data-access';
 import { EnvironmentService } from '@tamu-gisc/common/ngx/environment';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'tamu-gisc-sidebar-menu',
@@ -16,44 +17,70 @@ import { EnvironmentService } from '@tamu-gisc/common/ngx/environment';
   styleUrls: ['./sidebar-menu.component.scss']
 })
 export class SidebarMenuComponent implements OnInit {
-  public parent: ReplaySubject<CategoryEntry> = new ReplaySubject(1);
+  public parent: Observable<CategoryEntry>;
+  public parentId: Observable<number>;
   public levelText: Observable<string> = new Subject();
 
   public categories: Observable<CmsResponse<CategoryEntry>>;
   public locations: Observable<CmsResponse<LocationEntry>>;
   public assetsUrl: string;
 
-  constructor(private readonly cs: CategoryService, private readonly ls: LocationService, private env: EnvironmentService) {}
+  constructor(
+    private readonly cs: CategoryService,
+    private readonly ls: LocationService,
+    private env: EnvironmentService,
+    private readonly router: Router,
+    private readonly route: ActivatedRoute
+  ) {}
 
   public ngOnInit(): void {
     this.assetsUrl = this.env.value('Connections').cms_base;
 
-    this.categories = this.parent.pipe(
-      startWith(0),
-      switchMap((parent) => {
-        if (parent === 0) {
+    this.parentId = this.route.queryParams.pipe(
+      map((params) => {
+        if (params.parent) {
+          return params.parent;
+        } else {
+          return 0;
+        }
+      }),
+      shareReplay()
+    );
+
+    this.parent = this.parentId.pipe(
+      switchMap((id) => {
+        if (id === 0) {
+          return of(null);
+        } else {
+          return this.cs.getCategory(id).pipe(map((res) => res.data[0]));
+        }
+      }),
+      shareReplay()
+    );
+
+    this.categories = this.parentId.pipe(
+      switchMap((id) => {
+        if (id === 0) {
           return this.cs.getCategories();
         } else {
-          return this.cs.getCategories((parent as CategoryEntry).attributes.catId);
+          return this.cs.getCategories(id);
         }
       })
     );
 
-    this.locations = this.parent.pipe(
-      startWith(0),
-      switchMap((parent) => {
-        if (parent === 0) {
+    this.locations = this.parentId.pipe(
+      switchMap((id) => {
+        if (id === 0) {
           return this.ls.getLocations();
         } else {
-          return this.ls.getLocations((parent as CategoryEntry).attributes.catId);
+          return this.ls.getLocations(id);
         }
       })
     );
 
     this.levelText = this.parent.pipe(
-      startWith(0),
       map((parent) => {
-        if (parent === 0) {
+        if (!parent) {
           return 'Main';
         } else {
           return (parent as CategoryEntry).attributes.name;
@@ -65,7 +92,11 @@ export class SidebarMenuComponent implements OnInit {
   public setParent(parent: CategoryEntry) {
     // Toggles do not have render-able children, so we don't want to drill down to the next level
     if (parent.attributes.type !== 'toggle') {
-      this.parent.next(parent);
+      this.router.navigate([], {
+        relativeTo: this.route,
+        queryParams: { parent: parent.attributes.catId },
+        queryParamsHandling: 'merge'
+      });
     }
   }
 }
