ecf416d77b7b86a99a081301e5fc809c44a0a125
Edgar Hernandez <imacleopard2010@gmail.com>
2023-07-19 20:47:37 -0500

    Move active layer filtering and mapping to menu svc
    Location status pipe will also need to access the active layers list

--- libs/aggiemap/ngx/ui/desktop/src/lib/modules/sidebar/pipes/category-status/category-status.pipe.ts
+++ libs/aggiemap/ngx/ui/desktop/src/lib/modules/sidebar/pipes/category-status/category-status.pipe.ts
@@ -1,37 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { filter, from, map, Observable, switchMap, toArray, withLatestFrom } from 'rxjs';
+import { map, Observable, withLatestFrom } from 'rxjs';
 
-import { LayerListService } from '@tamu-gisc/maps/feature/layer-list';
 import { CategoryEntry } from '@tamu-gisc/aggiemap/ngx/data-access';
 
 import { CategoryLocationMenuService } from '../../services/category-location-menu/category-location-menu.service';
-
-import esri = __esri;
 
 @Pipe({
   name: 'categoryStatus'
 })
 export class CategoryStatusPipe implements PipeTransform {
-  private _activeLayerIds = this.ls.layers().pipe(
-    switchMap((items) => {
-      return from(items).pipe(
-        // Only filter out category layers
-        filter((item) => item.layer.id.startsWith('cat-')),
-
-        // Filter out layers with no graphics. This is to reflect a layer that has been turned off.
-        filter((item) => (item.layer as esri.GraphicsLayer).graphics.length > 0),
-
-        // Map to layer id
-        map((layer) => layer.layer.id),
-        toArray()
-      );
-    })
-  );
-
-  constructor(private readonly ms: CategoryLocationMenuService, private readonly ls: LayerListService) {}
+  constructor(private readonly ms: CategoryLocationMenuService) {}
 
   public transform(category: CategoryEntry): Observable<boolean> {
-    return this._activeLayerIds.pipe(
+    return this.ms.layerIds.pipe(
       withLatestFrom(this.ms.categoriesDictionary),
       map(([layerIds, catDict]) => {
         const isCategoryActive = this._isCategoryActive(category, layerIds);
