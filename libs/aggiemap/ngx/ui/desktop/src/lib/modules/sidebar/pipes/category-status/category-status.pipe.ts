95a910c958a16836221e74fd8022d8d107432919
Edgar Hernandez <imacleopard2010@gmail.com>
2023-07-19 19:11:21 -0500

    Limit cat status pipe to layers with graphics
    Omit those layers with graphic counts of 0. This is because no layers are actually removed when graphics are cleaned up from a layer.
    
    This behavior might change in the future

--- libs/aggiemap/ngx/ui/desktop/src/lib/modules/sidebar/pipes/category-status/category-status.pipe.ts
+++ libs/aggiemap/ngx/ui/desktop/src/lib/modules/sidebar/pipes/category-status/category-status.pipe.ts
@@ -1,18 +1,27 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { from, map, Observable, switchMap, toArray } from 'rxjs';
+import { filter, from, map, Observable, switchMap, toArray } from 'rxjs';
 
 import { LayerListService } from '@tamu-gisc/maps/feature/layer-list';
 import { CategoryEntry } from '@tamu-gisc/aggiemap/ngx/data-access';
 
 import { CategoryLocationMenuService } from '../../services/category-location-menu/category-location-menu.service';
 
+import esri = __esri;
+
 @Pipe({
   name: 'categoryStatus'
 })
 export class CategoryStatusPipe implements PipeTransform {
   private _activeLayerIds = this.ls.layers().pipe(
-    switchMap((layers) => {
-      return from(layers).pipe(
+    switchMap((items) => {
+      return from(items).pipe(
+        // Only filter out category layers
+        filter((item) => item.layer.id.startsWith('cat-')),
+
+        // Filter out layers with no graphics. This is to reflect a layer that has been turned off.
+        filter((item) => (item.layer as esri.GraphicsLayer).graphics.length > 0),
+
+        // Map to layer id
         map((layer) => layer.layer.id),
         toArray()
       );
