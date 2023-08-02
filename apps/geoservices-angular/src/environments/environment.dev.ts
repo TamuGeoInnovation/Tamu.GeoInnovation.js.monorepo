85e7dd5989a39c596b8df6c522871e54789b5f5a
Edgar Hernandez <Proantagonist@users.noreply.github.com>
2023-08-02 01:42:40 -0500

    fix(geoservices-angular): Add missing dev env config (#338)

---
+++ apps/geoservices-angular/src/environments/environment.dev.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true
+};
+
+export const api_url = `${window.location.protocol}//${window.location.hostname}/geoservices.tamu.edu/rest/`;
+
+export const covid_api_url = `https://nodes.geoservices.tamu.edu/api/covid/`;
+export const auth_url = covid_api_url;
+
