313f64010e02bcb5ccbd185e3d5d8ec25ec1f5ea
Edgar Hernandez <imacleopard2010@gmail.com>
2023-08-02 01:55:37 -0500

    Add geoservices-angular to ci jobs
    Add env target and environment

--- apps/geoservices-angular/src/environments/environment.dev.ts
+++ apps/geoservices-angular/src/environments/environment.dev.ts
@@ -2,8 +2,9 @@ export const environment = {
   production: true
 };
 
-export const api_url = `${window.location.protocol}//${window.location.hostname}/geoservices.tamu.edu/rest/`;
+export const api_url = `https://geoservices.tamu.edu/api/`;
+export const legacy_api_url = `https://geoservices-dev.geoservices.tamu.edu/rest/`;
+export const accounts_url = `https://geoservices-dev.geoservices.tamu.edu`;
 
-export const covid_api_url = `https://nodes.geoservices.tamu.edu/api/covid/`;
-export const auth_url = covid_api_url;
+export * from './definitions';
 
