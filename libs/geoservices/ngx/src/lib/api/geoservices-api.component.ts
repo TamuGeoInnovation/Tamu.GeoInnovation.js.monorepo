1fd3d24c5cce9b8c3a646dc87f129c54bb98fb5e
Edgar Hernandez <imacleopard2010@gmail.com>
2023-08-03 01:52:29 -0500

    docs: Reset scroll position on navigation

--- libs/geoservices/ngx/src/lib/api/geoservices-api.component.ts
+++ libs/geoservices/ngx/src/lib/api/geoservices-api.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, Subject, filter, takeUntil } from 'rxjs';
 
 import { ResponsiveService } from '@tamu-gisc/dev-tools/responsive';
 
@@ -8,12 +9,32 @@ import { ResponsiveService } from '@tamu-gisc/dev-tools/responsive';
   templateUrl: './geoservices-api.component.html',
   styleUrls: ['./geoservices-api.component.scss']
 })
-export class GeoservicesApiComponent implements OnInit {
+export class GeoservicesApiComponent implements OnInit, OnDestroy {
+  @ViewChild('scrollContainer', { static: true })
+  private container: ElementRef;
+
   public mobile: Observable<boolean>;
   public mobileNavToggle: Subject<boolean> = new Subject();
 
-  constructor(private readonly rs: ResponsiveService) {}
+  private _$destroy: Subject<boolean> = new Subject();
+
+  constructor(private readonly rs: ResponsiveService, private readonly rt: Router) {}
   public ngOnInit(): void {
     this.mobile = this.rs.isMobile;
+
+    // Because we are using an abnormal layout, we need to manually scroll to the top of the page on navigation.
+    this.rt.events
+      .pipe(
+        filter((event) => event.constructor.name === 'NavigationEnd'),
+        takeUntil(this._$destroy)
+      )
+      .subscribe(() => {
+        this.container.nativeElement.scrollTop = 0;
+      });
+  }
+
+  public ngOnDestroy(): void {
+    this._$destroy.next(true);
+    this._$destroy.complete();
   }
 }
