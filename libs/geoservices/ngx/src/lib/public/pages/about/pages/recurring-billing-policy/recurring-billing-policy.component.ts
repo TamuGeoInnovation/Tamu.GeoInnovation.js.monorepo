2ce8af204b6a17ed68ad04160a6a7c438142d745
Edgar Hernandez <imacleopard2010@gmail.com>
2023-08-03 00:00:16 -0500

    Add recurring billing policy

---
+++ libs/geoservices/ngx/src/lib/public/pages/about/pages/recurring-billing-policy/recurring-billing-policy.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tamu-gisc-recurring-billing-policy',
+  templateUrl: './recurring-billing-policy.component.html',
+  styleUrls: ['./recurring-billing-policy.component.scss']
+})
+export class RecurringBillingPolicyComponent {}
+
