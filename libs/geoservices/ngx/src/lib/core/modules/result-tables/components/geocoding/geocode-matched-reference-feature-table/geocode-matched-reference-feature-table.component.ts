dde53ac16fdb0f8b0613f022e002985f5303a334
Edgar Hernandez <imacleopard2010@gmail.com>
2023-08-09 03:45:20 -0500

    Filter and order geocode matched feature props

--- libs/geoservices/ngx/src/lib/core/modules/result-tables/components/geocoding/geocode-matched-reference-feature-table/geocode-matched-reference-feature-table.component.ts
+++ libs/geoservices/ngx/src/lib/core/modules/result-tables/components/geocoding/geocode-matched-reference-feature-table/geocode-matched-reference-feature-table.component.ts
@@ -1,7 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Observable, filter, from, reduce } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
 
-import { IGeocodeReferenceFeature } from '@tamu-gisc/geoprocessing-v5';
+import { GeocodeReferenceFeatureField, IGeocodeReferenceFeature } from '@tamu-gisc/geoprocessing-v5';
+import { EnumeratorKeyValuePairs, FieldEnumerator } from '@tamu-gisc/common/utils/object';
 
 import { GeocodeReferenceFeatureLabel } from '../../../../../util/dictionaries';
 
@@ -14,21 +15,32 @@ export class GeocodeMatchedReferenceFeatureTableComponent implements OnInit {
   @Input()
   public matchedFeature: IGeocodeReferenceFeature;
 
-  public filteredProps: Observable<{ [key: string]: unknown }>;
+  public filteredProps: Observable<EnumeratorKeyValuePairs>;
 
   public referenceFeatureDict = GeocodeReferenceFeatureLabel;
+
+  private _excludedProps = [GeocodeReferenceFeatureField.Address, GeocodeReferenceFeatureField.Geometry];
+
+  private _defaultOrder = [
+    GeocodeReferenceFeatureField.Source,
+    GeocodeReferenceFeatureField.Vintage,
+    GeocodeReferenceFeatureField.Area,
+    GeocodeReferenceFeatureField.AreaType,
+    GeocodeReferenceFeatureField.GeometrySRID,
+    GeocodeReferenceFeatureField.PrimaryIdField,
+    GeocodeReferenceFeatureField.PrimaryIdValue,
+    GeocodeReferenceFeatureField.SecondaryIdField,
+    GeocodeReferenceFeatureField.SecondaryIdValue,
+    GeocodeReferenceFeatureField.ServerName
+  ];
 
   public ngOnInit(): void {
     // Set filteredProps to be an object of key-value pairs of the matched feature, but only for properties found
     // in the reference feature dictionary.
-    this.filteredProps = from(Object.entries(this.matchedFeature)).pipe(
-      filter(([key]) => {
-        return this.referenceFeatureDict[key] !== undefined;
-      }),
-      reduce((acc, [key, value]) => {
-        acc[key] = value;
-        return acc;
-      }, {})
+    this.filteredProps = of(this.matchedFeature).pipe(
+      map((feature) => {
+        return new FieldEnumerator(feature).filter('exclude', this._excludedProps).order(this._defaultOrder);
+      })
     );
   }
 }
