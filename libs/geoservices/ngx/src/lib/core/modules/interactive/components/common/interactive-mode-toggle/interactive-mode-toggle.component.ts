be94638039edee3a0333d5a5890323d4ec82379e
Edgar Hernandez <imacleopard2010@gmail.com>
2023-08-16 00:31:13 -0500

    Separate mode toggle from interactive address processing component

---
+++ libs/geoservices/ngx/src/lib/core/modules/interactive/components/common/interactive-mode-toggle/interactive-mode-toggle.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+import { ComponentMode } from '../base-interactive-geoprocessing/base-interactive-geoprocessing.component';
+
+@Component({
+  selector: 'tamu-gisc-interactive-mode-toggle',
+  templateUrl: './interactive-mode-toggle.component.html',
+  styleUrls: ['./interactive-mode-toggle.component.scss']
+})
+export class InteractiveModeToggleComponent {
+  @Input()
+  public mode: ComponentMode;
+
+  @Output()
+  public modeChange: EventEmitter<null> = new EventEmitter();
+
+  public toggleMode() {
+    this.modeChange.next(null);
+  }
+}
+
