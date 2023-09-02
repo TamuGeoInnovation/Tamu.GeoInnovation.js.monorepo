cb763a0b14086e9df2043e3d6f0a257899cb08b1
Edgar Hernandez <imacleopard2010@gmail.com>
2023-09-01 20:28:49 -0500

    Fix tile click navigation menu toggle
    Menu was not closing when a tile had a tile link directive

--- libs/ui-kits/ngx/navigation/mobile-tile/src/lib/components/tile/tile.component.ts
+++ libs/ui-kits/ngx/navigation/mobile-tile/src/lib/components/tile/tile.component.ts
@@ -1,20 +1,30 @@
-import { Component, ContentChild, HostListener } from '@angular/core';
+import { AfterContentInit, Component, ContentChild, HostListener, OnDestroy } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
 
 import { TileTitleComponent } from '../tile-title/tile-title.component';
 import { TileSubmenuDirective } from '../../directives/tile-submenu/tile-submenu.directive';
 import { TileService } from '../../services/tile.service';
+import { TileLinkDirective } from '../../directives/tile-link/tile-link.directive';
 
 @Component({
   selector: 'tamu-gisc-tile',
   templateUrl: './tile.component.html',
   styleUrls: ['./tile.component.scss']
 })
-export class TileComponent {
+export class TileComponent implements AfterContentInit, OnDestroy {
+  private _destroy$: Subject<boolean> = new Subject();
+
   @ContentChild(TileTitleComponent, { static: true })
   public title: TileTitleComponent;
 
+  // A tile can redirect to open a submenu. Need a reference to the submenu template to render it.
   @ContentChild(TileSubmenuDirective, { static: true })
   public submenu: TileSubmenuDirective;
+
+  // A tile can have a link directive to link directly to a location. Need a reference to the link directive
+  // to subscribe to its click event and toggle the menu.
+  @ContentChild(TileLinkDirective)
+  public link: TileLinkDirective;
 
   @HostListener('click')
   private _tileClick() {
@@ -29,4 +39,17 @@ export class TileComponent {
   }
 
   constructor(private service: TileService) {}
+
+  public ngAfterContentInit(): void {
+    if (this.link) {
+      this.link.clicked.pipe(takeUntil(this._destroy$)).subscribe(() => {
+        this.service.toggleMenu(false);
+      });
+    }
+  }
+
+  public ngOnDestroy(): void {
+    this._destroy$.next(null);
+    this._destroy$.complete();
+  }
 }
