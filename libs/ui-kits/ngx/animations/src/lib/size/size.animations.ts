d1e03d57bafd864025e53637e16d9ed5457c9d7a
Edgar Hernandez <imacleopard2010@gmail.com>
2023-09-12 04:28:40 -0500

    Documentation

--- libs/ui-kits/ngx/animations/src/lib/size/size.animations.ts
+++ libs/ui-kits/ngx/animations/src/lib/size/size.animations.ts
@@ -1,7 +1,12 @@
 import { trigger, transition, style, animate } from '@angular/animations';
 
-// Animation that makes the element grow by modifying the max-height attribute
-// Being at 0% height and 100% opacity at the start of the animation and 100% height and 0% opacity at the end
+/**
+ * Returns animation that makes the element grow by modifying the max-height attribute
+ *
+ * Being at 0% height and 100% opacity at the start of the animation and 100% height and 0% opacity at the end
+ * @param {number} maxPixelHeight
+ * @return {*}
+ */
 export const growAnimationBuilder = (maxPixelHeight: number) => {
   return trigger('growAnimation', [
     transition(':enter', [
