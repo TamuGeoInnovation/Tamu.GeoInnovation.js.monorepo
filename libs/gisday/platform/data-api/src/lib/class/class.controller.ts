81b2cec102d1b9eaa6bb87bf6f60626a3ca78c4e
Edgar Hernandez <imacleopard2010@gmail.com>
2023-09-17 21:05:27 -0500

    Pull BaseController from classes resource

--- libs/gisday/platform/data-api/src/lib/class/class.controller.ts
+++ libs/gisday/platform/data-api/src/lib/class/class.controller.ts
@@ -1,12 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotImplementedException, Param, Patch, Post } from '@nestjs/common';
+import { DeepPartial } from 'typeorm';
 
-import { Class } from '../entities/all.entity';
 import { ClassProvider } from './class.provider';
-import { BaseController } from '../_base/base.controller';
+import { Class } from '../entities/all.entity';
 
 @Controller('classes')
-export class ClassController extends BaseController<Class> {
-  constructor(private readonly classProvider: ClassProvider) {
-    super(classProvider);
+export class ClassController {
+  constructor(private readonly provider: ClassProvider) {}
+
+  @Get()
+  public async getClasses() {
+    return this.provider.find();
+  }
+
+  @Get(':guid')
+  public async getClass(@Param('guid') guid) {
+    return this.provider.findOne({
+      where: {
+        guid: guid
+      }
+    });
+  }
+
+  @Post()
+  public async insertClass(@Body() body: DeepPartial<Class>) {
+    return this.provider.save(body);
+  }
+
+  @Patch()
+  public async updateClass(@Body() body) {
+    throw new NotImplementedException();
+  }
+
+  @Delete(':guid')
+  public async deleteClass(@Param() params) {
+    return this.provider.deleteEntity(params.guid);
   }
 }
