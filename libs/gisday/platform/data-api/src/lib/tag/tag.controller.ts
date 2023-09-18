2165c9a30be79d6c7c4ae42b8286895a408d46bf
Edgar Hernandez <imacleopard2010@gmail.com>
2023-09-17 21:37:43 -0500

    Pull BaseController from tags resource

--- libs/gisday/platform/data-api/src/lib/tag/tag.controller.ts
+++ libs/gisday/platform/data-api/src/lib/tag/tag.controller.ts
@@ -1,18 +1,25 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotImplementedException, Param, Patch, Post } from '@nestjs/common';
+import { DeepPartial } from 'typeorm';
 
 import { Tag } from '../entities/all.entity';
 import { TagProvider } from './tag.provider';
-import { BaseController } from '../_base/base.controller';
 
 @Controller('tags')
-export class TagController extends BaseController<Tag> {
-  constructor(private readonly tagProvider: TagProvider) {
-    super(tagProvider);
-  }
+export class TagController {
+  constructor(private readonly provider: TagProvider) {}
 
   @Get()
   public async getTags() {
-    return this.tagProvider.getTags();
+    return this.provider.getTags();
+  }
+
+  @Get(':guid')
+  public async getEntity(@Param('guid') guid) {
+    return this.provider.findOne({
+      where: {
+        guid: guid
+      }
+    });
   }
 
   @Post('/bulk')
@@ -25,6 +32,21 @@ export class TagController extends BaseController<Tag> {
       return tag;
     });
 
-    return this.tagProvider.insertTags(_tags);
+    return this.provider.insertTags(_tags);
+  }
+
+  @Post()
+  public async insertEntity(@Body() body: DeepPartial<Tag>) {
+    throw new NotImplementedException();
+  }
+
+  @Patch()
+  public async updateEntity(@Body() body) {
+    throw new NotImplementedException();
+  }
+
+  @Delete(':guid')
+  public async deleteEntity(@Param() params) {
+    throw new NotImplementedException();
   }
 }
