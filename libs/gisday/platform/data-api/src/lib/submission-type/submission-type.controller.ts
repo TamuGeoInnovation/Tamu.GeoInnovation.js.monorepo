9982ed2d3a371da6d58f624df1667b2914d0d502
Edgar Hernandez <imacleopard2010@gmail.com>
2023-09-17 21:37:33 -0500

    Pull BaseController from submission types resource

--- libs/gisday/platform/data-api/src/lib/submission-type/submission-type.controller.ts
+++ libs/gisday/platform/data-api/src/lib/submission-type/submission-type.controller.ts
@@ -1,12 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotImplementedException, Param, Patch, Post } from '@nestjs/common';
+import { DeepPartial } from 'typeorm';
 
 import { SubmissionType } from '../entities/all.entity';
 import { SubmissionTypeProvider } from './submission-type.provider';
-import { BaseController } from '../_base/base.controller';
 
 @Controller('submission-types')
-export class SubmissionTypeController extends BaseController<SubmissionType> {
-  constructor(private readonly submissionTypeProvider: SubmissionTypeProvider) {
-    super(submissionTypeProvider);
+export class SubmissionTypeController {
+  constructor(private readonly provider: SubmissionTypeProvider) {}
+
+  @Get()
+  public async getEntities() {
+    return this.provider.find();
+  }
+
+  @Get(':guid')
+  public async getEntity(@Param('guid') guid) {
+    return this.provider.findOne({
+      where: {
+        guid: guid
+      }
+    });
+  }
+
+  @Post()
+  public async insertEntity(@Body() body: DeepPartial<SubmissionType>) {
+    throw new NotImplementedException();
+  }
+
+  @Patch()
+  public async updateEntity(@Body() body) {
+    throw new NotImplementedException();
+  }
+
+  @Delete(':guid')
+  public async deleteEntity(@Param() params) {
+    throw new NotImplementedException();
   }
 }
