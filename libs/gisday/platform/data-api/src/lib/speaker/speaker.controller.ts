b651d6e5f53c9dd56825a6f8c112c70c1a24ab83
Edgar Hernandez <imacleopard2010@gmail.com>
2023-09-17 21:36:47 -0500

    Pull BaseController from speakers resource

--- libs/gisday/platform/data-api/src/lib/speaker/speaker.controller.ts
+++ libs/gisday/platform/data-api/src/lib/speaker/speaker.controller.ts
@@ -1,43 +1,80 @@
-import { Body, Controller, Get, Param, Patch, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotImplementedException,
+  Param,
+  Patch,
+  Post,
+  UploadedFile,
+  UseInterceptors
+} from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 
 import { DeepPartial } from 'typeorm';
 
-import { Speaker } from '../entities/all.entity';
+import { EntityRelationsLUT, Speaker } from '../entities/all.entity';
 import { SpeakerProvider } from './speaker.provider';
-import { BaseController } from '../_base/base.controller';
 
 @Controller('speakers')
-export class SpeakerController extends BaseController<Speaker> {
-  constructor(private readonly speakerProvider: SpeakerProvider) {
-    super(speakerProvider);
+export class SpeakerController {
+  constructor(private readonly provider: SpeakerProvider) {}
+
+  @Get()
+  public async getEntities() {
+    return this.provider.find();
+  }
+
+  @Get(':guid')
+  public async getEntity(@Param('guid') guid) {
+    return this.provider.findOne({
+      where: {
+        guid: guid
+      }
+    });
+  }
+
+  @Get(':guid/speaker')
+  public async getEntityWithRelations(@Param('guid') guid) {
+    return this.provider.findOne({
+      where: {
+        guid: guid
+      },
+      relations: EntityRelationsLUT.getRelation('speaker')
+    });
   }
 
   @Get('/presenter/:guid')
   public async presenter(@Param() params) {
-    return this.speakerProvider.getPresenter(params.guid);
+    return this.provider.getPresenter(params.guid);
   }
 
   @Get('/presenters')
   public async presenters() {
-    return this.speakerProvider.getPresenters();
+    return this.provider.getPresenters();
+  }
+
+  @Get('/photo/:guid')
+  public async getSpeakerPhoto(@Param() params) {
+    return this.provider.getSpeakerPhoto(params.guid);
   }
 
   @Post('')
   @UseInterceptors(FileInterceptor('file'))
   public async speakerAndInfo(@Body() body, @UploadedFile() file) {
     const _speaker: DeepPartial<Speaker> = body;
-    return this.speakerProvider.insertWithInfo(_speaker, file);
-  }
-
-  @Get('/photo/:guid')
-  public async getSpeakerPhoto(@Param() params) {
-    return this.speakerProvider.getSpeakerPhoto(params.guid);
+    return this.provider.insertWithInfo(_speaker, file);
   }
 
   @Patch()
   @UseInterceptors(FileInterceptor('file'))
   public async updateSpeakerInfo(@Body() body: DeepPartial<Speaker>, @UploadedFile() file) {
-    return this.speakerProvider.updateWithInfo(body, file);
+    return this.provider.updateWithInfo(body, file);
+  }
+
+  @Delete(':guid')
+  public async deleteEntity(@Param() params) {
+    throw new NotImplementedException();
   }
 }
