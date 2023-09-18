82868b1e40d25842161f7ca695a172e9bf23bd74
Edgar Hernandez <imacleopard2010@gmail.com>
2023-09-17 21:36:27 -0500

    Pull BaseController from initial surveys resource

--- libs/gisday/platform/data-api/src/lib/initial-survey/initial-survey.controller.ts
+++ libs/gisday/platform/data-api/src/lib/initial-survey/initial-survey.controller.ts
@@ -1,20 +1,17 @@
-import { Body, Controller, Get, NotImplementedException, Post, Request } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotImplementedException, Param, Patch, Post, Request } from '@nestjs/common';
 
 import { InitialSurveyQuestion, InitialSurveyResponse } from '../entities/all.entity';
-import { BaseController } from '../_base/base.controller';
 import { InitialSurveyProvider } from './initial-survey.provider';
 
 @Controller('initial-surveys')
-export class InitialSurveyController extends BaseController<InitialSurveyResponse> {
-  constructor(private readonly initialSurveyProvider: InitialSurveyProvider) {
-    super(initialSurveyProvider);
-  }
+export class InitialSurveyController {
+  constructor(private readonly provider: InitialSurveyProvider) {}
 
   @Get()
   public async userTookSurvey(@Request() req) {
     // TODO: Add middleware for appending userGuid to request
     if (req.user) {
-      const tookSurvey = await this.initialSurveyProvider.initialSurveyRepo.count({
+      const tookSurvey = await this.provider.initialSurveyRepo.count({
         where: {
           accountGuid: req.user.sub
         }
@@ -29,23 +26,40 @@ export class InitialSurveyController extends BaseController<InitialSurveyRespons
     }
   }
 
+  @Get(':guid')
+  public async getSurvey(@Param('guid') guid) {
+    return this.provider.findOne({
+      where: {
+        guid: guid
+      }
+    });
+  }
+
   @Get('/questions/all')
   public async getQuestionsAll() {
-    return this.initialSurveyProvider.initialSurveyQuestionRepo.find();
+    return this.provider.initialSurveyQuestionRepo.find();
   }
 
   @Post('/questions')
   public async insertQuestion(@Body() body: Partial<InitialSurveyQuestion>) {
     const questionTypeGuid = body.questionType.guid;
 
-    return this.initialSurveyProvider.insertQuestion(questionTypeGuid, body);
+    return this.provider.insertQuestion(questionTypeGuid, body);
   }
 
   @Post()
-  public async insertInitialSurveyResponse() {
-    //  TODO: Add httpintercept to append userGuid to body
-
-    // return this.initialSurveyProvider.insertInitialSurveyResponse(questionGuids, questionGuidsObj, body.sub);
+  public async insertSurveyResponse() {
+    // Should return initial survey response
     return new NotImplementedException();
+  }
+
+  @Patch()
+  public async updateSurvey(@Body() body) {
+    throw new NotImplementedException();
+  }
+
+  @Delete(':guid')
+  public async deleteSurvey(@Param() params) {
+    throw new NotImplementedException();
   }
 }
