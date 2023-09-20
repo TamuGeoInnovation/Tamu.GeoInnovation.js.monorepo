fc00419edfa89270c5ad662e24bc7b7329db9eac
Edgar Hernandez <imacleopard2010@gmail.com>
2023-09-20 02:41:28 -0500

    Landing page pull season dates from active season

--- libs/gisday/platform/ngx/core/src/lib/pages/landing/landing.component.ts
+++ libs/gisday/platform/ngx/core/src/lib/pages/landing/landing.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { interval, map, Observable, Subscription } from 'rxjs';
 
 import { SeasonService } from '@tamu-gisc/gisday/platform/ngx/data-access';
-
-import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'tamu-gisc-landing',
@@ -18,6 +17,7 @@ export class LandingComponent implements OnInit {
   public daysTill: string;
   public subscription: Subscription;
   public activeSeason$ = this.ss.getActiveSeason();
+  public dateRange$: Observable<Array<Date>>;
 
   private source = interval(1000);
 
@@ -30,6 +30,19 @@ export class LandingComponent implements OnInit {
     this.firstDay = new Date(2022, 10, 13, 14, 30, 0, 0); // Idk why but we have to take a month and a day off for the numbers to add up - Aaron H (3/26/22)
 
     this.loadCountdown();
+
+    this.dateRange$ = this.ss.activeSeason$.pipe(
+      map((season) => {
+        if (season.days === undefined || season.days.length === 0) {
+          return null;
+        }
+
+        const firstDay = season.days[0].date;
+        const lastDay = season.days[season.days.length - 1].date;
+
+        return [firstDay, lastDay];
+      })
+    );
   }
 
   public loadCountdown() {
