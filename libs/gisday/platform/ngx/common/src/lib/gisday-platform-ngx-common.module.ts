5dd0f59e04e209d0d29b754a960649b37fb2676c
Edgar Hernandez <imacleopard2010@gmail.com>
2023-09-23 04:02:48 -0500

    Main CSS styles audit
    Update componenttized for with required markup changes due to CSS updates

--- libs/gisday/platform/ngx/common/src/lib/gisday-platform-ngx-common.module.ts
+++ libs/gisday/platform/ngx/common/src/lib/gisday-platform-ngx-common.module.ts
@@ -2,25 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { CommonNgxAuthModule } from '@tamu-gisc/common/ngx/auth';
 import { UITileNavigationModule } from '@tamu-gisc/ui-kits/ngx/navigation/mobile-tile';
 import { UINavigationTriggersModule } from '@tamu-gisc/ui-kits/ngx/navigation/triggers';
 
 import { FooterComponent } from './modules/footer/footer.component';
 import { HeaderComponent } from './modules/header/header.component';
 import { DetailViewComponent } from './modules/detail-view/detail-view.component';
-import { OrderByPipe } from './utils/order-by-pipe';
-import { CommonNgxAuthModule } from '@tamu-gisc/common/ngx/auth';
+import { OrderByPipe } from './pipes/order-by/order-by-pipe';
+import { ParseDateTimeStringsPipe } from './pipes/parse-date-time-strings/parse-date-time-strings.pipe';
 
 @NgModule({
   imports: [CommonModule, RouterModule, UITileNavigationModule, UINavigationTriggersModule, CommonNgxAuthModule],
-  declarations: [FooterComponent, HeaderComponent, DetailViewComponent],
+  declarations: [FooterComponent, HeaderComponent, DetailViewComponent, OrderByPipe, ParseDateTimeStringsPipe],
   providers: [],
-  exports: [FooterComponent, HeaderComponent, DetailViewComponent]
+  exports: [FooterComponent, HeaderComponent, DetailViewComponent, OrderByPipe, ParseDateTimeStringsPipe]
 })
 export class GisdayPlatformNgxCommonModule {}
-
-@NgModule({
-  declarations: [OrderByPipe],
-  exports: [OrderByPipe]
-})
-export class GisdayPlatformNgxPipesModule {}
