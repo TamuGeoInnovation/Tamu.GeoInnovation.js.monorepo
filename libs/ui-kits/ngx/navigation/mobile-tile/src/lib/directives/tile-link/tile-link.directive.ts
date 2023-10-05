464d1582fb82a6454a338d24d79a4923091239d9
Edgar Hernandez <imacleopard2010@gmail.com>
2023-10-04 19:34:08 -0500

    Fix tile mobile nav routing
    Actually routes now. Suports relative and absolute routing with fragments

--- libs/ui-kits/ngx/navigation/mobile-tile/src/lib/directives/tile-link/tile-link.directive.ts
+++ libs/ui-kits/ngx/navigation/mobile-tile/src/lib/directives/tile-link/tile-link.directive.ts
@@ -1,10 +1,10 @@
-import {Directive, ElementRef, EventEmitter, HostListener, Input, OnInit, Output, Renderer2} from '@angular/core';
-import {Router} from '@angular/router';
+import { Directive, ElementRef, EventEmitter, HostListener, Input, Output, Renderer2 } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Directive({
   selector: '[tamuGiscTileLink]'
 })
-export class TileLinkDirective implements OnInit {
+export class TileLinkDirective {
   /**
    * Location of the link to redirect to. Can be internal relative URL or absolute external URL
    *
@@ -12,21 +12,44 @@ export class TileLinkDirective implements OnInit {
   @Input()
   public location: string;
 
+  /**
+   * Used when input location is a relative application link
+   */
+  @Input()
+  public fragment: string;
+
   @Output()
   public clicked: EventEmitter<HTMLElement> = new EventEmitter();
 
-  constructor(private router: Router, private renderer: Renderer2, private el: ElementRef) {
-  }
-
-  public ngOnInit(): void {
-    if (this.location) {
-      // this.renderer.setAttribute(this.el.nativeElement, 'href', this.location);
-    }
-  }
+  constructor(private router: Router, private renderer: Renderer2, private el: ElementRef) {}
 
   @HostListener('click', ['$event.target'])
   private _click(target: HTMLElement) {
+    const lt = this.testLocation(this.location);
+
+    if (lt === LocationType.EXTERNAL) {
+      window.location.href = this.location;
+    } else if (lt === LocationType.INTERNAL) {
+      this.router.navigate([this.location], this.fragment ? { fragment: this.fragment } : undefined);
+    }
+
     this.clicked.next(target);
   }
+
+  private testLocation(loc: string): LocationType {
+    const reg = new RegExp('^(http|https)://', 'i');
+    const isAbsolute = reg.test(loc);
+
+    if (isAbsolute) {
+      return LocationType.EXTERNAL;
+    } else {
+      return LocationType.INTERNAL;
+    }
+  }
+}
+
+export enum LocationType {
+  INTERNAL,
+  EXTERNAL
 }
 
