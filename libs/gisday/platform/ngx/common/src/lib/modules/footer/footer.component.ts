666144d20301226f3674d94cfb1f4d2c0eeb099e
Edgar Hernandez <imacleopard2010@gmail.com>
2023-11-06 16:41:24 -0600

    Link to organization if website url is available

--- libs/gisday/platform/ngx/common/src/lib/modules/footer/footer.component.ts
+++ libs/gisday/platform/ngx/common/src/lib/modules/footer/footer.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { Season } from '@tamu-gisc/gisday/platform/data-api';
-import { SeasonService } from '@tamu-gisc/gisday/platform/ngx/data-access';
+import { Place, Season } from '@tamu-gisc/gisday/platform/data-api';
+import { PlaceService, SeasonService } from '@tamu-gisc/gisday/platform/ngx/data-access';
 
 @Component({
   selector: 'tamu-gisc-app-footer',
@@ -11,12 +11,14 @@ import { SeasonService } from '@tamu-gisc/gisday/platform/ngx/data-access';
 })
 export class FooterComponent implements OnInit {
   public activeSeason$: Observable<Partial<Season>>;
+  public organizations$: Observable<Array<Partial<Place>>>;
   public currentYear: number;
 
-  constructor(private readonly ss: SeasonService) {}
+  constructor(private readonly ss: SeasonService, private readonly os: PlaceService) {}
 
   public ngOnInit(): void {
     this.activeSeason$ = this.ss.activeSeason$;
     this.currentYear = new Date().getFullYear();
+    this.organizations$ = this.os.getEntities();
   }
 }
