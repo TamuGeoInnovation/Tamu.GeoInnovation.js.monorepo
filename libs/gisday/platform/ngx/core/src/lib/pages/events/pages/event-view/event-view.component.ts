7ec3a13f7883de23c50bed947ddbe0b2849b2c63
Edgar Hernandez <imacleopard2010@gmail.com>
2023-11-06 01:05:55 -0600

    Add logged in checks before registering events

--- libs/gisday/platform/ngx/core/src/lib/pages/events/pages/event-view/event-view.component.ts
+++ libs/gisday/platform/ngx/core/src/lib/pages/events/pages/event-view/event-view.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { BehaviorSubject, EMPTY, Observable, Subject } from 'rxjs';
+import { filter, map, shareReplay, switchMap, tap, withLatestFrom } from 'rxjs/operators';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 import { ActiveSeasonDto, Event, GisDayAppMetadata, Place, Tag } from '@tamu-gisc/gisday/platform/data-api';
@@ -13,6 +13,7 @@ import {
   UserService
 } from '@tamu-gisc/gisday/platform/ngx/data-access';
 import { NotificationService } from '@tamu-gisc/common/ngx/ui/notification';
+import { AuthService } from '@tamu-gisc/common/ngx/auth';
 
 @Component({
   selector: 'tamu-gisc-event-view',
@@ -49,6 +50,7 @@ export class EventViewComponent implements OnInit {
     private readonly fb: FormBuilder,
     private readonly rs: RsvpService,
     private readonly us: UserService,
+    private readonly as: AuthService,
     private ns: NotificationService
   ) {}
 
@@ -62,7 +64,17 @@ export class EventViewComponent implements OnInit {
     this.events$ = this.eventService.getEvents().pipe(shareReplay());
     this.tags$ = this.tagService.getEntities().pipe(shareReplay());
     this.organizations$ = this.os.getEntities().pipe(shareReplay());
-    this.userInfo$ = this.us.getSignedOnEntity().pipe(shareReplay());
+    this.userInfo$ = this.as.isAuthenticated$.pipe(
+      filter((auth) => {
+        return auth === true;
+      }),
+      switchMap(() => {
+        return this.us.getSignedOnEntity();
+      }),
+
+      shareReplay()
+    );
+
     this.userProfileComplete$ = this.userInfo$.pipe(
       map((user) => {
         return user?.app_metadata?.gisday?.completedProfile === true;
@@ -72,11 +84,18 @@ export class EventViewComponent implements OnInit {
 
     this.activeTagFilters$ = this.form.get('tags').valueChanges.pipe(shareReplay());
     this.activeOrgFilters$ = this.form.get('organizations').valueChanges.pipe(shareReplay());
-    this.rsvps$ = this.rs.getRsvpsForSignedInUser().pipe(
-      map((rsvps) => {
-        return rsvps.map((rsvp) => {
-          return rsvp?.event?.guid;
-        });
+    this.rsvps$ = this.as.isAuthenticated$.pipe(
+      filter((auth) => {
+        return auth === true;
+      }),
+      switchMap(() => {
+        return this.rs.getRsvpsForSignedInUser().pipe(
+          map((rsvps) => {
+            return rsvps.map((rsvp) => {
+              return rsvp?.event?.guid;
+            });
+          })
+        );
       })
     );
   }
@@ -85,33 +104,74 @@ export class EventViewComponent implements OnInit {
     this._filtersVisible$.next(!this._filtersVisible$.value);
   }
 
+  // this.userProfileComplete$
   public registerEvent(eventGuid: string) {
-    this.userProfileComplete$.subscribe((complete) => {
-      if (complete) {
-        this.rs.createRsvp(eventGuid).subscribe(() => {
-          console.log(`Registered for event ${eventGuid}`);
-        });
-      } else {
-        this.ns.toast({
-          message: 'You must complete your profile before registering for events.',
-          id: 'gisday-register-fail',
-          title: 'Profile Incomplete'
-        });
-      }
-    });
+    this.as.isAuthenticated$
+      .pipe(
+        tap((auth) => {
+          if (auth === false) {
+            this.ns.toast({
+              message: 'You must be signed in to register for events.',
+              id: 'gisday-register-fail',
+              title: 'Not Signed In'
+            });
+          }
+        }),
+        filter((auth) => {
+          return auth === true;
+        }),
+        withLatestFrom(this.userProfileComplete$),
+        switchMap(([, complete]) => {
+          if (complete) {
+            return this.rs.createRsvp(eventGuid);
+          } else {
+            this.ns.toast({
+              message: 'You must complete your profile before registering for events.',
+              id: 'gisday-register-fail',
+              title: 'Profile Incomplete'
+            });
+
+            return EMPTY;
+          }
+        })
+      )
+      .subscribe(() => {
+        console.log(`Registered for event ${eventGuid}`);
+      });
   }
 
   public unregisterEvent(eventGuid: string) {
-    this.userProfileComplete$.subscribe((complete) => {
-      if (complete) {
-        this.rs.deleteEntity(eventGuid).subscribe(() => {
-          console.log(`Unregistered for event ${eventGuid}`);
-        });
-      } else {
-        this.rs.deleteEntity(eventGuid).subscribe(() => {
-          console.log(`Unregistered for event ${eventGuid}`);
-        });
-      }
-    });
+    this.as.isAuthenticated$
+      .pipe(
+        tap((auth) => {
+          if (auth === false) {
+            this.ns.toast({
+              message: 'You must be signed in to unregister for events.',
+              id: 'gisday-unregister-fail',
+              title: 'Not Signed In'
+            });
+          }
+        }),
+        filter((auth) => {
+          return auth === true;
+        }),
+        withLatestFrom(this.userProfileComplete$),
+        switchMap(([, complete]) => {
+          if (complete) {
+            return this.rs.deleteEntity(eventGuid);
+          } else {
+            this.ns.toast({
+              message: 'You must complete your profile before unregistering for events.',
+              id: 'gisday-unregister-fail',
+              title: 'Profile Incomplete'
+            });
+
+            return EMPTY;
+          }
+        })
+      )
+      .subscribe(() => {
+        console.log(`Unregistered for event ${eventGuid}`);
+      });
   }
 }
