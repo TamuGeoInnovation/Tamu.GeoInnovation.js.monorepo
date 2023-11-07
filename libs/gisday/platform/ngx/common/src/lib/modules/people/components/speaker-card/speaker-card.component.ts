9512770b0eaddb535f29bb4b06b32d5111f22053
Edgar Hernandez <imacleopard2010@gmail.com>
2023-11-06 21:20:40 -0600

    Fix speaker class banner

--- libs/gisday/platform/ngx/common/src/lib/modules/people/components/speaker-card/speaker-card.component.ts
+++ libs/gisday/platform/ngx/common/src/lib/modules/people/components/speaker-card/speaker-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { Speaker } from '@tamu-gisc/gisday/platform/data-api';
 
@@ -7,8 +7,23 @@ import { Speaker } from '@tamu-gisc/gisday/platform/data-api';
   templateUrl: './speaker-card.component.html',
   styleUrls: ['./speaker-card.component.scss']
 })
-export class SpeakerCardComponent {
+export class SpeakerCardComponent implements OnInit {
   @Input()
   public speaker: Partial<Speaker>;
-}
 
+  public graduationYear: string;
+
+  public ngOnInit(): void {
+    this.graduationYear = this.speaker?.graduationYear ? this._truncateGraduationYear(this.speaker.graduationYear) : '';
+  }
+
+  private _truncateGraduationYear(year: string) {
+    if (year.length === 4) {
+      return `'${year.substring(2, 4)}`;
+    } else if (year.length === 2) {
+      return `' ${year}`;
+    } else {
+      return '';
+    }
+  }
+}
