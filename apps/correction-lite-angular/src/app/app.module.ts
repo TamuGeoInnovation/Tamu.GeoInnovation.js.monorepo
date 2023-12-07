87cda61e8115e5d91872ffc1609ef500b689463c
Edgar Hernandez <imacleopard2010@gmail.com>
2023-12-07 13:02:26 -0600

    Remove initial nx welcome component

--- apps/correction-lite-angular/src/app/app.module.ts
+++ apps/correction-lite-angular/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 
+import { AppComponent } from './app.component';
+
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' })],
   providers: [],
   bootstrap: [AppComponent]
