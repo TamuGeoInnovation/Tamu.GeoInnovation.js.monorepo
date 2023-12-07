7d78c994fe46b3083a235cd2d46138461da8297f
Edgar Hernandez <imacleopard2010@gmail.com>
2023-12-07 12:59:53 -0600

    Init correction lib boilerplate

--- apps/correction-lite-angular/src/app/app.component.ts
+++ apps/correction-lite-angular/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'correction-lite-angular';
+  title = 'Geocode Correction Lite | Texas A&M Geoservices ';
 }
---
+++ libs/correction/ngx/README.md
@@ -0,0 +1,7 @@
+# correction-ngx
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test correction-ngx` to execute the unit tests.
