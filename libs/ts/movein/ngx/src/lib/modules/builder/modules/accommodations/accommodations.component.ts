80298745d261fe89cce52922538eface5b9bad03
Edgar Hernandez <imacleopard2010@gmail.com>
2024-07-01 08:53:33 -0500

    Accessible parking selection working

--- libs/ts/movein/ngx/src/lib/modules/builder/modules/accommodations/accommodations.component.ts
+++ libs/ts/movein/ngx/src/lib/modules/builder/modules/accommodations/accommodations.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Angulartics2 } from 'angulartics2';
+
+import { LocalStoreService } from '@tamu-gisc/common/ngx/local-store';
 
 @Component({
   selector: 'tamu-gisc-accommodations',
@@ -6,7 +11,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accommodations.component.scss']
 })
 export class AccommodationsComponent implements OnInit {
-  constructor() {}
+  private returnState: string;
+  public savedAccessible: boolean;
 
-  ngOnInit(): void {}
+  constructor(
+    private store: LocalStoreService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private angulartics: Angulartics2
+  ) {}
+
+  public ngOnInit() {
+    this.savedAccessible = this.store.getStorageObjectKeyValue({ primaryKey: 'aggiemap-movein', subKey: 'accessible' });
+
+    this.route.params.subscribe((params) => {
+      if (params['returnState']) {
+        this.returnState = params['returnState'];
+      }
+    });
+  }
+
+  /**
+   * Saves component value in local storage
+   *
+   * @param {*} item
+   */
+  public saveAccessible = (item: boolean): void => {
+    this.store.setStorageObjectKeyValue({
+      primaryKey: 'aggiemap-movein',
+      subKey: 'accessible',
+      value: item
+    });
+
+    const confirm = this.store.getStorageObjectKeyValue({
+      primaryKey: 'aggiemap-movein',
+      subKey: 'accessible'
+    });
+
+    this.angulartics.eventTrack.next({
+      action: 'Accommodation Selection',
+      properties: {
+        category: 'UI Interaction',
+        label: confirm
+      }
+    });
+
+    if (this.returnState !== undefined) {
+      this.router.navigate([`builder/${this.returnState}`]);
+    } else {
+      this.router.navigate(['builder/review']);
+    }
+  };
 }
