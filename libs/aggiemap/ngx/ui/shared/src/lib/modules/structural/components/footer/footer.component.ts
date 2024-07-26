ed6c2493c623f5023f5f507c0f6dacf5cd0d8ed9
Edgar Hernandez <imacleopard2010@gmail.com>
2024-07-26 11:19:20 -0500

    Add dynamic copyright year to footer

--- libs/aggiemap/ngx/ui/shared/src/lib/modules/structural/components/footer/footer.component.ts
+++ libs/aggiemap/ngx/ui/shared/src/lib/modules/structural/components/footer/footer.component.ts
@@ -5,4 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {}
+export class FooterComponent {
+  public year = new Date().getFullYear();
+}
+
