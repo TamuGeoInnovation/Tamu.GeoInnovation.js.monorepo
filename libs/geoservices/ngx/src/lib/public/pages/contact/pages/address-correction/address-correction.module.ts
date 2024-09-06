8e3b89858a1aaff5e98d47d3d75b29e3d8420958
Edgar Hernandez <imacleopard2010@gmail.com>
2024-09-06 12:21:10 -0500

    geoservices-ngx-public: Fix style issues

--- libs/geoservices/ngx/src/lib/public/pages/contact/pages/address-correction/address-correction.module.ts
+++ libs/geoservices/ngx/src/lib/public/pages/contact/pages/address-correction/address-correction.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { AddressCorrectionComponent } from './address-correction.component';
 import { GeocodeCorrectionFormModule } from '../../../../../core/modules/forms/geocode-correction-form/geocode-correction-form.module';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
