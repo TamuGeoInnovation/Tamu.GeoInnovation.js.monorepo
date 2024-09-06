56c1f7dbab055d0ffdc334bbc110d2394f70532d
Edgar Hernandez <imacleopard2010@gmail.com>
2024-09-06 11:19:31 -0500

    Documentation typo in size.animations

--- libs/ui-kits/ngx/animations/src/lib/size/size.animations.ts
+++ libs/ui-kits/ngx/animations/src/lib/size/size.animations.ts
@@ -3,7 +3,7 @@ import { trigger, transition, style, animate } from '@angular/animations';
 /**
  * Returns animation that makes the element grow by modifying the max-height attribute
  *
- * Being at 0% height and 100% opacity at the start of the animation and 100% height and 0% opacity at the end
+ * Begin at 0% height and 100% opacity at the start of the animation and 100% height and 0% opacity at the end
  * @param {number} maxPixelHeight
  * @return {*}
  */
