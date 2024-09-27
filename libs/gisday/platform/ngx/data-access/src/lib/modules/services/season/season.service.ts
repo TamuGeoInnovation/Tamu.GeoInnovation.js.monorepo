185a8ccf4abf7a85b413077a5d283605b0db69fc
Edgar Hernandez <imacleopard2010@gmail.com>
2024-09-26 21:59:22 -0500

    Make data-access season service refresh active season when create or delete season

--- libs/gisday/platform/ngx/data-access/src/lib/modules/services/season/season.service.ts
+++ libs/gisday/platform/ngx/data-access/src/lib/modules/services/season/season.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, shareReplay } from 'rxjs';
+import { Observable, shareReplay, startWith, Subject, switchMap, tap } from 'rxjs';
 
 import { ActiveSeasonDto, Season } from '@tamu-gisc/gisday/platform/data-api';
 import { EnvironmentService } from '@tamu-gisc/common/ngx/environment';
@@ -11,8 +11,18 @@ import { BaseService } from '../_base/base.service';
   providedIn: 'root'
 })
 export class SeasonService extends BaseService<Season> {
-  public seasons$: Observable<Array<Partial<Season>>> = this.getEntities().pipe(shareReplay(1));
-  public activeSeason$: Observable<ActiveSeasonDto> = this.getActiveSeason().pipe(shareReplay(1));
+  private _signal$: Subject<void> = new Subject();
+
+  public seasons$: Observable<Array<Partial<Season>>> = this._signal$.pipe(
+    startWith(null),
+    switchMap(() => this.getEntities()),
+    shareReplay(1)
+  );
+  public activeSeason$: Observable<ActiveSeasonDto> = this._signal$.pipe(
+    startWith(null),
+    switchMap(() => this.getActiveSeason()),
+    shareReplay(1)
+  );
 
   constructor(private env1: EnvironmentService, private http1: HttpClient) {
     super(env1, http1, 'seasons');
@@ -20,5 +30,13 @@ export class SeasonService extends BaseService<Season> {
 
   public getActiveSeason() {
     return this.http1.get<ActiveSeasonDto>(this.resource + '/active');
+  }
+
+  public override createEntity(newEntity?: Partial<Season>) {
+    return this.http1.post<Partial<Season>>(this.resource, newEntity).pipe(tap(() => this._signal$.next()));
+  }
+
+  public override deleteEntity(entityGuid: string) {
+    return this.http1.delete<Partial<Season>>(`${this.resource}/${entityGuid}`).pipe(tap(() => this._signal$.next()));
   }
 }
