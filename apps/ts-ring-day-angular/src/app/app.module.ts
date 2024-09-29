04ebf55a0d578aefae4b3667297b440585e7bfbf
Edgar Hernandez <imacleopard2010@gmail.com>
2024-09-29 15:25:59 -0500

    Displaying map

--- apps/ts-ring-day-angular/src/app/app.module.ts
+++ apps/ts-ring-day-angular/src/app/app.module.ts
@@ -1,14 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
+import { TsMoveinNgxModule } from '@tamu-gisc/ts/movein/ngx';
+import { EnvironmentModule, env } from '@tamu-gisc/common/ngx/environment';
+import { SettingsModule } from '@tamu-gisc/common/ngx/settings';
+import { NotificationModule, notificationStorage } from '@tamu-gisc/common/ngx/ui/notification';
+
+import { Angulartics2Module } from 'angulartics2';
+import * as WebFont from 'webfontloader';
+
+import * as environment from '../environments/environment';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 
+WebFont.load({
+  google: {
+    families: ['Material Icons', 'Material Icons Outlined', 'Open Sans:300,400,600', 'Oswald']
+  }
+});
+
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' })],
-  providers: [],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    Angulartics2Module.forRoot(),
+    EnvironmentModule,
+    SettingsModule,
+    NotificationModule,
+    TsMoveinNgxModule,
+    RouterModule.forRoot([])
+  ],
+  declarations: [AppComponent],
+  providers: [
+    {
+      provide: env,
+      useValue: environment
+    },
+    { provide: notificationStorage, useValue: 'ring-day-notifications' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
