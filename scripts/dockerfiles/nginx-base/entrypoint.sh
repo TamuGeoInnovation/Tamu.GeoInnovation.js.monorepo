11122154e3a91aa28efc972dab2e9c9ed7086cd7
Edgar Hernandez <imacleopard2010@gmail.com>
2024-11-11 12:08:21 -0600

    Use published nginx-base image for publish angular images
    Removed all of the token sub files since they are now independently provided by the base nginx image

--- scripts/dockerfiles/nginx-base/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Running token substitution at runtime"
-
-# Run the token substitution
-/usr/local/bin/tokenSubstitute.sh
-
-# Start nginx after substitution
-exec nginx -g "daemon off;"
