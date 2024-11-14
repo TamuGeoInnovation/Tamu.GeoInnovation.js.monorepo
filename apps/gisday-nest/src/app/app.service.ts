3db2ce6743790317b409dd4b265ff4db4b515ed5
Edgar Hernandez <imacleopard2010@gmail.com>
2024-11-13 23:21:27 -0600

    Add repo url to gisday-nest. This is a trigger commit

--- apps/gisday-nest/src/app/app.service.ts
+++ apps/gisday-nest/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   public getHello(): string {
-    return 'GIS Day API';
+    return 'GIS Day API: https://github.com/TamuGeoInnovation/Tamu.GeoInnovation.js.monorepo/tree/development/apps/gisday-nest';
   }
 }
