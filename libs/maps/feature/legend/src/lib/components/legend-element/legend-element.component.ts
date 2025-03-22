4dd908b5505f61e2bd76dac34713dfbe78ee1123
Edgar Hernandez <Proantagonist@users.noreply.github.com>
2025-03-22 15:29:13 -0500

    feat(maps-feature-legend): Add definition expression filtering to legend items (#392)
    Limits legend items displayed based on the definition expression of a layer to reduce feature overload when they are not rendered on the map.

--- libs/maps/feature/legend/src/lib/components/legend-element/legend-element.component.ts
+++ libs/maps/feature/legend/src/lib/components/legend-element/legend-element.component.ts
@@ -12,6 +12,9 @@ export class LegendElementComponent implements OnInit {
   public element: ILegendElement;
 
   @Input()
+  public layer: ILayer;
+
+  @Input()
   public groupTitle: string;
 
   /**
@@ -21,6 +24,9 @@ export class LegendElementComponent implements OnInit {
    */
   @Input()
   public deduplicateChildren = false;
+
+  @Input()
+  public respectDefinitionExpression = false;
 
   public infos:
     | any[]
@@ -38,8 +44,78 @@ export class LegendElementComponent implements OnInit {
     } else {
       this.infos = this.element.infos;
     }
+
+    // Return early if we are not respecting definition expressions or there is no operable layer
+    if (this.respectDefinitionExpression === false || this.layer === undefined) {
+      return;
+    }
+
+    const operableLayer = this.layer as esri.FeatureLayer;
+
+    // If operable layer has no definition expression, return early
+    if (operableLayer.definitionExpression === null || operableLayer.definitionExpression === undefined) {
+      return;
+    }
+
+    const renderer = operableLayer.renderer as esri.UniqueValueRenderer;
+
+    // Ensure that the renderer is a unique value renderer
+    if (renderer.type !== 'unique-value') {
+      return;
+    }
+
+    // Ensure that the renderer has operable unique value infos
+    if (
+      renderer === null ||
+      renderer === undefined ||
+      renderer.uniqueValueInfos === null ||
+      renderer.uniqueValueInfos === undefined ||
+      renderer.uniqueValueInfos.length === 0
+    ) {
+      return;
+    }
+
+    // Determine how many fields are expected in the renderer value string.
+    // This is necessary because the renderer value string can be a concatenation of multiple fields.
+    const operableFields = [renderer.field, renderer.field2, renderer.field3].filter((f) => f !== null);
+
+    // If there are no operable fields, return early
+    if (operableFields.length === 0) {
+      return;
+    }
+
+    // Unique value expressions can at most be a concatenation of 3 fields, with exact value matches. This means
+    // that we can split the definition expression by the AND operator and check if any of the fields in the renderer
+    // are part of the expression.
+    //
+    // We split the definition expression by the AND operator to get the individual expressions, if any.
+    const expressions = operableLayer.definitionExpression.split('AND').map((exp) => exp.trim());
+
+    // For every grouped expression, split it by the '=' comparison operator to get the field and value.
+    const fieldValuePairs = expressions.map((exp) => {
+      const [field, value] = exp.split('=').map((part) => part.trim());
+      return { field, value: value.replace(/'/g, '') };
+    });
+
+    // Test if either expression includes any of the fields in the renderer value string.
+    // If it does, we can filter the legend element infos to only include those that match the definition expression.
+    const filteredInfos = this.infos.filter((info) => {
+      // Deconstruct legend info.value so we can accurately compare it to the definition expression.
+      const valueParts = operableFields.length ? info.value.split(renderer.fieldDelimiter) : [info.value];
+
+      return fieldValuePairs.some((pair) => {
+        return operableFields.some((field) => {
+          const operableValue = valueParts[operableFields.indexOf(field)];
+
+          return pair.field === field && pair.value === operableValue;
+        });
+      });
+    });
+
+    this.infos = filteredInfos;
   }
 }
 
 // Browser doesn't like direct esri types for inputs.
 type ILegendElement = esri.LegendElement;
+type ILayer = esri.Layer;
