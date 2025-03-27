0bde223f89c869c72bb8e4d1507696662742904e
Edgar Hernandez <imacleopard2010@gmail.com>
2025-03-27 18:55:53 +0000

    Add custom event intro text

--- libs/ts/events/ngx/src/lib/definitions/phys-engineering-festival.definitions.ts
+++ libs/ts/events/ngx/src/lib/definitions/phys-engineering-festival.definitions.ts
@@ -71,6 +71,7 @@ export const PhysicsAndEngineeringFestivalConfiguration: EventConfiguration = {
   name: 'Physics and Engineering Festival',
   applicationName: 'Physics and Engineering Festival Transportation Map',
   shortApplicationName: 'Physics and Engineering Festival Map',
+  introductionText: 'Get the best parking information for the',
   eventDates: ['2025-04-05'],
   mapCenter: [-96.33771, 30.62143],
   zoom: 17
