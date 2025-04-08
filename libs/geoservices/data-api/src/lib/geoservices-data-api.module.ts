9586d0c29ff38a59e2cc63bdeecf67580c3ce1b1
Edgar Hernandez <imacleopard2010@gmail.com>
2025-04-08 19:07:37 +0000

    Add base payments module, controller, and service

--- libs/geoservices/data-api/src/lib/geoservices-data-api.module.ts
+++ libs/geoservices/data-api/src/lib/geoservices-data-api.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { ContactModule } from './modules/contact/contact.module';
+import { PaymentsModule } from './modules/payments/payments.module';
 
 @Module({
-  imports: [ContactModule],
+  imports: [ContactModule, PaymentsModule],
   controllers: [],
   providers: [],
   exports: []
