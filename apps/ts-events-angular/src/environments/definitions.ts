ec5512f1c6bce57144784d8383d748c0cc186674
Edgar Hernandez <Proantagonist@users.noreply.github.com>
2025-04-14 16:25:18 -0500

    feat(ts-events-angular): Physics & Engineering Festival Event 2025 (#399)

--- apps/ts-events-angular/src/environments/definitions.ts
+++ apps/ts-events-angular/src/environments/definitions.ts
@@ -1,7 +1,8 @@
 export {
-  BigEventColdLayerSources as ColdLayerSources,
-  BigEventOptions as SpecialEventOptions,
-  BigEventConfiguration as SpecialEventConfiguration
+  PhysEngFestivalColdLayerSources as ColdLayerSources,
+  PhysicsAndEngineeringFestivalOptions as SpecialEventOptions,
+  PhysicsAndEngineeringFestivalConfiguration as SpecialEventConfiguration,
+  PHYSICS_AND_ENGINEERING_FESTIVAL_LAYERS as SpecialEventLayerReferences
 } from '@tamu-gisc/ts/events/ngx';
 
 export const NotificationEvents = [];
---
+++ libs/ts/events/ngx/src/lib/interfaces/physics-engineering-festival.interface.ts
@@ -0,0 +1,4 @@
+export enum PHYSICS_AND_ENGINEERING_FESTIVAL_LAYERS {
+  FESTIVAL_AREAS = 'phys-eng-festival-areas',
+  PEDESTRIAN_PATH = 'phys-eng-festival-pedestrian-path'
+}
