2f3aebcf33e6f52cab10eff192f84237c2817710
Edgar Hernandez <imacleopard2010@gmail.com>
2025-04-14 21:49:10 +0000

    Add event layer enums and event configuration definition

--- libs/ts/events/ngx/src/index.ts
+++ libs/ts/events/ngx/src/index.ts
@@ -8,9 +8,11 @@ export * from './lib/interfaces/aggieland-saturday.interface';
 export * from './lib/interfaces/big-event.interface';
 export * from './lib/interfaces/physics-engineering-festival.interface';
 export * from './lib/interfaces/family-weekend.interface';
+export * from './lib/interfaces/maroon-white-game.interface';
 
 export * from './lib/definitions/graduation.definitions';
 export * from './lib/definitions/aggieland-saturday.definitions';
 export * from './lib/definitions/big-event.definitions';
 export * from './lib/definitions/phys-engineering-festival.definitions';
 export * from './lib/definitions/family-weekend.definitions';
+export * from './lib/definitions/maroon-white-game.definitions';
---
+++ libs/ts/events/ngx/src/lib/definitions/maroon-white-game.definitions.ts
@@ -0,0 +1,47 @@
+import { LayerSource } from '@tamu-gisc/common/types';
+
+import { MarkdownWDirectionsPopupComponent } from '../modules/popups/markdown-w-directions-popup/markdown-w-directions-popup.component';
+import { EventConfiguration, SpecialEventOptions } from '../interfaces/special-event.interface';
+import { MAROON_WHITE_GAME_LAYERS } from '../interfaces/maroon-white-game.interface';
+
+const eventUrl = 'https://gis.tamu.edu/arcgis/rest/services/TS/Maroon_White_Game/MapServer';
+
+export const MaroonWhiteGameDefinitions = {
+  PARKING_LOTS: {
+    id: MAROON_WHITE_GAME_LAYERS.PARKING_LOTS,
+    layerId: MAROON_WHITE_GAME_LAYERS.PARKING_LOTS,
+    name: 'Maroon & White Game Parking Lots',
+    url: `${eventUrl}/0`
+  }
+};
+
+export const MaroonWhiteGameColdLayerSources: LayerSource[] = [
+  {
+    type: 'feature',
+    id: MaroonWhiteGameDefinitions.PARKING_LOTS.id,
+    title: MaroonWhiteGameDefinitions.PARKING_LOTS.name,
+    url: MaroonWhiteGameDefinitions.PARKING_LOTS.url,
+    popupComponent: MarkdownWDirectionsPopupComponent,
+    popupData: {
+      name: 'attributes.Type',
+      description: 'attributes.description'
+    },
+    visible: true,
+    listMode: 'show',
+    native: {
+      outFields: ['*']
+    }
+  }
+];
+
+export const MaroonWhiteGameConfiguration: EventConfiguration = {
+  id: 'maroon-white-game-2025',
+  name: 'Maroon & White Game',
+  applicationName: 'Maroon & White Game Transportation Map',
+  shortApplicationName: 'Maroon & White Game Map',
+  eventDates: ['2025-04-20'],
+  mapCenter: [-96.3405, 30.61114],
+  zoom: 16
+};
+
+export const MaroonWhiteGameOptions: SpecialEventOptions = [];
