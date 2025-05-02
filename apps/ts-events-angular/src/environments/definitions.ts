398d5834db8e862eb3579efead9e1b6cd458d3fc
Edgar Hernandez <imacleopard2010@gmail.com>
2025-05-02 20:01:31 +0000

    Add muster layers

--- apps/ts-events-angular/src/environments/definitions.ts
+++ apps/ts-events-angular/src/environments/definitions.ts
@@ -1,8 +1,8 @@
 export {
-  FamilyWeekendColdLayerSources as ColdLayerSources,
-  FamilyWeekendOptions as SpecialEventOptions,
-  FamilyWeekendConfiguration as SpecialEventConfiguration,
-  FAMILY_WEEKEND_LAYERS as SpecialEventLayerReferences
+  MusterEventColdLayerSources as ColdLayerSources,
+  MusterOptions as SpecialEventOptions,
+  MusterConfiguration as SpecialEventConfiguration,
+  MUSTER_LAYERS as SpecialEventLayerReferences
 } from '@tamu-gisc/ts/events/ngx';
 
 export { SelectionSymbols, Polygons } from '@tamu-gisc/aggiemap/ngx/common';
