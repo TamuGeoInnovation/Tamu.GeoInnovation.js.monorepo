a2df3330d406d78fa8824fdb51cdbef444407585
Edgar Hernandez <Proantagonist@users.noreply.github.com>
2025-05-15 15:27:24 -0500

    feat(ts-events-angular): High School Graduation 2025 (#407)

--- apps/ts-events-angular/src/environments/definitions.ts
+++ apps/ts-events-angular/src/environments/definitions.ts
@@ -1,8 +1,8 @@
 export {
-  TroubadourColdLayerSources as ColdLayerSources,
-  TroubadourOptions as SpecialEventOptions,
-  TroubadourConfiguration as SpecialEventConfiguration,
-  TROUBADOUR_LAYERS as SpecialEventLayerReferences
+  HsGraduationColdLayerSources as ColdLayerSources,
+  HsGraduationOptions as SpecialEventOptions,
+  HsGraduationConfiguration as SpecialEventConfiguration,
+  HS_GRADUATION_LAYERS as SpecialEventLayerReferences
 } from '@tamu-gisc/ts/events/ngx';
 
 export { SelectionSymbols, Polygons } from '@tamu-gisc/aggiemap/ngx/common';
