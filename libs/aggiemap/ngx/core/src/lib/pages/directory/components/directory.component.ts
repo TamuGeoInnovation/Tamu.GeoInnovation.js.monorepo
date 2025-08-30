356a4d4468429462671c6ede0925fe4620426647
Edgar Hernandez <imacleopard2010@gmail.com>
2025-08-30 02:41:39 +0000

    WIP: tabular building directory

--- libs/aggiemap/ngx/core/src/lib/pages/directory/components/directory.component.ts
+++ libs/aggiemap/ngx/core/src/lib/pages/directory/components/directory.component.ts
@@ -1,8 +1,59 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'tamu-gisc-aggiemap-directory',
   templateUrl: './directory.component.html',
   styleUrls: ['./directory.component.scss']
 })
-export class DirectoryComponent {}
+export class DirectoryComponent implements OnInit {
+  public data: Observable<Array<BuildingDirectoryEntry>>;
+  public rows: BuildingDirectoryEntry[] = [];
+  public loadingIndicator = true;
+
+  // Define the columns for the datatable
+  public columns = [
+    { name: 'Name', prop: 'BldgName', canAutoResize: true },
+    { name: 'Number', prop: 'Bldg', canAutoResize: true },
+    { name: 'Abbreviation', prop: 'BldgAbbr', canAutoResize: true }
+  ];
+
+  constructor(private readonly http: HttpClient) {}
+
+  public ngOnInit(): void {
+    this.data = this.http.get<Array<BuildingDirectoryEntry>>('/assets/data/building-directory-1756502603044.json');
+
+    // Load data into rows array for the datatable
+    this.data.subscribe({
+      next: (buildings) => {
+        this.rows = buildings;
+        this.loadingIndicator = false;
+      },
+      error: (error) => {
+        console.error('DirectoryComponent: Error loading data:', error);
+        this.loadingIndicator = false;
+      }
+    });
+  }
+}
+
+interface BuildingDirectoryEntry {
+  Bldg: string;
+  BldgName: string;
+  BldgAbbr: string;
+  OwnerFICE: string;
+  OwnerAbbr: string;
+  StatusCode: string;
+  Status: string;
+  SiteLocCode: string;
+  SiteLocCodeDescription: string;
+  LocationType: string;
+  LocationTypeDescription: string;
+  ConditionCode: string;
+  TypeCode: string;
+  YearBuilt: string | number;
+  Address1: string;
+  City: string;
+  ZipCode: string | number;
+}
--- libs/aggiemap/ngx/core/src/lib/pages/directory/directory.module.ts
+++ libs/aggiemap/ngx/core/src/lib/pages/directory/directory.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+
 import { AggiemapNgxSharedUiStructuralModule } from '@tamu-gisc/aggiemap/ngx/ui/shared';
 
 import { DirectoryComponent } from './components/directory.component';
@@ -14,7 +16,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes), AggiemapNgxSharedUiStructuralModule],
+  imports: [CommonModule, RouterModule.forChild(routes), AggiemapNgxSharedUiStructuralModule, NgxDatatableModule],
   declarations: [DirectoryComponent]
 })
 export class DirectoryModule {}
