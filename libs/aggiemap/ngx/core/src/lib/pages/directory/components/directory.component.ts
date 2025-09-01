e73d0728cb13a1168a6b73cd20bb8cf7bd508339
Edgar Hernandez <Proantagonist@users.noreply.github.com>
2025-08-31 21:42:07 -0500

    feat(aggiemap-angular): Dynamic building directory (#427)

--- libs/aggiemap/ngx/core/src/lib/pages/directory/components/directory.component.ts
+++ libs/aggiemap/ngx/core/src/lib/pages/directory/components/directory.component.ts
@@ -1,8 +1,115 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {
+  combineLatest,
+  concatMap,
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  Observable,
+  of,
+  shareReplay,
+  startWith,
+  switchMap,
+  tap,
+  toArray
+} from 'rxjs';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
+import { DatatableComponent, TableColumn } from '@swimlane/ngx-datatable';
 
 @Component({
   selector: 'tamu-gisc-aggiemap-directory',
   templateUrl: './directory.component.html',
   styleUrls: ['./directory.component.scss']
 })
-export class DirectoryComponent {}
+export class DirectoryComponent implements OnInit {
+  public form: FormGroup;
+
+  @ViewChild(DatatableComponent)
+  public table: DatatableComponent;
+
+  public _rows$: Observable<Array<BuildingDirectoryEntry>>;
+  // Define the columns for the datatable
+  public columns: TableColumn[] = [
+    { name: 'Name', prop: 'BldgName', canAutoResize: true, width: 350 },
+    { name: 'Number', prop: 'Bldg', canAutoResize: true },
+    { name: 'Abbreviation', prop: 'BldgAbbr', canAutoResize: true }
+  ];
+
+  public loadingIndicator = true;
+  private _data$: Observable<Array<BuildingDirectoryEntry>>;
+
+  constructor(private readonly http: HttpClient, private readonly fb: FormBuilder) {}
+
+  public ngOnInit(): void {
+    this._data$ = this.http.get<Array<BuildingDirectoryEntry>>('/assets/data/building-directory.json').pipe(
+      concatMap((data) => data),
+      map((buildingEntry) => {
+        // If the building abbreviation is a number, set it to an empty string (per requirements)
+        if (!isNaN(Number(buildingEntry.BldgAbbr))) {
+          buildingEntry.BldgAbbr = '';
+        }
+
+        return buildingEntry;
+      }),
+      toArray(),
+      shareReplay(1)
+    );
+
+    this.form = this.fb.group({
+      search: [null]
+    });
+
+    this._rows$ = combineLatest([
+      this._data$,
+      (this.form.get('search') as FormControl).valueChanges.pipe(startWith(null), debounceTime(300), distinctUntilChanged())
+    ]).pipe(
+      tap(() => {
+        this.loadingIndicator = true;
+      }),
+      switchMap(([data, search]) => {
+        if (search) {
+          const filtered = data.filter((building) => {
+            const searchLower = search.toLowerCase();
+
+            return (
+              building.BldgName.toLowerCase().includes(searchLower) ||
+              building.BldgAbbr.toLowerCase().includes(searchLower) ||
+              String(building.Bldg).toLowerCase().includes(searchLower) // Bldg is sometimes a number in the data set
+            );
+          });
+          return of(filtered);
+        } else {
+          return of(data);
+        }
+      }),
+      tap(() => {
+        this.loadingIndicator = false;
+        if (this.table) {
+          this.table.offset = 0;
+        }
+      })
+    );
+  }
+}
+
+interface BuildingDirectoryEntry {
+  Bldg: string;
+  BldgName: string;
+  BldgAbbr: string;
+  OwnerFICE: string;
+  OwnerAbbr: string;
+  StatusCode: string;
+  Status: string;
+  SiteLocCode: string;
+  SiteLocCodeDescription: string;
+  LocationType: string;
+  LocationTypeDescription: string;
+  ConditionCode: string;
+  TypeCode: string;
+  YearBuilt: string | number;
+  Address1: string;
+  City: string;
+  ZipCode: string | number;
+}
--- libs/aggiemap/ngx/core/src/lib/pages/directory/directory.module.ts
+++ libs/aggiemap/ngx/core/src/lib/pages/directory/directory.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+
 import { AggiemapNgxSharedUiStructuralModule } from '@tamu-gisc/aggiemap/ngx/ui/shared';
+import { ReactiveFormsModule } from '@angular/forms';
+import { UIFormsModule } from '@tamu-gisc/ui-kits/ngx/forms';
 
 import { DirectoryComponent } from './components/directory.component';
 
@@ -14,7 +18,14 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes), AggiemapNgxSharedUiStructuralModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+    UIFormsModule,
+    AggiemapNgxSharedUiStructuralModule,
+    NgxDatatableModule
+  ],
   declarations: [DirectoryComponent]
 })
 export class DirectoryModule {}
