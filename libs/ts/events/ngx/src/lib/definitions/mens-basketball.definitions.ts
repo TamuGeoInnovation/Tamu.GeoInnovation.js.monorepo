31cc084ac7b92f47b604b101c0d6e567f7ba2413
Proantagonist <imacleopard2010@gmail.com>
2025-10-31 07:33:15 +0000

    add remaining layers for mens basketball

--- libs/ts/events/ngx/src/lib/definitions/mens-basketball.definitions.ts
+++ libs/ts/events/ngx/src/lib/definitions/mens-basketball.definitions.ts
@@ -4,37 +4,75 @@ import { MarkdownPopupComponent } from '../modules/popups/markdown-popup/markdow
 import { EventConfiguration, ISpecialEventRoot, SpecialEventOptions } from '../interfaces/special-event.interface';
 
 export enum MENS_BASKETBALL_LAYERS {
-  PARKING_LOTS = 'mens-basketball-parking-lots',
-  ROUTE = 'mens-basketball-route'
+  LINE_PAINT = 'mens-basketball-line-paint',
+  GATES = 'mens-basketball-gates',
+  SYMBOLS = 'mens-basketball-symbols',
+  PARKING_LOTS = 'mens-basketball-parking-lots'
 }
 
-const eventUrl = 'https://gis.it.tamu.edu/arcgis/rest/services/TS/BaseMBasketTennisXCountry/MapServer/9';
+const eventUrl = 'https://gis.it.tamu.edu/arcgis/rest/services/TS/BaseMBasketTennisXCountry/MapServer';
 
 export const MensBasketball_Definitions = {
+  LINE_PAINT: {
+    id: MENS_BASKETBALL_LAYERS.LINE_PAINT,
+    layerId: MENS_BASKETBALL_LAYERS.LINE_PAINT,
+    name: "Men's Basketball Line Paint",
+    url: `${eventUrl}/6`
+  },
+  GATES: {
+    id: MENS_BASKETBALL_LAYERS.GATES,
+    layerId: MENS_BASKETBALL_LAYERS.GATES,
+    name: "Men's Basketball Gates",
+    url: `${eventUrl}/1`
+  },
+  SYMBOLS: {
+    id: MENS_BASKETBALL_LAYERS.SYMBOLS,
+    layerId: MENS_BASKETBALL_LAYERS.SYMBOLS,
+    name: "Men's Basketball Symbols",
+    url: `${eventUrl}/4`
+  },
   PARKING: {
     id: MENS_BASKETBALL_LAYERS.PARKING_LOTS,
     layerId: MENS_BASKETBALL_LAYERS.PARKING_LOTS,
-    name: 'Men\'s Basketball Parking',
-    url: `${eventUrl}/0`
-  },
-  ROUTE: {
-    id: MENS_BASKETBALL_LAYERS.ROUTE,
-    layerId: MENS_BASKETBALL_LAYERS.ROUTE,
-    name: 'Men\'s Basketball Parking Map',
-    url: `${eventUrl}/1`
+    name: "Men's Basketball Parking",
+    url: `${eventUrl}/9`
   }
 };
 
 export const MensBasketball_ColdLayerSources: LayerSource[] = [
   {
     type: 'feature',
-    id: MensBasketball_Definitions.PARKING.id,
-    title: MensBasketball_Definitions.PARKING.name,
-    url: MensBasketball_Definitions.PARKING.url,
+    id: MensBasketball_Definitions.LINE_PAINT.id,
+    title: MensBasketball_Definitions.LINE_PAINT.name,
+    url: MensBasketball_Definitions.LINE_PAINT.url,
+    visible: true,
+    listMode: 'hide',
+    native: {
+      outFields: ['*']
+    }
+  },
+
+  {
+    type: 'feature',
+    id: MensBasketball_Definitions.GATES.id,
+    title: MensBasketball_Definitions.GATES.name,
+    url: MensBasketball_Definitions.GATES.url,
+    visible: true,
+    listMode: 'show',
+    native: {
+      outFields: ['*']
+    }
+  },
+
+  {
+    type: 'feature',
+    id: MensBasketball_Definitions.SYMBOLS.id,
+    title: MensBasketball_Definitions.SYMBOLS.name,
+    url: MensBasketball_Definitions.SYMBOLS.url,
     popupComponent: MarkdownPopupComponent,
     popupData: {
-      name: '{attributes.name}',
-      description: `{attributes.description}`
+      name: '{attributes.Type}',
+      description: `{attributes.Event}`
     },
     visible: true,
     listMode: 'show',
@@ -45,36 +83,27 @@ export const MensBasketball_ColdLayerSources: LayerSource[] = [
 
   {
     type: 'feature',
-    id: MensBasketball_Definitions.ROUTE.id,
-    title: MensBasketball_Definitions.ROUTE.name,
-    url: MensBasketball_Definitions.ROUTE.url,
+    id: MensBasketball_Definitions.PARKING.id,
+    title: MensBasketball_Definitions.PARKING.name,
+    url: MensBasketball_Definitions.PARKING.url,
+    popupComponent: MarkdownPopupComponent,
+    popupData: {
+      name: '{attributes.LotType} - {attributes.LotNum}',
+      description: `{attributes.Description}`
+    },
     visible: true,
+    listMode: 'show',
     native: {
-      listMode: 'show',
-      outFields: ['*'],
-      renderer: {
-        type: 'simple',
-        symbol: {
-          type: 'simple-line',
-          color: 'rgb(56, 168, 0)',
-          width: 2.5,
-          style: 'solid',
-          marker: {
-            style: 'arrow',
-            color: 'rgb(56, 168, 0)',
-            placement: 'end'
-          }
-        }
-      }
+      outFields: ['*']
     }
   }
 ];
 
 export const MensBasketball_Configuration: EventConfiguration = {
   id: 'mens-basketball',
-  name: 'Men\'s Basketball Parking',
-  applicationName: 'Men\'s Basketball Parking Map',
-  shortApplicationName: 'Men\'s Basketball Parking Map',
+  name: "Men's Basketball Parking",
+  applicationName: "Men's Basketball Parking Map",
+  shortApplicationName: "Men's Basketball Parking Map",
   eventDates: ['2025-04-27'],
   zoom: 16,
   mapCenter: [-96.3434, 30.61017]
@@ -90,7 +119,7 @@ export const MensBasketball_Ts: ISpecialEventRoot = {
   discover: {
     id: MensBasketball_Configuration.id,
     name: MensBasketball_Configuration.name,
-    description: 'Transportation and parking information for Men\'s Basketball events.',
+    description: "Transportation and parking information for Men's Basketball events.",
     source: 'internal',
     type: 'event',
     keywords: ['bike', 'mens-basketball', 'cycling', 'parking', 'transportation']
