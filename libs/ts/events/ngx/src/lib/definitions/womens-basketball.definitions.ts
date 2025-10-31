127b711443a6f5d062f1571ba4441aa00e2a03ca
Proantagonist <imacleopard2010@gmail.com>
2025-10-31 08:01:55 +0000

    add remaining layers to womens basketball

--- libs/ts/events/ngx/src/lib/definitions/womens-basketball.definitions.ts
+++ libs/ts/events/ngx/src/lib/definitions/womens-basketball.definitions.ts
@@ -4,37 +4,51 @@ import { MarkdownPopupComponent } from '../modules/popups/markdown-popup/markdow
 import { EventConfiguration, ISpecialEventRoot, SpecialEventOptions } from '../interfaces/special-event.interface';
 
 export enum WOMENS_BASKETBALL_LAYERS {
+  VISITOR_KIOSK = 'womens-basketball-visitor-kiosk',
+  ACCESSIBLE_PARKING = 'womens-basketball-accessible-parking',
   PARKING_LOTS = 'womens-basketball-parking-lots',
-  ROUTE = 'womens-basketball-route'
+  SAFETY_FIRST = 'womens-basketball-safety-first'
 }
 
-const eventUrl = 'https://gis.it.tamu.edu/arcgis/rest/services/TS/TracSocSoftSwimVollWbask/MapServer/12';
+const eventUrl = 'https://gis.it.tamu.edu/arcgis/rest/services/TS/TracSocSoftSwimVollWbask/MapServer';
 
 export const WomensBasketball_Definitions = {
+  VISITOR_KIOSK: {
+    id: WOMENS_BASKETBALL_LAYERS.VISITOR_KIOSK,
+    layerId: WOMENS_BASKETBALL_LAYERS.VISITOR_KIOSK,
+    name: 'Purchase a Visitor Permit Here',
+    url: `${eventUrl}/0`
+  },
+  ACCESSIBLE_PARKING: {
+    id: WOMENS_BASKETBALL_LAYERS.ACCESSIBLE_PARKING,
+    layerId: WOMENS_BASKETBALL_LAYERS.ACCESSIBLE_PARKING,
+    name: 'Accessible Parking Spaces',
+    url: `${eventUrl}/1`
+  },
   PARKING: {
     id: WOMENS_BASKETBALL_LAYERS.PARKING_LOTS,
     layerId: WOMENS_BASKETBALL_LAYERS.PARKING_LOTS,
-    name: 'Women\'s Basketball Parking',
-    url: `${eventUrl}/0`
+    name: "Women's Basketball Event Parking Lots",
+    url: `${eventUrl}/12`
   },
-  ROUTE: {
-    id: WOMENS_BASKETBALL_LAYERS.ROUTE,
-    layerId: WOMENS_BASKETBALL_LAYERS.ROUTE,
-    name: 'Women\'s Basketball Parking Map',
-    url: `${eventUrl}/1`
+  SAFETY_FIRST: {
+    id: WOMENS_BASKETBALL_LAYERS.SAFETY_FIRST,
+    layerId: WOMENS_BASKETBALL_LAYERS.SAFETY_FIRST,
+    name: 'Crosswalks',
+    url: `${eventUrl}/14`
   }
 };
 
 export const WomensBasketball_ColdLayerSources: LayerSource[] = [
   {
     type: 'feature',
-    id: WomensBasketball_Definitions.PARKING.id,
-    title: WomensBasketball_Definitions.PARKING.name,
-    url: WomensBasketball_Definitions.PARKING.url,
+    id: WomensBasketball_Definitions.VISITOR_KIOSK.id,
+    title: WomensBasketball_Definitions.VISITOR_KIOSK.name,
+    url: WomensBasketball_Definitions.VISITOR_KIOSK.url,
     popupComponent: MarkdownPopupComponent,
     popupData: {
-      name: '{attributes.name}',
-      description: `{attributes.description}`
+      name: 'Visitor Permit Kiosk',
+      description: 'Purchase visitor parking permits here'
     },
     visible: true,
     listMode: 'show',
@@ -45,25 +59,66 @@ export const WomensBasketball_ColdLayerSources: LayerSource[] = [
 
   {
     type: 'feature',
-    id: WomensBasketball_Definitions.ROUTE.id,
-    title: WomensBasketball_Definitions.ROUTE.name,
-    url: WomensBasketball_Definitions.ROUTE.url,
+    id: WomensBasketball_Definitions.ACCESSIBLE_PARKING.id,
+    title: WomensBasketball_Definitions.ACCESSIBLE_PARKING.name,
+    url: WomensBasketball_Definitions.ACCESSIBLE_PARKING.url,
+    popupComponent: MarkdownPopupComponent,
+    popupData: {
+      name: '{attributes.Type}',
+      description: `**Event:** {attributes.Event}`
+    },
     visible: true,
+    listMode: 'show',
     native: {
-      listMode: 'show',
+      outFields: ['*']
+    }
+  },
+
+  {
+    type: 'feature',
+    id: WomensBasketball_Definitions.PARKING.id,
+    title: WomensBasketball_Definitions.PARKING.name,
+    url: WomensBasketball_Definitions.PARKING.url,
+    popupComponent: MarkdownPopupComponent,
+    popupData: {
+      name: {
+        field: 'GIS.TS.ParkingLots.LotName',
+        collapsed: true
+      },
+      description: {
+        field: 'GIS.TS.SpEv_Lot_Notes.WBasketballN',
+        collapsed: true
+      }
+    },
+    visible: true,
+    listMode: 'show',
+    native: {
+      outFields: ['*']
+    }
+  },
+
+  {
+    type: 'feature',
+    id: WomensBasketball_Definitions.SAFETY_FIRST.id,
+    title: WomensBasketball_Definitions.SAFETY_FIRST.name,
+    url: WomensBasketball_Definitions.SAFETY_FIRST.url,
+    popupComponent: MarkdownPopupComponent,
+    popupData: {
+      name: 'Crosswalk',
+      description: `**Location:** {attributes.Location}`
+    },
+    visible: true,
+    listMode: 'show',
+    native: {
       outFields: ['*'],
+      definitionExpression: "Street_Use = 'X-Walk'",
       renderer: {
         type: 'simple',
         symbol: {
           type: 'simple-line',
-          color: 'rgb(56, 168, 0)',
-          width: 2.5,
-          style: 'solid',
-          marker: {
-            style: 'arrow',
-            color: 'rgb(56, 168, 0)',
-            placement: 'end'
-          }
+          color: [214, 170, 81, 255],
+          width: 2,
+          style: 'short-dash'
         }
       }
     }
@@ -72,9 +127,9 @@ export const WomensBasketball_ColdLayerSources: LayerSource[] = [
 
 export const WomensBasketball_Configuration: EventConfiguration = {
   id: 'womens-basketball',
-  name: 'Women\'s Basketball Parking',
-  applicationName: 'Women\'s Basketball Parking Map',
-  shortApplicationName: 'Women\'s Basketball Parking Map',
+  name: "Women's Basketball Parking",
+  applicationName: "Women's Basketball Parking Map",
+  shortApplicationName: "Women's Basketball Parking Map",
   eventDates: ['2025-04-27'],
   zoom: 16,
   mapCenter: [-96.3434, 30.61017]
@@ -90,7 +145,7 @@ export const WomensBasketball_Ts: ISpecialEventRoot = {
   discover: {
     id: WomensBasketball_Configuration.id,
     name: WomensBasketball_Configuration.name,
-    description: 'Transportation and parking information for Women\'s Basketball events.',
+    description: "Transportation and parking information for Women's Basketball events.",
     source: 'internal',
     type: 'event',
     keywords: ['bike', 'womens-basketball', 'cycling', 'parking', 'transportation']
