aeda282ec5655305c4c2e5937e151b96a14d3724
Slone Early <sloneearly@tamu.edu>
2025-11-11 20:39:55 +0000

    feat(gisday-angular): add GIS Day event map

--- libs/ts/events/ngx/src/lib/definitions/gis-day-map.definitions.ts
+++ libs/ts/events/ngx/src/lib/definitions/gis-day-map.definitions.ts
@@ -1,30 +1,64 @@
 import { LayerSource } from '@tamu-gisc/common/types';
-
 import { MarkdownPopupComponent } from '../modules/popups/markdown-popup/markdown-popup.component';
 import { EventConfiguration, ISpecialEventRoot, SpecialEventOptions } from '../interfaces/special-event.interface';
-export enum GIS_DAY_LAYERS {
-  GIS_DAY_MSC = 'gis-day-msc'
-}
-const gisDayUrl = 'https://services1.arcgis.com/qr14biwnHA6Vis6l/ArcGIS/rest/services/MSC_and_Rudder_Building_Polygon_Layer/FeatureServer/0'
 
-const GisDayEventDefinitions = {
-  MSC: {
-    id: GIS_DAY_LAYERS.GIS_DAY_MSC,
-    layerId: GIS_DAY_LAYERS.GIS_DAY_MSC,
-    name: 'GIS Day – MSC',
-    url: gisDayUrl
-  }
-};
+export enum GIS_DAY_LAYERS {
+  MSC_RUDDER = 'gis-day-msc-rudder',
+  GARAGES = 'gis-day-garages',
+  CSG = 'gis-day-csg'
+}
+
+const GARAGES_URL =
+  'https://services1.arcgis.com/qr14biwnHA6Vis6l/ArcGIS/rest/services/MSC_and_Rudder_Building_Polygon_Layer/FeatureServer/0';
+
+const CSG_URL =
+  'https://services1.arcgis.com/qr14biwnHA6Vis6l/ArcGIS/rest/services/MSC_and_Rudder_Building_Polygon_Layer/FeatureServer/1';
+
+const MSC_RUDDER_URL =
+  'https://services1.arcgis.com/qr14biwnHA6Vis6l/ArcGIS/rest/services/MSC_and_Rudder_Building_Polygon_Layer/FeatureServer/2';
 
 export const GisDayLayerSources: LayerSource[] = [
   {
     type: 'feature',
-    id: GisDayEventDefinitions.MSC.id,
-    title: GisDayEventDefinitions.MSC.name,
-    url: GisDayEventDefinitions.MSC.url,
+    id: GIS_DAY_LAYERS.MSC_RUDDER,
+    title: 'MSC & Rudder Buildings',
+    url: MSC_RUDDER_URL,
     popupComponent: MarkdownPopupComponent,
     popupData: {
-      name: 'MSC_and_Rudder_Buildings'
+      name: 'attributes.Name',         
+      description: 'attributes.Description' 
+    },
+    visible: true,
+    listMode: 'show',
+    native: {
+      outFields: ['*']
+    }
+  },
+  {
+    type: 'feature',
+    id: GIS_DAY_LAYERS.GARAGES,
+    title: 'GIS Day Parking Garages',
+    url: GARAGES_URL,
+    popupComponent: MarkdownPopupComponent,
+    popupData: {
+      name: 'attributes.Name',
+      description: 'attributes.Description'
+    },
+    visible: true,
+    listMode: 'show',
+    native: {
+      outFields: ['*']
+    }
+  },
+  {
+    type: 'feature',
+    id: GIS_DAY_LAYERS.CSG,
+    title: 'Gene Stallings Parking Garage',
+    url: CSG_URL,
+    popupComponent: MarkdownPopupComponent,
+    popupData: {
+      name: 'attributes.Name',
+      description: 'attributes.Description'
     },
     visible: true,
     listMode: 'show',
@@ -35,12 +69,12 @@ export const GisDayLayerSources: LayerSource[] = [
 ];
 
 export const GisDayConfiguration: EventConfiguration = {
-  id: 'gis-day-msc',
-  name: 'GIS Day @ MSC',
+  id: 'gis-day',
+  name: 'GIS Day',
   applicationName: 'GIS Day Event Map',
   shortApplicationName: 'GIS Day',
-  introductionText: 'Map of GIS Day events at the Memorial Student Center.',
-  eventDates: ['2025-11-17', '2025-11-18','2025-11-19','2025-11-20','2025-11-21'],
+  introductionText: 'Building and parking locations for GIS Day.',
+  eventDates: ['2025-11-17', '2025-11-18', '2025-11-19', '2025-11-20', '2025-11-21'],
   mapCenter: [-96.3417, 30.6122],
   zoom: 18
 };
@@ -55,9 +89,9 @@ export const GisDayTs: ISpecialEventRoot = {
   discover: {
     id: GisDayConfiguration.id,
     name: GisDayConfiguration.name,
-    description: 'Locations for Texas A&M GIS Day.',
+    description: 'Building and parking location information for GIS Day.',
     source: 'internal',
     type: 'event',
-    keywords: ['gis', 'gis day', 'msc', 'tamu']
+    keywords: ['gis', 'msc', 'rudder', 'parking']
   }
 };
