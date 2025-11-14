1870301ba06b844e797155b5584287da144f819b
Slone Early <sloneearly@tamu.edu>
2025-11-14 19:50:19 +0000

    Add family bathrooms with popup info

--- libs/aggiemap/ngx/common/src/lib/definitions.ts
+++ libs/aggiemap/ngx/common/src/lib/definitions.ts
@@ -95,8 +95,15 @@ export function Definitions(Connections: IComposedConnections): IComposedIDefini
     AGGIEPRINT_LOCATIONS: {
       id: 'aggieprint-locations',
       layerId: 'aggieprint-locations-layer',
-      name: 'AggiePrint Locations',
+      name: 'Aggieprint Locations',
       url: 'https://services1.arcgis.com/qr14biwnHA6Vis6l/ArcGIS/rest/services/TAMUPrinters/FeatureServer/0',
+      popupComponent: Popups.MarkdownWDirectionsPopupComponent
+    },
+    FAMILY_FRIENDLY_BATHROOMS: {
+      id: 'family-friendly-bathrooms-locations',
+      layerId: 'family-friendly-bathrooms-locations-layer',
+      name: 'Family Friendly Bathroom Locations',
+      url: 'https://gis.tamu.edu/arcgis/rest/services/FCOR/MapInfo_20190529/MapServer/1',
       popupComponent: Popups.MarkdownWDirectionsPopupComponent
     }
   };
@@ -125,4 +132,5 @@ export interface IComposedIDefinitions {
   BIKE_LOCATIONS: IDefinition;
   DINING_LOCATIONS: IDefinition;
   AGGIEPRINT_LOCATIONS: IDefinition;
+  FAMILY_FRIENDLY_BATHROOMS: IDefinition;
 }
