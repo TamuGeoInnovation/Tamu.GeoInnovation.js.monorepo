bbfb2027a1604b66b5a8b219785ddcaf9e5a305b
Slone Early <sloneearly@tamu.edu>
2025-12-09 21:40:59 +0000

    Updated all instances of Construction Zone to Construction Zones

--- apps/ts-ring-day-angular/src/environments/definitions.ts
+++ apps/ts-ring-day-angular/src/environments/definitions.ts
@@ -28,7 +28,7 @@ export const Definitions = {
   CONSTRUCTION: {
     id: 'construction_zone',
     layerId: 'construction_zone-layer',
-    name: 'Construction Zone',
+    name: 'Construction Zones',
     url: `${Connections.constructionUrl}`,
     popupComponent: Popups.ConstructionPopupComponent
   },
