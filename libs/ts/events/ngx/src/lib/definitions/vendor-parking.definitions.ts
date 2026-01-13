ea12ba329b3f6659216bce68f69946a604a48fde
DaltonPeterson <dalton03peterson@gmail.com>
2026-01-13 19:33:14 +0000

    Updating Vendor Parking Map.

--- libs/ts/events/ngx/src/lib/definitions/vendor-parking.definitions.ts
+++ libs/ts/events/ngx/src/lib/definitions/vendor-parking.definitions.ts
@@ -1,62 +1,47 @@
 import { LayerSource } from '@tamu-gisc/common/types';
 
 import { MarkdownPopupComponent } from '../modules/popups/markdown-popup/markdown-popup.component';
+import { MarkdownWDirectionsPopupComponent } from '../modules/popups/markdown-w-directions-popup/markdown-w-directions-popup.component';
 import { EventConfiguration, ISpecialEventRoot, SpecialEventOptions } from '../interfaces/special-event.interface';
+
+// import esri = __esri
 
 export enum VENDOR_PARKING_LAYERS {
   CONSTRUCTION = 'Construction',
-  VENDOR_LOTS = 'Vendor Parking Lots',
-  BUSINESS_SPOTS = 'Marked Business Spots'
+  MARKED_BUSINESS_SPACES = 'Marked Business Spaces',
+  VENDOR_PARKING_LOTS = 'Vendor Parking Lots'
 }
 
 const eventUrl = 'https://gis.tamu.edu/arcgis/rest/services/TS/AVPVisBSUBVenNWRetNSCMed/MapServer';
 
-export const VendorParking_Definitions = {
+export const VendorParkingDefinitions = {
   CONSTRUCTION: {
     id: VENDOR_PARKING_LAYERS.CONSTRUCTION,
     layerId: VENDOR_PARKING_LAYERS.CONSTRUCTION,
-    name: "Construction",
+    name: 'Construction',
     url: `${eventUrl}/0`
   },
-  VENDOR_LOTS: {
-    id: VENDOR_PARKING_LAYERS.VENDOR_LOTS,
-    layerId: VENDOR_PARKING_LAYERS.VENDOR_LOTS,
-    name: "Vendor Parking Lots",
-    url: `${eventUrl}/9`
-  },
-  BUSINESS_SPOTS: {
-    id: VENDOR_PARKING_LAYERS.VENDOR_LOTS,
-    layerId: VENDOR_PARKING_LAYERS.VENDOR_LOTS,
-    name: "Marked Business Spots",
+  MARKED_BUSINESS_SPACES: {
+    id: VENDOR_PARKING_LAYERS.MARKED_BUSINESS_SPACES,
+    layerId: VENDOR_PARKING_LAYERS.MARKED_BUSINESS_SPACES,
+    name: 'Marked Business Spaces',
     url: `${eventUrl}/2`
+  },
+    VENDOR_PARKING_LOTS: {
+    id: VENDOR_PARKING_LAYERS.VENDOR_PARKING_LOTS,
+    layerId: VENDOR_PARKING_LAYERS.VENDOR_PARKING_LOTS,
+    name: 'Vendor Parking Lots',
+    url: `${eventUrl}/9`
   }
 };
 
-export const VendorParking_ColdLayerSources: LayerSource[] = [
+export const VendorParkingColdLayerSources: LayerSource[] = [
   {
     type: 'feature',
-    id: VendorParking_Definitions.CONSTRUCTION.id,
-    title: VendorParking_Definitions.CONSTRUCTION.name,
-    url: VendorParking_Definitions.CONSTRUCTION.url,
-    visible: true,
-    listMode: 'show',
-    native: {
-      outFields: ['*']
-    }
-  },
-
-  {
-    type: 'feature',
-    id: VendorParking_Definitions.VENDOR_LOTS.id,
-    title: VendorParking_Definitions.VENDOR_LOTS.name,
-    url: VendorParking_Definitions.VENDOR_LOTS.url,
+    id: VendorParkingDefinitions.CONSTRUCTION.id,
+    title: VendorParkingDefinitions.CONSTRUCTION.name,
+    url: VendorParkingDefinitions.CONSTRUCTION.url,
     popupComponent: MarkdownPopupComponent,
-    popupData: {
-      name: '{attributes.Type}',
-      description: `{attributes.Event}`
-    },
-    visible: true,
-    listMode: 'show',
     native: {
       outFields: ['*']
     }
@@ -64,40 +49,86 @@ export const VendorParking_ColdLayerSources: LayerSource[] = [
 
   {
     type: 'feature',
-    id: VendorParking_Definitions.BUSINESS_SPOTS.id,
-    title: VendorParking_Definitions.BUSINESS_SPOTS.name,
-    url: VendorParking_Definitions.BUSINESS_SPOTS.url,
+    id: VendorParkingDefinitions.MARKED_BUSINESS_SPACES.id,
+    title: VendorParkingDefinitions.MARKED_BUSINESS_SPACES.name,
+    url: VendorParkingDefinitions.MARKED_BUSINESS_SPACES.url,
+    popupComponent: MarkdownWDirectionsPopupComponent,
     visible: true,
-    listMode: 'show',
     native: {
       outFields: ['*']
-    }
+    },
   },
+
+  {
+    type: 'feature',
+    id: VendorParkingDefinitions.VENDOR_PARKING_LOTS.id,
+    title: VendorParkingDefinitions.VENDOR_PARKING_LOTS.name,
+    url: VendorParkingDefinitions.VENDOR_PARKING_LOTS.url,
+    popupComponent: MarkdownPopupComponent,
+    visible: true,
+    native: {
+      outFields: ['*'],
+      // renderer: {
+      //   type: 'unique-value',
+      //   field: 'GIS.TS.Lot_Use.Vendor_Lot',
+      //   field2: 'GIS.TS.ParkingLots.LotType',
+      //   fieldDelimiter: ',',
+      //   uniqueValueInfos: [
+      //             {
+      //               value: '1,Surface',
+      //               label: 'Vender Permit and Vendor+ Permit Authorized',
+      //               symbol: {
+      //                 type: 'simple-fill',
+      //                 color: 'rgb(90, 0, 0)'
+      //               } as unknown as esri.SimpleFillSymbolProperties
+      //             },
+      //             {
+      //               value: '1,Street',
+      //               label: 'Vender Permit and Vendor+ Permit Authorized',
+      //               symbol: {
+      //                 type: 'simple-fill',
+      //                 color: 'rgb(90, 0, 0)'
+      //               } as unknown as esri.SimpleFillSymbolProperties
+      //             },
+      //             {
+      //               value: '1,Garage Visitor',
+      //               label: 'Vender Permit and Vendor+ Permit Authorized',
+      //               symbol: {
+      //                 type: 'simple-fill',
+      //                 color: 'rgb(90, 0, 0)'
+      //               } as unknown as esri.SimpleFillSymbolProperties
+      //             },
+      //           ]
+      // }
+    },
+  }
 ];
 
-export const VendorParking_Configuration: EventConfiguration = {
+export const VendorParkingConfiguration: EventConfiguration = {
   id: 'vendor-parking',
-  name: "Vendor Parking",
-  applicationName: "Vendor Parking Map",
-  shortApplicationName: "Vendor Parking",
+  name: 'Vendor Parking',
+  applicationName: 'Vendor Parking Map',
+  shortApplicationName: 'Vendor Parking Map',
+  introductionText: 'Parking Map for Vendors',
   eventDates: [],
-  zoom: 17,
-  mapCenter: [-96.35106, 30.61118]
+  mapCenter: [-96.33771, 30.62143],
+  zoom: 17
 };
 
-export const VendorParking_Options: SpecialEventOptions = [];
+export const VendorParkingOptions: SpecialEventOptions = [];
 
 export const VendorParking_Ts: ISpecialEventRoot = {
-  configuration: VendorParking_Configuration,
-  options: VendorParking_Options,
-  sources: VendorParking_ColdLayerSources,
+  configuration: VendorParkingConfiguration,
+  options: VendorParkingOptions,
+  sources: VendorParkingColdLayerSources,
   references: VENDOR_PARKING_LAYERS,
   discover: {
-    id: VendorParking_Configuration.id,
-    name: VendorParking_Configuration.name,
-    description: "Parking Information for Vendors",
+    id: VendorParkingConfiguration.id,
+    name: VendorParkingConfiguration.name,
+    description: 'Parking information for vendors.',
     source: 'internal',
     type: 'event',
     keywords: ['vendor', 'parking', 'transportation']
   }
 };
+
