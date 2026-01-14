c4c57d986de312eaf41025828e6c497498f7780b
Slone Early <sloneearly@tamu.edu>
2026-01-14 16:28:09 +0000

    Built config file for move in map

--- libs/ts/events/ngx/src/lib/definitions/all.definitions.ts
+++ libs/ts/events/ngx/src/lib/definitions/all.definitions.ts
@@ -18,7 +18,7 @@ import { ISpecialEventRoot } from '../interfaces/special-event.interface';
 import { MensBasketball_Ts } from './mens-basketball.definitions';
 import { WomensBasketball_Ts } from './womens-basketball.definitions';
 import { GisDayTs } from './gis-day-map.definitions';
-
+import { MoveInTs } from './move-in.definitions';
 export const EventDefinitions: Array<ISpecialEventRoot> = [
   FourHRoundupTs,
   AggielandSaturdayEventTs,
@@ -38,5 +38,6 @@ export const EventDefinitions: Array<ISpecialEventRoot> = [
   FootballParkingEvent,
   RingDayEvent,
   WomensBasketball_Ts,
-  GisDayTs
+  GisDayTs,
+  MoveInTs
 ];
