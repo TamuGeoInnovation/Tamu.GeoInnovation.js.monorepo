acb7f20b454958babb7f1edbb90059a2469297aa
Slone Early <sloneearly@tamu.edu>
2026-01-21 14:45:40 +0000

    Added definition for summer break parking

--- libs/ts/events/ngx/src/lib/definitions/all.definitions.ts
+++ libs/ts/events/ngx/src/lib/definitions/all.definitions.ts
@@ -18,7 +18,7 @@ import { ISpecialEventRoot } from '../interfaces/special-event.interface';
 import { MensBasketball_Ts } from './mens-basketball.definitions';
 import { WomensBasketball_Ts } from './womens-basketball.definitions';
 import { GisDayTs } from './gis-day-map.definitions';
-import { BreakSummerTs } from './break_summer.definitions';
+import { BreakSummerParkingTs } from './break-summer.definitions';
 
 export const EventDefinitions: Array<ISpecialEventRoot> = [
   FourHRoundupTs,
@@ -40,5 +40,5 @@ export const EventDefinitions: Array<ISpecialEventRoot> = [
   RingDayEvent,
   WomensBasketball_Ts,
   GisDayTs,
-  BreakSummerTs
+  BreakSummerParkingTs
 ];
