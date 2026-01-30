09a44646a6acfcb0357976785796875e6fb2f6ee
Slone Early <sloneearly@tamu.edu>
2026-01-30 19:01:59 +0000

    Simplified for testing purposes

--- libs/ts/events/ngx/src/lib/definitions/break-summer.definitions.ts
+++ libs/ts/events/ngx/src/lib/definitions/break-summer.definitions.ts
@@ -1,76 +1,54 @@
 import { LayerSource } from '@tamu-gisc/common/types';
 
+import { MarkdownPopupComponent } from '../modules/popups/markdown-popup/markdown-popup.component';
 import { EventConfiguration, ISpecialEventRoot, SpecialEventOptions } from '../interfaces/special-event.interface';
 
-import esri = __esri;
+// import esri = __esri
 
 export enum BREAK_SUMMER_PARKING_LAYERS {
-  BREAK_SUMMER_PARKING = 'break-summer-parking',
-  CONSTRUCTION = 'construction'
+  CONSTRUCTION = 'Construction',
+  BREAK_SUMMER_PARKING_LOTS = 'Break-Summer Parking Lots'
 }
 
 const eventUrl = 'https://gis.tamu.edu/arcgis/rest/services/TS/AVPVisBSUBVenNWRetNSCMed/MapServer';
 
-export const BreakSummerParkingEventDefinitions = {
-  BREAK_SUMMER_PARKING: {
-    id: BREAK_SUMMER_PARKING_LAYERS.BREAK_SUMMER_PARKING,
-    layerId: BREAK_SUMMER_PARKING_LAYERS.BREAK_SUMMER_PARKING,
-    name: 'Break-Summer Parking Lots',
-    url: `${eventUrl}/5`
-  },
+export const BreakSummerParkingDefinitions = {
   CONSTRUCTION: {
     id: BREAK_SUMMER_PARKING_LAYERS.CONSTRUCTION,
     layerId: BREAK_SUMMER_PARKING_LAYERS.CONSTRUCTION,
     name: 'Construction',
     url: `${eventUrl}/0`
+  },
+  BREAK_SUMMER_PARKING_LOTS: {
+    id: BREAK_SUMMER_PARKING_LAYERS.BREAK_SUMMER_PARKING_LOTS,
+    layerId: BREAK_SUMMER_PARKING_LAYERS.BREAK_SUMMER_PARKING_LOTS,
+    name: 'Break-Summer Parking Lots',
+    url: `${eventUrl}/5`
   }
 };
 
 export const BreakSummerParkingColdLayerSources: LayerSource[] = [
   {
     type: 'feature',
-    id: BreakSummerParkingEventDefinitions.BREAK_SUMMER_PARKING.id,
-    title: BreakSummerParkingEventDefinitions.BREAK_SUMMER_PARKING.name,
-    url: BreakSummerParkingEventDefinitions.BREAK_SUMMER_PARKING.url,
-    visible: true,
-    listMode: 'show',
+    id: BreakSummerParkingDefinitions.CONSTRUCTION.id,
+    title: BreakSummerParkingDefinitions.CONSTRUCTION.name,
+    url: BreakSummerParkingDefinitions.CONSTRUCTION.url,
+    popupComponent: MarkdownPopupComponent,
     native: {
-      outFields: ['*'],
-      renderer: {
-        type: 'simple-fill',
-        symbol: {
-          type: 'simple-fill',
-          color: [70, 130, 180, 0.6],
-          outline: {
-            color: [0, 0, 0],
-            width: 2
-          }
-        }
-      } as unknown as esri.RendererProperties
+      outFields: ['*']
     }
   },
-
   {
     type: 'feature',
-    id: BreakSummerParkingEventDefinitions.CONSTRUCTION.id,
-    title: BreakSummerParkingEventDefinitions.CONSTRUCTION.name,
-    url: BreakSummerParkingEventDefinitions.CONSTRUCTION.url,
+    id: BreakSummerParkingDefinitions.BREAK_SUMMER_PARKING_LOTS.id,
+    title: BreakSummerParkingDefinitions.BREAK_SUMMER_PARKING_LOTS.name,
+    url: BreakSummerParkingDefinitions.BREAK_SUMMER_PARKING_LOTS.url,
+    popupComponent: MarkdownPopupComponent,
     visible: true,
-    listMode: 'show',
     native: {
-      outFields: ['*'],
-      renderer: {
-        type: 'simple',
-        symbol: {
-          type: 'simple-fill',
-          color: [70, 130, 180, 0.6],
-          outline: {
-            color: [0, 0, 0],
-            width: 2
-          }
-        }
-      } as unknown as esri.SimpleRendererProperties
+      outFields: ['*']
     }
+  }
 ];
 
 export const BreakSummerParkingConfiguration: EventConfiguration = {
@@ -78,6 +56,7 @@ export const BreakSummerParkingConfiguration: EventConfiguration = {
   name: 'Break/Summer Parking',
   applicationName: 'Break/Summer Parking Map',
   shortApplicationName: 'Break/Summer Parking Map',
+  introductionText: 'Parking areas authorized during break and summer sessions.',
   eventDates: [],
   mapCenter: [-96.34467, 30.60585],
   zoom: 16
@@ -87,8 +66,8 @@ export const BreakSummerParkingOptions: SpecialEventOptions = [];
 
 export const BreakSummerParkingTs: ISpecialEventRoot = {
   configuration: BreakSummerParkingConfiguration,
-  sources: BreakSummerParkingColdLayerSources,
   options: BreakSummerParkingOptions,
+  sources: BreakSummerParkingColdLayerSources,
   references: BREAK_SUMMER_PARKING_LAYERS,
   discover: {
     id: BreakSummerParkingConfiguration.id,
