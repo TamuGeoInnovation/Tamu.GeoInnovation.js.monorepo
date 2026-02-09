95915f687ea71096ac44641561ec584fb25351b5
Slone Early <sloneearly@tamu.edu>
2026-02-09 19:29:50 +0000

    Updated with new TS source

--- libs/ts/events/ngx/src/lib/definitions/timed-parking.definitions.ts
+++ libs/ts/events/ngx/src/lib/definitions/timed-parking.definitions.ts
@@ -4,64 +4,56 @@ import { MarkdownPopupComponent } from '../modules/popups/markdown-popup/markdow
 import { EventConfiguration, ISpecialEventRoot, SpecialEventOptions } from '../interfaces/special-event.interface';
 
 export enum TIMED_PARKING_LAYERS {
-  TIMED_PARKING_AREA = 'Timed Parking in this area',
   TIMED_PARKING_SPACE = 'Timed Parking Space'
 }
 
-const eventUrl = 'https://gis.tamu.edu/arcgis/rest/services/TS/DisabledTimedMotorcycle/MapServer';
+const eventUrl = 'https://gis.tamu.edu/arcgis/rest/services/TS/TimedParking/MapServer';
 
 export const TimedParkingDefinitions = {
-  TIMED_PARKING_AREA: {
-    id: TIMED_PARKING_LAYERS.TIMED_PARKING_AREA,
-    layerId: TIMED_PARKING_LAYERS.TIMED_PARKING_AREA,
-    name: 'Timed Spaces in this area',
-    url: `${eventUrl}/5`
-  },
   TIMED_PARKING_SPACE: {
     id: TIMED_PARKING_LAYERS.TIMED_PARKING_SPACE,
     layerId: TIMED_PARKING_LAYERS.TIMED_PARKING_SPACE,
     name: 'Timed Parking Space',
-    url: `${eventUrl}/6`
+    url: `${eventUrl}/0`
   }
 };
 
 export const TimedParkingColdLayerSources: LayerSource[] = [
   {
     type: 'feature',
-    id: TimedParkingDefinitions.TIMED_PARKING_AREA.id,
-    title: TimedParkingDefinitions.TIMED_PARKING_AREA.name,
-    url: TimedParkingDefinitions.TIMED_PARKING_AREA.url,
-    // popupComponent: MarkdownPopupComponent,
-    visible: true,
-    native: {
-      listMode: 'show',
-      outFields: ['*']
-    }
-  },
-  {
-    type: 'feature',
     id: TimedParkingDefinitions.TIMED_PARKING_SPACE.id,
     title: TimedParkingDefinitions.TIMED_PARKING_SPACE.name,
     url: TimedParkingDefinitions.TIMED_PARKING_SPACE.url,
-    // popupComponent: MarkdownPopupComponent,
     visible: true,
+    listMode: 'show',
+    popupComponent: MarkdownPopupComponent,
+    popupData: {
+      name: {
+        field: 'LotName',
+        collapsed: true
+      },
+      description: {
+        field: 'Anno_Type',
+        collapsed: true
+      }
+    },
     native: {
-      listMode: 'show',
       outFields: ['*']
     }
-  },
+  }
 ];
 
 export const TimedParkingConfiguration: EventConfiguration = {
   id: 'timed-parking',
   name: 'Timed Parking',
-  applicationName: 'Areas with Timed Parking',
-  shortApplicationName: 'Timed Parking Map',
+  applicationName: 'Timed Parking Map',
+  shortApplicationName: 'Timed Parking',
   eventDates: [],
-  mapCenter: [-96.33771, 30.62143]
+  mapCenter: [-96.34409, 30.6073],
+  zoom: 18
 };
 
-export const TimedParkingOptions: SpecialEventOptions = []
+export const TimedParkingOptions: SpecialEventOptions = [];
 
 export const TimedParking_Ts: ISpecialEventRoot = {
   configuration: TimedParkingConfiguration,
@@ -71,9 +63,9 @@ export const TimedParking_Ts: ISpecialEventRoot = {
   discover: {
     id: TimedParkingConfiguration.id,
     name: TimedParkingConfiguration.name,
-    description: 'Areas with timed parking on campus.',
+    description: 'Timed parking spaces on campus.',
     source: 'internal',
     type: 'event',
-    keywords: ['timed', 'parking', 'transportation', '30 minute', '30 min']
+    keywords: ['timed', 'parking', 'transportation']
   }
 };
