ac6fc35cf0f5b0464540ec311c7faf9a7650e460
Slone Early <sloneearly@tamu.edu>
2026-02-09 19:09:51 +0000

    chore(events): Update retiree parking TS service + popup

--- libs/ts/events/ngx/src/lib/definitions/retiree-parking.definitions.ts
+++ libs/ts/events/ngx/src/lib/definitions/retiree-parking.definitions.ts
@@ -1,73 +1,45 @@
 import { LayerSource } from '@tamu-gisc/common/types';
 
-import {
-  AggiemapCustomMapConfiguration,
-  EventConfiguration,
-  SpecialEventOptions
-} from '../interfaces/special-event.interface';
+import { EventConfiguration, ISpecialEventRoot, SpecialEventOptions } from '../interfaces/special-event.interface';
+import { MarkdownPopupComponent } from '../modules/popups/markdown-popup/markdown-popup.component';
 
 export enum RETIREE_PARKING_LAYERS {
-  LOT_SPECIFIC = 'Lot Specific Permit Required',
-  RETIREE_AUTHORIZED = 'Retiree Permit Authorized'
+  RETIREE_PARKING_LOTS = 'Retiree Parking Lots'
 }
 
-const eventUrl = 'https://gis.tamu.edu/arcgis/rest/services/TS/AVPVisBSUBVenNWRetNSCMed/MapServer';
+const eventUrl = 'https://gis.tamu.edu/arcgis/rest/services/TS/RetireeParking/MapServer';
 
 export const RetireeParkingDefinitions = {
-  LOT_SPECIFIC: {
-    id: RETIREE_PARKING_LAYERS.LOT_SPECIFIC,
-    layerId: RETIREE_PARKING_LAYERS.LOT_SPECIFIC,
-    name: 'Lot Specific Permit Required',
-    url: `${eventUrl}/7`
-  },
-  RETIREE_AUTHORIZED: {
-    id: RETIREE_PARKING_LAYERS.RETIREE_AUTHORIZED,
-    layerId: RETIREE_PARKING_LAYERS.RETIREE_AUTHORIZED,
-    name: 'Retiree Permit Authorized',
-    url: `${eventUrl}/7`
+  RETIREE_PARKING_LOTS: {
+    id: RETIREE_PARKING_LAYERS.RETIREE_PARKING_LOTS,
+    layerId: RETIREE_PARKING_LAYERS.RETIREE_PARKING_LOTS,
+    name: 'Retiree Parking Lots',
+    url: `${eventUrl}/0`
   }
 };
 
 export const RetireeParkingColdLayerSources: LayerSource[] = [
   {
     type: 'feature',
-    id: RetireeParkingDefinitions.RETIREE_AUTHORIZED.id,
-    title: RetireeParkingDefinitions.RETIREE_AUTHORIZED.name,
-    url: RetireeParkingDefinitions.RETIREE_AUTHORIZED.url,
+    id: RetireeParkingDefinitions.RETIREE_PARKING_LOTS.id,
+    title: RetireeParkingDefinitions.RETIREE_PARKING_LOTS.name,
+    url: RetireeParkingDefinitions.RETIREE_PARKING_LOTS.url,
     visible: true,
     listMode: 'show',
-    native: {
-      outFields: ['*'],
-      definitionExpression: `"GIS.TS.Lot_Use.Retired_Lot" = 1`,
-      renderer: {
-        type: 'simple',
-        symbol: {
-          type: 'simple-fill',
-          color: [90, 0, 0, 255],
-          outline: null
-        }
+    popupComponent: MarkdownPopupComponent,
+    popupData: {
+      name: {
+        field: 'GIS.TS.ParkingLots.Name',
+        collapsed: true
+      },
+      description: {
+        field: 'GIS.TS.Lot_Notes.RetireeN',
+        collapsed: true
       }
-    } as any
-  },
-  {
-    type: 'feature',
-    id: RetireeParkingDefinitions.LOT_SPECIFIC.id,
-    title: RetireeParkingDefinitions.LOT_SPECIFIC.name,
-    url: RetireeParkingDefinitions.LOT_SPECIFIC.url,
-    visible: true,
-    listMode: 'show',
+    },
     native: {
-      outFields: ['*'],
-      definitionExpression: `"GIS.TS.Lot_Use.Retired_Lot" <> 1 OR "GIS.TS.Lot_Use.Retired_Lot" IS NULL`,
-      renderer: {
-        type: 'simple',
-        symbol: {
-          type: 'simple-fill',
-          color: [204, 204, 204, 255],
-          outline: null
-        }
-      }
-    } as any
+      outFields: ['*']
+    }
   }
 ];
 
