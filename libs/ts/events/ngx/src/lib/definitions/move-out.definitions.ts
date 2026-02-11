62780912ea1204a97b4559a62aab6775b22e7287
Slone Early <sloneearly@tamu.edu>
2026-02-11 15:54:37 +0000

    Updated TS source

--- libs/ts/events/ngx/src/lib/definitions/move-out.definitions.ts
+++ libs/ts/events/ngx/src/lib/definitions/move-out.definitions.ts
@@ -1,5 +1,6 @@
 import { LayerSource } from '@tamu-gisc/common/types';
 
+import { MarkdownPopupComponent } from '../modules/popups/markdown-popup/markdown-popup.component';
 import {
   AggiemapCustomMapConfiguration,
   EventConfiguration,
@@ -7,46 +8,35 @@ import {
 } from '../interfaces/special-event.interface';
 
 export enum MOVE_OUT_LAYERS {
-  CONSTRUCTION = 'Construction',
   NO_PARKING = 'No Parking Areas',
-  STREET_PARKING = 'Move-Out Allowed Street Parking'
+  STREET_PARKING = 'Move-Out Allowed Street Parking',
+  MOVE_OUT_LOTS = 'Move-Out Lots'
 }
 
-const eventUrl = 'https://gis.tamu.edu/arcgis/rest/services/TS/MoveInMoveOut/MapServer';
+const eventUrl = 'https://gis.tamu.edu/arcgis/rest/services/TS/MoveOutParking/MapServer';
 
 export const MoveOutDefinitions = {
-  CONSTRUCTION: {
-    id: MOVE_OUT_LAYERS.CONSTRUCTION,
-    layerId: MOVE_OUT_LAYERS.CONSTRUCTION,
-    name: 'Construction',
-    url: `${eventUrl}/0`
-  },
   NO_PARKING: {
     id: MOVE_OUT_LAYERS.NO_PARKING,
     layerId: MOVE_OUT_LAYERS.NO_PARKING,
     name: 'No Parking Areas',
-    url: `${eventUrl}/1`
+    url: `${eventUrl}/0`
   },
   STREET_PARKING: {
     id: MOVE_OUT_LAYERS.STREET_PARKING,
     layerId: MOVE_OUT_LAYERS.STREET_PARKING,
-    name: 'Street Parking',
-    url: `${eventUrl}/4`
+    name: 'Move-Out Allowed Street Parking',
+    url: `${eventUrl}/1`
+  },
+  MOVE_OUT_LOTS: {
+    id: MOVE_OUT_LAYERS.MOVE_OUT_LOTS,
+    layerId: MOVE_OUT_LAYERS.MOVE_OUT_LOTS,
+    name: 'Move-Out Lots',
+    url: `${eventUrl}/2`
   }
 };
 
 export const MoveOutColdLayerSources: LayerSource[] = [
-  {
-    type: 'feature',
-    id: MoveOutDefinitions.CONSTRUCTION.id,
-    title: MoveOutDefinitions.CONSTRUCTION.name,
-    url: MoveOutDefinitions.CONSTRUCTION.url,
-    visible: true,
-    listMode: 'show',
-    native: {
-      outFields: ['*']
-    }
-  },
   {
     type: 'feature',
     id: MoveOutDefinitions.NO_PARKING.id,
@@ -54,6 +44,17 @@ export const MoveOutColdLayerSources: LayerSource[] = [
     url: MoveOutDefinitions.NO_PARKING.url,
     visible: true,
     listMode: 'show',
+    popupComponent: MarkdownPopupComponent,
+    popupData: {
+      name: {
+        field: 'Type',
+        collapsed: true
+      },
+      description: {
+        field: 'Note',
+        collapsed: true
+      }
+    },
     native: {
       outFields: ['*']
     }
@@ -65,6 +66,39 @@ export const MoveOutColdLayerSources: LayerSource[] = [
     url: MoveOutDefinitions.STREET_PARKING.url,
     visible: true,
     listMode: 'show',
+    popupComponent: MarkdownPopupComponent,
+    popupData: {
+      name: {
+        field: 'A_Name',
+        collapsed: true
+      },
+      description: {
+        field: 'SP_SH_Notes',
+        collapsed: true
+      }
+    },
+    native: {
+      outFields: ['*']
+    }
+  },
+  {
+    type: 'feature',
+    id: MoveOutDefinitions.MOVE_OUT_LOTS.id,
+    title: MoveOutDefinitions.MOVE_OUT_LOTS.name,
+    url: MoveOutDefinitions.MOVE_OUT_LOTS.url,
+    visible: true,
+    listMode: 'show',
+    popupComponent: MarkdownPopupComponent,
+    popupData: {
+      name: {
+        field: 'GIS.TS.ParkingLots.LotName',
+        collapsed: true
+      },
+      description: {
+        field: 'GIS.TS.SpEv_Lot_Notes.MoveOutN',
+        collapsed: true
+      }
+    },
     native: {
       outFields: ['*']
     }
@@ -84,11 +118,11 @@ export const MoveOutConfiguration: EventConfiguration = {
 export const MoveOutOptions: SpecialEventOptions = [];
 
 export const MoveOut: AggiemapCustomMapConfiguration = {
+  type: 'general-map',
   configuration: MoveOutConfiguration,
   options: MoveOutOptions,
   sources: MoveOutColdLayerSources,
   references: MOVE_OUT_LAYERS,
-  type: 'special-event',
   discover: {
     id: MoveOutConfiguration.id,
     name: MoveOutConfiguration.name,
