54ce6b79af1bb625cfd93edad8535c732ddfcecf
slahn <43726697+sloneearly@users.noreply.github.com>
2026-02-18 21:17:36 -0600

    fix(aggiemap-angular): Rearrange Layers for Aggieland Saturday (#632)

--- libs/ts/events/ngx/src/lib/definitions/aggieland-saturday.definitions.ts
+++ libs/ts/events/ngx/src/lib/definitions/aggieland-saturday.definitions.ts
@@ -7,10 +7,10 @@ import { MarkdownWDirectionsPopupComponent } from '../modules/popups/markdown-w-
 import esri = __esri;
 
 export enum AGGIELAND_SATURDAY_LAYERS {
-  PARKING = 'aggieland-saturday-parking',
-  BUS_ROUTES = 'aggieland-saturday-bus-routes',
   BUS_STOPS = 'aggieland-saturday-bus-stops',
-  SPECIAL_POIS = 'aggieland-saturday-special-pois'
+  BUS_ROUTES = 'aggieland-saturday-bus-routes',
+  SPECIAL_POIS = 'aggieland-saturday-special-pois',
+  PARKING = 'aggieland-saturday-parking'
 }
 
 const eventUrl = 'https://gis.tamu.edu/arcgis/rest/services/TS/AggielandSaturday/MapServer';
