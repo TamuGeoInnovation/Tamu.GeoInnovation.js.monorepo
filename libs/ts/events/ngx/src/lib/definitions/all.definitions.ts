06e76f796ad13d210a388ee5965a38446993400a
slahn <43726697+sloneearly@users.noreply.github.com>
2026-02-19 14:39:01 -0600

    feat(aggiemap-angular): Add AVP Parking Map (#610)

--- libs/ts/events/ngx/src/lib/definitions/all.definitions.ts
+++ libs/ts/events/ngx/src/lib/definitions/all.definitions.ts
@@ -36,7 +36,7 @@ import { RetireeParkingTs } from './retiree-parking.definitions';
 import { MaintenanceParkingTs } from './maintenance-parking.definitions';
 import { BaseballParkingTs } from './baseball-parking.definitions';
 import { SavannahBananasParkingTs } from './savannah-bananas.definitions';
-import { VisitorParkingTs } from './visitor-parking.definitions';
+import { AVPParkingTs } from './avp-parking.definitions';
 
 export const EventDefinitions: Array<AggiemapCustomMapConfiguration> = [
   FourHRoundupTs,
@@ -76,5 +76,5 @@ export const EventDefinitions: Array<AggiemapCustomMapConfiguration> = [
   MaintenanceParkingTs,
   BaseballParkingTs,
   SavannahBananasParkingTs,
-  VisitorParkingTs
+  AVPParkingTs
 ];
