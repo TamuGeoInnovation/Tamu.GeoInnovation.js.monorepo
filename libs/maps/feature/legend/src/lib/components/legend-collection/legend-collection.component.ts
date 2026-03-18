527f31fef5c02cbfcf7deb143a2de729a40c9028
slahn <43726697+sloneearly@users.noreply.github.com>
2026-03-18 09:04:48 -0500

    fix(aggiemap-angular): Fix issue with safety-first on sport map legends (#652)

--- libs/maps/feature/legend/src/lib/components/legend-collection/legend-collection.component.ts
+++ libs/maps/feature/legend/src/lib/components/legend-collection/legend-collection.component.ts
@@ -36,6 +36,15 @@ export class LegendCollectionComponent {
 
   public expanded = true;
 
+  private readonly _sportsSafetyFirstLayerIds = new Set([
+    'softball-parking-safety-first',
+    'swimming-parking-safety-first',
+    'soccer-parking-safety-first',
+    'volleyball-parking-safety-first',
+    'indoor-track-parking-safety-first',
+    'outdoor-track-parking-safety-first'
+  ]);
+
   public get childGroups(): IActiveLayerInfo[] {
     const children = this._toArray<IActiveLayerInfo>(this.group?.children);
 
@@ -63,8 +72,16 @@ export class LegendCollectionComponent {
     return this._toArray<esri.LegendElement>(this.group?.legendElements);
   }
 
+  public get visibleChildGroups(): IActiveLayerInfo[] {
+    if (this._shouldHideSportsSafetyFirstChildGroups()) {
+      return [];
+    }
+
+    return this.childGroups;
+  }
+
   public get hasChildren(): boolean {
-    return this.childGroups.length > 0;
+    return this.visibleChildGroups.length > 0;
   }
 
   public get childHideElementGroupHeaders(): boolean {
@@ -143,6 +160,14 @@ export class LegendCollectionComponent {
 
   private _isPhysicsFestivalGroup(layerId?: string): boolean {
     return layerId?.startsWith('phys-eng-festival-') ?? false;
+  }
+
+  public get showGroupTitle(): boolean {
+    return this.hasChildren && !this._sportsSafetyFirstLayerIds.has(this.group?.layer?.id);
+  }
+
+  private _shouldHideSportsSafetyFirstChildGroups(): boolean {
+    return this._sportsSafetyFirstLayerIds.has(this.group?.layer?.id) && this.legendElements.length > 0 && this.childGroups.length > 0;
   }
 
   private _getPhysicsFestivalChildPriority(title?: string): number {
