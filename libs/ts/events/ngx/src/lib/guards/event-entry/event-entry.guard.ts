c47a693bd3d80232851872e1da47df2588f6a6ee
slahn <43726697+sloneearly@users.noreply.github.com>
2026-03-23 10:48:56 -0500

    fix(aggiemap-angular): Direct Discover map builder to accommodations page instead of intro page (#682)

--- libs/ts/events/ngx/src/lib/guards/event-entry/event-entry.guard.ts
+++ libs/ts/events/ngx/src/lib/guards/event-entry/event-entry.guard.ts
@@ -26,7 +26,7 @@ export class EventEntryGuard implements CanActivate {
       return true;
     }
 
-    const targetCommands = this.eventSettingsService.hasOptions ? ['builder', 'intro'] : ['map'];
+    const targetCommands = this.eventSettingsService.hasOptions ? ['builder', 'accommodations'] : ['map'];
 
     return this.router.createUrlTree(['/', ...eventPath, ...targetCommands], {
       queryParams: route.queryParams,
