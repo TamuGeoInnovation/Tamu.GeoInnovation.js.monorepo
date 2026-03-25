15a85f1af6624fe587e71cc7003fdb7c4304dbd4
Slone Early <sloneearly@tamu.edu>
2026-03-25 19:56:00 +0000

    Updated dev and prod settings

--- apps/aggiemap-angular/src/environments/environment.dev.ts
+++ apps/aggiemap-angular/src/environments/environment.dev.ts
@@ -14,7 +14,7 @@ export { SelectionSymbols, Polygons } from '@tamu-gisc/aggiemap/ngx/common';
 export * from './notification-events';
 
 const sources = factory({
-  environment: 'prod'
+  environment: 'dev'
 });
 
 export const { Definitions, Connections, SearchSources, ThreeDLayers, LayerSources } = sources;
