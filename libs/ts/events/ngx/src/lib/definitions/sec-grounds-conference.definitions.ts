2d6485ac397f48402158efb286d676c9fb872448
Slone Early <sloneearly@tamu.edu>
2026-03-27 23:11:30 +0000

    added patterns and adjusted renderings

--- libs/ts/events/ngx/src/lib/definitions/sec-grounds-conference.definitions.ts
+++ libs/ts/events/ngx/src/lib/definitions/sec-grounds-conference.definitions.ts
@@ -1,7 +1,9 @@
-import { LayerSource } from '@tamu-gisc/common/types';
+import { FeatureLayerSourceProperties, LayerSource } from '@tamu-gisc/common/types';
 
 import { MarkdownPopupComponent } from '../modules/popups/markdown-popup/markdown-popup.component';
 import { AggiemapCustomMapConfiguration, EventConfiguration, SpecialEventOptions } from '../interfaces/special-event.interface';
+
+import esri = __esri;
 
 export enum SEC_GROUNDS_LAYERS {
   DAY1_POIS = 'sec-grounds-day1-pois',
@@ -20,23 +22,217 @@ enum ConferenceDay {
 
 const eventUrl = 'https://gis.it.tamu.edu/arcgis/rest/services/TS/SEC_Grounds_Conference/MapServer';
 
+type FeatureLayerNative = NonNullable<FeatureLayerSourceProperties['native']>;
+type AutoCastCimSymbol = { type: 'cim' } & esri.CIMSymbolProperties;
+
+const BUS_ROUTE_COLOR: [number, number, number, number] = [0, 92, 230, 255];
+const WALKING_ROUTE_COLOR: [number, number, number, number] = [56, 168, 0, 255];
+
+const BUS_ROUTE_ARROW_SPACING = 52;
+const WALKING_ROUTE_ARROW_SPACING = 44;
+
+const createRepeatedArrowLineSymbol = (
+  color: [number, number, number, number],
+  strokeWidth: number,
+  arrowSize: number,
+  placement: number
+): AutoCastCimSymbol => ({
+  type: 'cim',
+  data: {
+    type: 'CIMSymbolReference',
+    symbol: {
+      type: 'CIMLineSymbol',
+      symbolLayers: [
+        {
+          type: 'CIMSolidStroke',
+          enable: true,
+          width: strokeWidth,
+          color
+        },
+        {
+          type: 'CIMVectorMarker',
+          enable: true,
+          size: arrowSize,
+          markerPlacement: {
+            type: 'CIMMarkerPlacementAlongLineSameSize',
+            endings: 'WithMarkers',
+            placementTemplate: [placement],
+            angleToLine: true
+          },
+          frame: {
+            xmin: -5,
+            ymin: -5,
+            xmax: 5,
+            ymax: 5
+          },
+          markerGraphics: [
+            {
+              type: 'CIMMarkerGraphic',
+              primitiveName: 'route-arrow',
+              textString: '',
+              geometry: {
+                rings: [
+                  [
+                    [-8, -5.47],
+                    [-8, 5.6],
+                    [1.96, -0.03],
+                    [-8, -5.47]
+                  ]
+                ]
+              },
+              symbol: {
+                type: 'CIMPolygonSymbol',
+                symbolLayers: [
+                  {
+                    type: 'CIMSolidFill',
+                    enable: true,
+                    color
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      ]
+    }
+  }
+});
+
+const createOutlinedDashedLineSymbol = (
+  color: [number, number, number, number],
+  casingWidth: number,
+  fillWidth: number,
+  dashWidth: number,
+  dashTemplate: number[]
+): AutoCastCimSymbol => ({
+  type: 'cim',
+  data: {
+    type: 'CIMSymbolReference',
+    symbol: {
+      type: 'CIMLineSymbol',
+      symbolLayers: [
+        {
+          type: 'CIMSolidStroke',
+          enable: true,
+          capStyle: 'Butt',
+          joinStyle: 'Miter',
+          effects: [
+            {
+              type: 'CIMGeometricEffectDashes',
+              dashTemplate,
+              offsetAlongLine: 0,
+              lineDashEnding: 'FullGap'
+            }
+          ],
+          width: dashWidth,
+          color
+        },
+        {
+          type: 'CIMSolidStroke',
+          enable: true,
+          capStyle: 'Butt',
+          joinStyle: 'Miter',
+          width: fillWidth,
+          color: [255, 255, 255, 255]
+        },
+        {
+          type: 'CIMSolidStroke',
+          enable: true,
+          capStyle: 'Butt',
+          joinStyle: 'Miter',
+          width: casingWidth,
+          color
+        }
+      ]
+    }
+  }
+});
+
+const BUS_ROUTE_ARROW_SYMBOL = createRepeatedArrowLineSymbol(BUS_ROUTE_COLOR, 3.5, 7, BUS_ROUTE_ARROW_SPACING);
+const WALKING_ROUTE_ARROW_SYMBOL = createRepeatedArrowLineSymbol(WALKING_ROUTE_COLOR, 2.25, 6, WALKING_ROUTE_ARROW_SPACING);
+const DAY1_ROUTE_PATTERN_SYMBOL = createOutlinedDashedLineSymbol([0, 100, 0, 255], 4.6, 3.2, 1.4, [4, 4]);
+
+const createRouteLayerNative = (
+  values: Array<{ value: string; label: string; symbol: AutoCastCimSymbol }>
+): FeatureLayerNative => ({
+  outFields: ['*'],
+  renderer: {
+    type: 'unique-value',
+    field: 'description',
+    uniqueValueInfos: values.map((valueInfo) => ({
+      ...valueInfo
+    }))
+  }
+});
+
+const DAY1_ROUTE_NATIVE = createRouteLayerNative([
+  {
+    value: 'Walking Tour Day 1',
+    label: 'Walking Tour Day 1',
+    symbol: DAY1_ROUTE_PATTERN_SYMBOL
+  }
+]);
+
+const DAY2_ROUTE_NATIVE = createRouteLayerNative([
+  {
+    value: 'Bus Tour Route Day 2',
+    label: 'Bus Tour Route Day 2',
+    symbol: BUS_ROUTE_ARROW_SYMBOL
+  },
+  {
+    value: 'Walking Tour Day 2',
+    label: 'Walking Tour Day 2',
+    symbol: DAY1_ROUTE_PATTERN_SYMBOL
+  }
+]);
+
+const DAY3_ROUTE_NATIVE = createRouteLayerNative([
+  {
+    value: 'Bus Tour Route Day 3',
+    label: 'Bus Tour Route Day 3',
+    symbol: BUS_ROUTE_ARROW_SYMBOL
+  },
+  {
+    value: 'Walking Tour Day 3',
+    label: 'Walking Tour Day 3',
+    symbol: DAY1_ROUTE_PATTERN_SYMBOL
+  }
+]);
+
+const VISIBLE_DAY_LAYER_OVERRIDES = {
+  visible: true,
+  listMode: 'show'
+} as const;
+
+const HIDDEN_DAY_LAYER_OVERRIDES = {
+  visible: false,
+  listMode: 'hide'
+} as const;
+
+const CONFERENCE_DAYS = [ConferenceDay.DAY1, ConferenceDay.DAY2, ConferenceDay.DAY3] as const;
+
+const createConferenceDayLayerConversions = (activeDay: ConferenceDay) =>
+  CONFERENCE_DAYS.map((day) => ({
+    input: day,
+    expression: day === activeDay ? '1=1' : '1=0',
+    propOverrides: day === activeDay ? VISIBLE_DAY_LAYER_OVERRIDES : HIDDEN_DAY_LAYER_OVERRIDES
+  }));
+
 export const SecGroundsColdLayerSources: LayerSource[] = [
   {
     type: 'feature',
     id: SEC_GROUNDS_LAYERS.DAY1_ROUTES,
     title: 'Day 1 Routes',
-    url: `${eventUrl}/2`,
+    url: eventUrl + '/2',
     visible: true,
     listMode: 'show',
-    native: {
-      outFields: ['*']
-    }
+    native: DAY1_ROUTE_NATIVE
   },
   {
     type: 'feature',
     id: SEC_GROUNDS_LAYERS.DAY1_POIS,
     title: 'Day 1 Points of Interest',
-    url: `${eventUrl}/1`,
+    url: eventUrl + '/1',
     popupComponent: MarkdownPopupComponent,
     popupData: {
       name: 'attributes.name',
@@ -52,18 +248,16 @@ export const SecGroundsColdLayerSources: LayerSource[] = [
     type: 'feature',
     id: SEC_GROUNDS_LAYERS.DAY2_ROUTES,
     title: 'Day 2 Routes',
-    url: `${eventUrl}/5`,
+    url: eventUrl + '/5',
     visible: true,
     listMode: 'show',
-    native: {
-      outFields: ['*']
-    }
+    native: DAY2_ROUTE_NATIVE
   },
   {
     type: 'feature',
     id: SEC_GROUNDS_LAYERS.DAY2_POIS,
     title: 'Day 2 Points of Interest',
-    url: `${eventUrl}/4`,
+    url: eventUrl + '/4',
     popupComponent: MarkdownPopupComponent,
     popupData: {
       name: 'attributes.name',
@@ -79,18 +273,16 @@ export const SecGroundsColdLayerSources: LayerSource[] = [
     type: 'feature',
     id: SEC_GROUNDS_LAYERS.DAY3_ROUTES,
     title: 'Day 3 Routes',
-    url: `${eventUrl}/8`,
+    url: eventUrl + '/8',
     visible: true,
     listMode: 'show',
-    native: {
-      outFields: ['*']
-    }
+    native: DAY3_ROUTE_NATIVE
   },
   {
     type: 'feature',
     id: SEC_GROUNDS_LAYERS.DAY3_POIS,
     title: 'Day 3 Points of Interest',
-    url: `${eventUrl}/7`,
+    url: eventUrl + '/7',
     popupComponent: MarkdownPopupComponent,
     popupData: {
       name: 'attributes.name',
@@ -144,51 +336,27 @@ export const SecGroundsSpecialEventOptions: SpecialEventOptions = [
       layers: [
         {
           layerId: SEC_GROUNDS_LAYERS.DAY1_POIS,
-          conversions: [
-            { input: ConferenceDay.DAY1, expression: '1=1' },
-            { input: ConferenceDay.DAY2, expression: '1=0' },
-            { input: ConferenceDay.DAY3, expression: '1=0' }
-          ]
+          conversions: createConferenceDayLayerConversions(ConferenceDay.DAY1)
         },
         {
           layerId: SEC_GROUNDS_LAYERS.DAY1_ROUTES,
-          conversions: [
-            { input: ConferenceDay.DAY1, expression: '1=1' },
-            { input: ConferenceDay.DAY2, expression: '1=0' },
-            { input: ConferenceDay.DAY3, expression: '1=0' }
-          ]
+          conversions: createConferenceDayLayerConversions(ConferenceDay.DAY1)
         },
         {
           layerId: SEC_GROUNDS_LAYERS.DAY2_POIS,
-          conversions: [
-            { input: ConferenceDay.DAY1, expression: '1=0' },
-            { input: ConferenceDay.DAY2, expression: '1=1' },
-            { input: ConferenceDay.DAY3, expression: '1=0' }
-          ]
+          conversions: createConferenceDayLayerConversions(ConferenceDay.DAY2)
         },
         {
           layerId: SEC_GROUNDS_LAYERS.DAY2_ROUTES,
-          conversions: [
-            { input: ConferenceDay.DAY1, expression: '1=0' },
-            { input: ConferenceDay.DAY2, expression: '1=1' },
-            { input: ConferenceDay.DAY3, expression: '1=0' }
-          ]
+          conversions: createConferenceDayLayerConversions(ConferenceDay.DAY2)
         },
         {
           layerId: SEC_GROUNDS_LAYERS.DAY3_POIS,
-          conversions: [
-            { input: ConferenceDay.DAY1, expression: '1=0' },
-            { input: ConferenceDay.DAY2, expression: '1=0' },
-            { input: ConferenceDay.DAY3, expression: '1=1' }
-          ]
+          conversions: createConferenceDayLayerConversions(ConferenceDay.DAY3)
         },
         {
           layerId: SEC_GROUNDS_LAYERS.DAY3_ROUTES,
-          conversions: [
-            { input: ConferenceDay.DAY1, expression: '1=0' },
-            { input: ConferenceDay.DAY2, expression: '1=0' },
-            { input: ConferenceDay.DAY3, expression: '1=1' }
-          ]
+          conversions: createConferenceDayLayerConversions(ConferenceDay.DAY3)
         }
       ]
     }
