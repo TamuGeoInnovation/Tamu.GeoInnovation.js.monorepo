12a6a36c0dfdd46d4a6e6ab592f5b7fbdcedf479
Slone Early <sloneearly@tamu.edu>
2026-03-27 15:06:53 +0000

    Updated football and softball regionals

--- libs/ts/events/ngx/src/lib/definitions/football-parking.definitions.ts
+++ libs/ts/events/ngx/src/lib/definitions/football-parking.definitions.ts
@@ -364,7 +364,7 @@ export const FootballParkingColdLayerSources: LayerSource[] = [
 
 export const FootballParkingConfiguration: EventConfiguration = {
   id: 'gameday-parking',
-  name: 'Football',
+  name: 'Football Map',
   applicationName: 'Gameday Transportation Map',
   shortApplicationName: 'Gameday Map',
   introductionText: 'Get the best transportation and parking information for game days.',
