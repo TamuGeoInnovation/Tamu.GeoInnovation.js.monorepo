8db00c4644ea9d4c73108ec3b81a40f7823096ca
Slone Early <sloneearly@tamu.edu>
2026-05-11 17:09:49 +0000

    new css for requesting maps text

--- libs/aggiemap/ngx/core/src/lib/pages/requesting-maps/components/requesting-maps.component.ts
+++ libs/aggiemap/ngx/core/src/lib/pages/requesting-maps/components/requesting-maps.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'tamu-gisc-aggiemap-requesting-maps',
   templateUrl: './requesting-maps.component.html',
+  styleUrls: ['./requesting-maps.component.scss']
 })
 export class RequestingMapsComponent {}
