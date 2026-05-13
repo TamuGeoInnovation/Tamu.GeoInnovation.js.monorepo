f7eebfade39cf9995214954165776c5aebf968a8
sloneearly <43726697+sloneearly@users.noreply.github.com>
2026-05-13 11:06:20 -0500

    fix(aggiemap-angular) Align numbered list text on Requesting Maps page (#794)

--- libs/aggiemap/ngx/core/src/lib/pages/requesting-maps/components/requesting-maps.component.ts
+++ libs/aggiemap/ngx/core/src/lib/pages/requesting-maps/components/requesting-maps.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'tamu-gisc-aggiemap-requesting-maps',
-  templateUrl: './requesting-maps.component.html',
+  templateUrl: './requesting-maps.component.html'
 })
 export class RequestingMapsComponent {}
